02337183ca2bd0b599f5866719a5e4777c775e77
DavideDaniel <davidedaniel@gmail.com>
2017-01-28 10:35:41 -0800

    amend

--- build/2.2.d21e778eab217c94a250.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{272:function(e,exports,t){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}function u(){return r}Object.defineProperty(exports,"__esModule",{value:!0});var l=t(3),a=n(l),r=a.default.createElement("div",null,a.default.createElement("h1",null,"Example component"));exports.default=u}});
-//# sourceMappingURL=2.2.d21e778eab217c94a250.js.map
