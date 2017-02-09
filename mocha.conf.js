f2138ba82586a189714bc04b20fe8466a0383378
David Daniel <davidedaniel@gmail.com>
2017-02-08 20:35:22 -0800

    updated mocha conf and packages

--- mocha.conf.js
+++ mocha.conf.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
+// chai syntax
 const chai = require('chai');
+// stubbing/spying library
 const sinon = require('sinon');
+// dom object in node
+const jsdom = require('jsdom').jsdom;
 
-global.navigator = {
-  userAgent: 'node.js'
-};
+// global setup
 global.assert = chai.assert;
 global.expect = chai.expect;
-global.chai   = chai;
-global.sinon  = sinon;
+global.chai = chai;
+global.sinon = sinon;
+
+// dom setup
+global.document = jsdom('');
+global.navigator = {
+  userAgent: 'node.js',
+};
+global.window = document.defaultView;
+
+const exposedProperties = ['window', 'navigator', 'document'];
+// we're going to emulate the dom with this part here
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+// ref setup
+windowRef = window;
+documentRef = document;
+
+// chai setup
+chai.use(sinonChai);
+chai.use(require('chai-enzyme')());
