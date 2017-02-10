2c9a2b7bb3a33b0a2232955b72b0b6e2b6ca2ea5
David Daniel <davidedaniel@gmail.com>
2017-02-09 23:00:34 -0800

    updated test setup + added mocha.options + separated test & test:watch npm scripts

--- mocha.conf.js
+++ mocha.conf.js
@@ -1,35 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
 // chai syntax
-const chai = require('chai');
-// stubbing/spying library
-const sinon = require('sinon');
+const chai  = require('chai');
 // dom object in node
 const jsdom = require('jsdom').jsdom;
+
+// stubbing/spying library
+const sinon     = require('sinon');
+const sinonChai = require('sinon-chai');
 
 // global setup
 global.assert = chai.assert;
 global.expect = chai.expect;
-global.chai = chai;
-global.sinon = sinon;
+global.chai   = chai;
+global.sinon  = sinon;
 
 // dom setup
-global.document = jsdom('');
+global.document  = jsdom('');
 global.navigator = {
   userAgent: 'node.js',
 };
-global.window = document.defaultView;
+global.window    = document.defaultView;
 
-const exposedProperties = ['window', 'navigator', 'document'];
 // we're going to emulate the dom with this part here
+const exposedProperties = ['window', 'navigator', 'document'];
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
     global[property] = document.defaultView[property];
   }
 });
-
-// ref setup
-windowRef = window;
-documentRef = document;
 
 // chai setup
 chai.use(sinonChai);
