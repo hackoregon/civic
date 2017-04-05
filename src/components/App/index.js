41ec64342517561eb0e6eb5cad7facf8287ff259
Merritt Lawrenson <merrittlawrenson@gmail.com>
2017-04-04 20:43:51 -0700

    added some cards

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '@hackoregon/component-library/lib/Navigation/Header';
+import StoryCard from '@hackoregon/component-library/lib/StoryCard/StoryCard';
+import BarChart from '@hackoregon/component-library/lib/BarChart/BarChart';
 
 const Container = styled.div`
   min-height: 100%;
@@ -16,6 +18,28 @@ function App(props) {
   return (
     <Container>
       <Header />
+      <StoryCard title="There was once a bagel shop on 23rd Ave">
+        <p className="Description">
+          It blew up and now there are no bagels.
+        </p>
+      </StoryCard>
+      <StoryCard title="Introduction to Fire Stuff">
+        <p className="Description">
+          Here are all things you don't know yet.
+        </p>
+      </StoryCard>
+      <StoryCard title="Have a Map">
+        <p className="Description">
+          map
+        </p>
+      </StoryCard>
+      <StoryCard title="Have a Bar Chart">
+        <p className="Description">
+          Here is a chart with some fun data.
+        </p>
+        <BarChart data={[{ name: 'Fires', x: 200, y: 300 }]} />
+      </StoryCard>
+
       {React.Children.toArray(props.children)}
     </Container>
   );
