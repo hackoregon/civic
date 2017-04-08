41aa3d3aad987295587c6522fdbb9e4588d18e06
Keir Staple <keirstaple@comcast.net>
2017-04-08 12:57:24 -0700

    add promiseToGet function in api utility file

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const promiseToGet = (url, init) => axios.get(url, init)
+export const promiseToGet = (url) => axios.get(url)
   .then(response => {
     console.log(response.data)
     return response.data;
