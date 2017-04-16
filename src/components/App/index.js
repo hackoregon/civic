b39396e21db3bbdd8a21519c51b6a4696e889a5a
Michael Lange <dingoeatingfuzz@gmail.com>
2017-04-15 19:08:59 -0700

    Update the index page to fetch data from the api

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,8 +2,12 @@
 // This should probably be the core component, containing, nav etc
 
 import React from 'react';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
 import Header from '@hackoregon/component-library/lib/Navigation/Header';
+import Table from './table';
+import { getNeighborhoodRequest } from '../../state/selectors/api';
+import { neighborhoodFetch } from '../../state/api';
 
 const Container = styled.div`
   min-height: 100%;
@@ -12,11 +16,30 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-function App(props) {
+export function App({ children, isLoading, neighborhoodData, onNeighborhoodRequest }) {
+  let content;
+  if (isLoading) {
+    content = <span>Loading...</span>;
+  } else if (!neighborhoodData) {
+    content = <span>No data loaded!</span>;
+  } else {
+    content = <Table rows={neighborhoodData} />;
+  }
+
   return (
     <Container>
       <Header />
-      {React.Children.toArray(props.children)}
+      <button onClick={() => onNeighborhoodRequest()}>Fetch Data</button>
+      # Bedrooms:
+      <select onChange={event => onNeighborhoodRequest(event.target.value)}>
+        <option value="Studio">Studio</option>
+        <option value="1-BR">1</option>
+        <option value="2-BR">2</option>
+        <option value="3-BR">3</option>
+        <option value="Homeownership">Owned</option>
+      </select>
+      {content}
+      {React.Children.toArray(children)}
     </Container>
   );
 }
@@ -24,10 +47,30 @@ function App(props) {
 App.displayName = 'App';
 App.defaultProps = {
   children: <div />,
+  neighborhoodData: [],
+  onNeighborhoodRequest: () => {},
+  isLoading: false,
 };
 
 App.propTypes = {
   children: React.PropTypes.node,
+  neighborhoodData: React.PropTypes.array,
+  onNeighborhoodRequest: React.PropTypes.func,
+  isLoading: React.PropTypes.bool,
 };
 
-export default App;
+const mapDispatch = dispatch => ({
+  onNeighborhoodRequest: (size) => {
+    dispatch(neighborhoodFetch({
+      demographic: 'Couple+with+Family',
+      housing_size: size || 'Studio',
+    }));
+  },
+});
+
+const mapProps = state => ({
+  neighborhoodData: getNeighborhoodRequest(state).data,
+  isLoading: getNeighborhoodRequest(state).pending,
+});
+
+export default connect(mapProps, mapDispatch)(App);
