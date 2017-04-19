bec64e7914d5cb282732ebdc8b25c7f9bd056db9
Michael Lange <dingoeatingfuzz@gmail.com>
2017-04-18 20:05:25 -0700

    Adds tests for new selectors

--- src/state/selectors/app.test.js
+++ src/state/selectors/app.test.js
@@ -1,9 +1,42 @@
 import * as selectors from './app';
+import * as constants from '../../utils/data-constants';
+
+let state;
+const app = {};
+const ARBITRARY = 'Ron Burgundy';
 
 describe('app selectors', () => {
+  beforeEach(() => {
+    state = { app };
+  });
+
   describe('getAppState', () => {
     it('handles no state without errors', () => {
       selectors.getAppState().should.be.eql({});
+    });
+  });
+
+  describe('getSelectedUnitSize', () => {
+    it('should return the first unit size when unset', () => {
+      state = { app: {} };
+      expect(selectors.getSelectedUnitSize(state)).to.be.eql(constants.UNIT_SIZES[0]);
+    });
+
+    it('should return the set unit size when the state is set', () => {
+      state = { app: { selectedUnitSize: ARBITRARY } };
+      expect(selectors.getSelectedUnitSize(state)).to.be.eql(ARBITRARY);
+    });
+  });
+
+  describe('getSelectedDemographic', () => {
+    it('should return the first demographic when unset', () => {
+      state = { app: {} };
+      expect(selectors.getSelectedDemographic(state)).to.be.eql(constants.DEMOGRAPHICS[0]);
+    });
+
+    it('should return the set demographic when the state is set', () => {
+      state = { app: { selectedDemographic: ARBITRARY } };
+      expect(selectors.getSelectedDemographic(state)).to.be.eql(ARBITRARY);
     });
   });
 });
