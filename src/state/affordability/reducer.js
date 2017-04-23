2c5fc524aeba5ea370b7704c30bf4f5bd76aa212
Noah Muldavin <nmuldavin@gmail.com>
2017-04-22 17:56:42 -0700

    make utility to create standard api data reducer

--- src/state/affordability/reducer.js
+++ src/state/affordability/reducer.js
@@ -1,26 +1,4 @@
 import { INITIAL_STATE, actionTypes } from './constants';
+import { apiReducer } from '../utils';
 
-export default (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case actionTypes.CALL_START:
-      return {
-        pending: true,
-        data: null,
-        error: null,
-      };
-    case actionTypes.CALL_FAIL:
-      return {
-        pending: false,
-        error: action.payload,
-        data: null,
-      };
-    case actionTypes.CALL_SUCCESS:
-      return {
-        pending: false,
-        error: null,
-        data: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+export default apiReducer({ INITIAL_STATE, ...actionTypes });
--- src/state/utils.js
+++ src/state/utils.js
@@ -3,3 +3,33 @@ export const actionEmitter = type => (payload) => {
   if (payload != null) { ret.payload = payload; }
   return ret;
 };
+
+/*
+ * Function that makes a api reducer for managing api data given intiial state and
+ * action types. Put here becuase this pattern will be reused in at least 3 places
+ */
+export const apiReducer = ({ INITIAL_STATE, CALL_START, CALL_FAIL, CALL_SUCCESS }) =>
+  (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case CALL_START:
+        return {
+          pending: true,
+          data: null,
+          error: null,
+        };
+      case CALL_FAIL:
+        return {
+          pending: false,
+          error: action.payload,
+          data: null,
+        };
+      case CALL_SUCCESS:
+        return {
+          pending: false,
+          error: null,
+          data: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
