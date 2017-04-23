7073598fd7b5600cc7e9faf9c5f3d5b2b2cfa0d6
Noah Muldavin <nmuldavin@gmail.com>
2017-04-23 07:55:00 -0700

    Change example for side effects middleware to use fake action

--- src/middleware/sideEffectsMiddleware.js
+++ src/middleware/sideEffectsMiddleware.js
@@ -1,14 +1,14 @@
-import { OPEN_MODAL } from '../state/app';
+export const EXAMPLE_ACTION = 'EXAMPLE/ACTION';
 /*
    The idea is to filter through the actions and do sideeffects here.
    Because we have access to next we can continue with the other actions.
 */
-export const TEST_ACTION = { type: 'SOME_ACTION' };
+export const EXAMPLE_NEXT_ACTION = { type: 'SOME_ACTION' };
 const sideEffectsMiddleware = store => next => action => { // eslint-disable-line
   switch (action.type) {
-    case OPEN_MODAL:
-      return next(TEST_ACTION);
-      console.log(TEST_ACTION); // eslint-disable-line
+    case EXAMPLE_ACTION:
+      return next(EXAMPLE_NEXT_ACTION);
+      console.log(EXAMPLE_NEXT_ACTION); // eslint-disable-line
       break;
     default:
       return next(action);
