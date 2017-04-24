d741f16ef0328ccb7988dab8227069c116099ef6
Noah Muldavin <nmuldavin@gmail.com>
2017-04-23 23:23:04 -0700

    first global selectors

--- src/state/globalSelectors.js
+++ src/state/globalSelectors.js
@@ -4,6 +4,11 @@ import { isRentPending } from './rent/selectors';
 import { isNeighborhoodsPending } from './neighborhoods/selectors';
 
 /**
+ * I believe less than 30 % of income is what is considered 'affordable'. Double check with
+ * data people
+ */
+const isAffordable = (rent, income) => (rent * 12) < (0.3 * income);
+/**
  * Selector that returns truthy if any api call is pending
  */
 export const isAnyCallPending = createSelector(
