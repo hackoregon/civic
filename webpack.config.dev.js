f6c60b60773c61706d349a39eb4053f1b46ec364
Noah Muldavin <nmuldavin@gmail.com>
2017-04-26 08:38:59 -0700

    Remove unneeded webpack config hack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -53,7 +53,7 @@ module.exports = {
       { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
       { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]' },
-      { test: /(\.ico|\.geojson)$/, loader: 'file?name=[name].[ext]' },
+      { test: /\.ico$/, loader: 'file?name=[name].[ext]' },
       { test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'postcss'] },
       { test: /\.json$/, loader: 'json' },
     ],
