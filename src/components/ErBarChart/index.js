7424663702f60dfb2ab51ca0df79bd3fcd71f92a
Merritt Lawrenson <merrittlawrenson@gmail.com>
2017-04-26 19:49:54 -0700

    linked in new er-components from library

--- src/components/ErBarChart/index.js
+++ src/components/ErBarChart/index.js
@@ -4,15 +4,35 @@ import {
 } from 'recharts';
 
 const HorizontalBarChart = ({ data }) =>
-  <BarChart layout="horizontal" width={730} height={250} data={data}>
-    <XAxis dataKey="name" />
-    <YAxis />
-    <CartesianGrid strokeDasharray="3 3" />
-    <Tooltip />
-    <Legend />
-    <Bar dataKey="x" fill="#8884d8" />
-    <Bar dataKey="y" fill="#82ca9d" />
-  </BarChart>;
+  <div>
+    <BarChart layout="horizontal" width={730} height={250} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="x" fill="#8884d8" />
+      <Bar dataKey="y" fill="#82ca9d" />
+    </BarChart>;
+    <BarChart layout="horizontal" width={730} height={250} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="x" fill="#8884d8" />
+      <Bar dataKey="y" fill="#82ca9d" />
+    </BarChart>;
+    <BarChart layout="horizontal" width={730} height={250} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="x" fill="#8884d8" />
+      <Bar dataKey="y" fill="#82ca9d" />
+    </BarChart>
+  </div>;
 
 HorizontalBarChart.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object),
