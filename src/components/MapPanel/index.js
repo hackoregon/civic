f5027bd15ec2422cca3113d50be888272a0e043f
Keir Staple <keirstaple@comcast.net>
2017-04-26 18:51:15 -0700

    add some stats to panel

--- src/components/MapPanel/index.js
+++ src/components/MapPanel/index.js
@@ -8,6 +8,16 @@ class MapPanel extends Component {
     return (
       <div>
         <h1>Fire Management Area {stats.fma}</h1>
+        <div className="panel-column-1">
+          <h3>Area Demographics</h3>
+          <ul>
+            <li><span>{stats.fma_population_total}</span> population</li>
+            <li><span>{stats.median_hh_income}</span> median household income</li>
+            <li><span>{stats.percent_non_white}</span> percent non-white</li>
+            <li><span>{stats.percent_non_white}</span> percent non-white</li>
+            <li><span>{stats.percent_total_lesh}</span> percent limited english proficiency</li>
+          </ul>
+        </div>
       </div>
     );
   }
