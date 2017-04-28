d8ecafb1e9a6c1eb8cde4859a5d32f54d5ae5cde
drabelpdx <drabelpdx@gmail.com>
2017-04-28 14:29:42 -0700

    Working Stacked AreaCharts, tiptool not pretty

--- src/components/StackedAreaChart/index.js
+++ src/components/StackedAreaChart/index.js
@@ -1,42 +1,35 @@
 import React from 'react';
-import { AreaChart, XAxis, YAxis, Area, Tooltip, Legend } from 'recharts';
-
+import { AreaChart, XAxis, YAxis, Area, Tooltip } from 'recharts';
 import { data, colors } from './utils';
 
-const StackedAreaChart = () => {
-  const lines = [];
+require('./Tooltip.css');
 
+const StackedAreaChart = () => {
+  // Maps data to create lines/areas
+  const lines = [];
   const myKeys = Object.keys(data[0]);
   myKeys.forEach((key, index) => {
     if (key !== 'name') {
       lines.push(<Area
         type="linear"
-        key={key}
+        key={`${key} + ${Math.rand}`}
         dataKey={myKeys[index]}
         stroke={'#fff'}
         strokeWidth={3}
+        stackId={index}
         fillOpacity={1}
         fill={colors[index]}
       />);
     }
   });
 
-  const CustomTooltip = () => {
-    return (
-      <div className="custom-tooltip">
-        <p className="label">{`${name}`}</p>
-        <p className="desc">Anything you want can be displayed here.</p>
-      </div>
-    );
-  };
-
   return (
     <div style={{ display: 'flex', justifyContent: 'space-around', margin: 'auto' }} >
       <AreaChart width={800} height={300} data={data}>
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip content={<CustomTooltip />} />
         {lines}
+        <Tooltip itemStyle={{ color: 'black' }} />
       </AreaChart>
     </div>
   );
--- src/components/StackedAreaChart/utils.js
+++ src/components/StackedAreaChart/utils.js
@@ -1,16 +1,15 @@
 
-
 export const data = [
-      { name: '06-07', 'Public Safety': 300000000, 'Community Development': 250000000, 'Public Utilities': 200000000, 'Legislative and Administrative': 150000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
-      { name: '07-08', 'Public Safety': 350000000, 'Community Development': 300000000, 'Public Utilities': 250000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 150000000, 'Parks, Recreation, & Culture': 100000000, 'Elected Officials': 2000 },
-      { name: '08-09', 'Public Safety': 400000000, 'Community Development': 350000000, 'Public Utilities': 300000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
-      { name: '09-10', 'Public Safety': 350000000, 'Community Development': 300000000, 'Public Utilities': 250000000, 'Legislative and Administrative': 150000000, 'Transportation & Parking': 50000000, 'Parks, Recreation, & Culture': 30000000, 'Elected Officials': 2000 },
-      { name: '10-11', 'Public Safety': 400000000, 'Community Development': 350000000, 'Public Utilities': 300000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
-      { name: '11-12', 'Public Safety': 350000000, 'Community Development': 300000000, 'Public Utilities': 250000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 150000000, 'Parks, Recreation, & Culture': 100000000, 'Elected Officials': 2000 },
-      { name: '12-13', 'Public Safety': 350000000, 'Community Development': 250000000, 'Public Utilities': 200000000, 'Legislative and Administrative': 150000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
-      { name: '13-14', 'Public Safety': 400000000, 'Community Development': 350000000, 'Public Utilities': 300000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 150000000, 'Parks, Recreation, & Culture': 100000000, 'Elected Officials': 2000 },
-      { name: '14-15', 'Public Safety': 450000000, 'Community Development': 400000000, 'Public Utilities': 350000000, 'Legislative and Administrative': 250000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
-      { name: '15-16', 'Public Safety': 400000000, 'Community Development': 350000000, 'Public Utilities': 300000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 150000000, 'Parks, Recreation, & Culture': 100000000, 'Elected Officials': 2000 },
+      { name: '2007', 'Public Safety': 300000000, 'Community Development': 250000000, 'Public Utilities': 200000000, 'Legislative and Administrative': 150000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
+      { name: '2008', 'Public Safety': 350000000, 'Community Development': 300000000, 'Public Utilities': 250000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 150000000, 'Parks, Recreation, & Culture': 100000000, 'Elected Officials': 2000 },
+      { name: '2009', 'Public Safety': 400000000, 'Community Development': 350000000, 'Public Utilities': 300000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
+      { name: '2010', 'Public Safety': 350000000, 'Community Development': 300000000, 'Public Utilities': 250000000, 'Legislative and Administrative': 150000000, 'Transportation & Parking': 50000000, 'Parks, Recreation, & Culture': 30000000, 'Elected Officials': 2000 },
+      { name: '2011', 'Public Safety': 400000000, 'Community Development': 350000000, 'Public Utilities': 300000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
+      { name: '2012', 'Public Safety': 350000000, 'Community Development': 300000000, 'Public Utilities': 250000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 150000000, 'Parks, Recreation, & Culture': 100000000, 'Elected Officials': 2000 },
+      { name: '2013', 'Public Safety': 350000000, 'Community Development': 250000000, 'Public Utilities': 200000000, 'Legislative and Administrative': 150000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
+      { name: '2014', 'Public Safety': 400000000, 'Community Development': 350000000, 'Public Utilities': 300000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 150000000, 'Parks, Recreation, & Culture': 100000000, 'Elected Officials': 2000 },
+      { name: '2015', 'Public Safety': 450000000, 'Community Development': 400000000, 'Public Utilities': 350000000, 'Legislative and Administrative': 250000000, 'Transportation & Parking': 100000000, 'Parks, Recreation, & Culture': 50000000, 'Elected Officials': 2000 },
+      { name: '2016', 'Public Safety': 400000000, 'Community Development': 350000000, 'Public Utilities': 300000000, 'Legislative and Administrative': 200000000, 'Transportation & Parking': 150000000, 'Parks, Recreation, & Culture': 100000000, 'Elected Officials': 2000 },
 ];
 
 export const colors = [
@@ -22,32 +21,3 @@ export const colors = [
   '#339963',
   '#b3e6cb',
 ];
-
-// '#339966',
-// '#c6ecd9',
-// '#9fdfbf',
-// '#39ac73',
-// '#8cd9b3',
-// '#b3e6cc',
-// '#79d2a4',
-
-// '#329f7c',
-// '#3BBA92',
-// '#54C9A4',
-// '#73D3B5',
-// '#277C61',
-// '#1D5D49',
-// '#143E31',
-
-
-      //
-      // { name: '06-07', 'Public Safety': 375542531, 'Community Development': 223755032, 'Public Utilities': 1472330509, 'Legislative and Administrative': 872038820, 'Transportation & Parking': 256320999, 'Parks, Recreation, & Culture': 131163373, 'Elected Officials': 2000 },
-      // { name: '07-08', 'Public Safety': 399363449, 'Community Development': 202622775, 'Public Utilities': 1609400176, 'Legislative and Administrative': 925177129, 'Transportation & Parking': 206543005, 'Parks, Recreation, & Culture': 151307392, 'Elected Officials': 2000 },
-      // { name: '08-09', 'Public Safety': 430686323, 'Community Development': 240250960, 'Public Utilities': 1250875024, 'Legislative and Administrative': 816094302, 'Transportation & Parking': 193378286, 'Parks, Recreation, & Culture': 148890051, 'Elected Officials': 2000 },
-      // { name: '09-10', 'Public Safety': 470884013, 'Community Development': 278318958, 'Public Utilities': 1303193630, 'Legislative and Administrative': 852309239, 'Transportation & Parking': 240283972, 'Parks, Recreation, & Culture': 128010408, 'Elected Officials': 2000 },
-      // { name: '10-11', 'Public Safety': 476588146, 'Community Development': 411658494, 'Public Utilities': 1705198802, 'Legislative and Administrative': 881091079, 'Transportation & Parking': 246067362, 'Parks, Recreation, & Culture': 128894919, 'Elected Officials': 2000 },
-      // { name: '11-12', 'Public Safety': 480159893, 'Community Development': 469510996, 'Public Utilities': 1189209735, 'Legislative and Administrative': 936606841, 'Transportation & Parking': 237111101, 'Parks, Recreation, & Culture': 142276810, 'Elected Officials': 2000 },
-      // { name: '12-13', 'Public Safety': 475218943, 'Community Development': 325118678, 'Public Utilities': 1537815556, 'Legislative and Administrative': 872416369, 'Transportation & Parking': 238643517, 'Parks, Recreation, & Culture': 142562186, 'Elected Officials': 2000 },
-      // { name: '13-14', 'Public Safety': 496342942, 'Community Development': 269674746, 'Public Utilities': 1582081673, 'Legislative and Administrative': 915480675, 'Transportation & Parking': 273179274, 'Parks, Recreation, & Culture': 177023891, 'Elected Officials': 2000 },
-      // { name: '14-15', 'Public Safety': 528545038, 'Community Development': 305495956, 'Public Utilities': 1728205623, 'Legislative and Administrative': 891908591, 'Transportation & Parking': 269265273, 'Parks, Recreation, & Culture': 173466646, 'Elected Officials': 2000 },
-      // { name: '15-16', 'Public Safety': 530104701, 'Community Development': 372799318, 'Public Utilities': 1985943260, 'Legislative and Administrative': 911966115, 'Transportation & Parking': 336629241, 'Parks, Recreation, & Culture': 219571533, 'Elected Officials': 2000 },
