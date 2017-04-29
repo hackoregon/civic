58a56544ff8c6de9f2422ebb157d5698c4e7a0b0
Evan Palmer <evjpalmer@gmail.com>
2017-04-29 16:49:50 -0700

    temporarily add global css import for local dev

--- src/client.js
+++ src/client.js
@@ -1,3 +1,4 @@
+import '@hackoregon/component-library/src/global.styles.css';
 import React from 'react';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { render } from 'react-dom';
