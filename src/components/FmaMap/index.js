b16e8d90d6ce0afb636e071a41d0fd5e386054ea
Keir Staple <keirstaple@comcast.net>
2017-04-29 15:27:36 -0700

    safe commit

--- src/components/FmaMap/index.js
+++ src/components/FmaMap/index.js
@@ -1,3 +1,79 @@
+import '!style-loader!css-loader!@hackoregon/component-library/assets/leaflet.css';
+import React, { Component } from 'react';
+import LeafletMap from '@hackoregon/component-library/lib/LeafletMap/LeafletMap';
+import { GeoJSON } from 'react-leaflet';
+import { connect } from 'react-redux';
+import { getFmasThunk, getFmasData, renderFmaPanelProperties, getFmaPanelData } from '../../state';
+import { MapPanel } from '../index';
+
+const portland = [45.52, -122.67];
+
+class FmaMap extends Component {
+  constructor() {
+    super();
+    this.onEachFeature = this.onEachFeature.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.getFmas();
+  }
+
+  openPanel(e) {
+    const layer = e.target;
+    const fmaProperties = layer.feature.properties;
+    this.props.renderPanel(fmaProperties);
+  }
+
+  setOpacity(e) {
+    const layer = e.target;
+      // console.log(layer);
+    layer.setStyle({ color: '#EE495C', fillColor: '#EE495C', fillOpacity: '1' });
+  }
+
+  clearOpacity(e) {
+    const layer = e.target;
+    layer.setStyle({ color: '#EE495C', fillColor: '#EE495C', fillOpacity: '0' });
+  }
+
+  onEachFeature(feature, layer) {
+    layer.on({
+      click: this.openPanel.bind(this),
+      mouseover: this.setOpacity.bind(this),
+      mouseout: this.clearOpacity.bind(this),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <LeafletMap center={portland} zoom={11} height={600} width={900}>
+          {
+            this.props.fmasData ?
+              <GeoJSON data={this.props.fmasData} onEachFeature={this.onEachFeature} /> :
+              null
+          }
+        </LeafletMap>
+        {
+          this.props.fmaPanelData ?
+            <MapPanel /> :
+          null
+        }
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    fmasData: getFmasData(state),
+    fmaPanelData: getFmaPanelData(state),
+  }),
+  dispatch => ({
+    getFmas: () => dispatch(getFmasThunk()),
+    renderPanel: fmaProperties => dispatch(renderFmaPanelProperties(fmaProperties)),
+  }),
+)(FmaMap);
+
 // import '!style-loader!css-loader!@hackoregon/component-library/assets/leaflet.css';
 // import React, { Component } from 'react';
 // import LeafletMap from '@hackoregon/component-library/lib/LeafletMap/LeafletMap';
@@ -39,21 +115,12 @@
 //     // this.props.renderPanel(layer.feature.properties.fma_id);
 //   }
 //
-//   setOpacity(e) {
-//     const layer = e.target;
-//     // console.log(layer);
-//     layer.setStyle({ color: '#EE495C', fillColor: '#EE495C', fillOpacity: '1' });
-//   }
-//   clearOpacity(e) {
-//     const layer = e.target;
-//     layer.setStyle({ color: '#EE495C', fillColor: '#EE495C', fillOpacity: '0' });
-//   }
+//
 //
 //   onEachFeature(feature, layer) {
 //     console.log(feature.properties.fma_id);
 //     layer.on({
-//       mouseover: this.setOpacity,
-//       mouseout: this.clearOpacity,
+//
 //     });
 //   }
 //
@@ -101,66 +168,3 @@
 //     renderPanel: fmaProperties => dispatch(renderFmaPanelProperties(fmaProperties)),
 //   }),
 // )(FmaMap);
-
-import '!style-loader!css-loader!@hackoregon/component-library/assets/leaflet.css';
-import React, { Component } from 'react';
-import LeafletMap from '@hackoregon/component-library/lib/LeafletMap/LeafletMap';
-import { GeoJSON } from 'react-leaflet';
-import { connect } from 'react-redux';
-import { getFmasThunk, getFmasData, renderFmaPanelProperties, getFmaPanelData } from '../../state';
-import { MapPanel } from '../index';
-
-const portland = [45.52, -122.67];
-
-class FmaMap extends Component {
-  constructor() {
-    super();
-    this.onEachFeature = this.onEachFeature.bind(this);
-  }
-
-  componentWillMount() {
-    this.props.getFmas();
-  }
-
-  openPanel(e) {
-    const layer = e.target;
-    const fmaProperties = layer.feature.properties;
-    this.props.renderPanel(fmaProperties);
-  }
-
-  onEachFeature(feature, layer) {
-    layer.on({
-      click: this.openPanel.bind(this),
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <LeafletMap center={portland} zoom={11} height={600} width={900}>
-          {
-            this.props.fmasData ?
-              <GeoJSON data={this.props.fmasData} onEachFeature={this.onEachFeature} /> :
-              null
-          }
-        </LeafletMap>
-        {
-          this.props.fmaPanelData ?
-            <MapPanel /> :
-          null
-        }
-      </div>
-    );
-  }
-}
-
-export default connect(
-  state => ({
-    fmasData: getFmasData(state),
-    fmaPanelData: getFmaPanelData(state),
-  }),
-  dispatch => ({
-    getFmas: () => dispatch(getFmasThunk()),
-    renderPanel: fmaProperties => dispatch(renderFmaPanelProperties(fmaProperties)),
-  }),
-)(FmaMap);
