38a48011dc73896e9dd38a9776ca999275a42ba9
Evan Palmer <evjpalmer@gmail.com>
2017-04-29 16:42:48 -0700

    temporarily import global css

--- src/client.js
+++ src/client.js
@@ -1,3 +1,4 @@
+import '@hackoregon/component-library/src/global.styles.css'; // FOR DEV ONLY
 import React from 'react';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { render } from 'react-dom';
