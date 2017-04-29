625732dc5224ae8cce8fc338186f68f503b6bc0a
David Daniel <davidedaniel@gmail.com>
2017-04-29 01:44:39 -0700

    remove mergeAssets from this pkg

--- packages/webpacker/index.js
+++ packages/webpacker/index.js
@@ -1,12 +1,10 @@
 const defaultConfig = require('./lib/webpack.config').default;
 const composeConfig = require('./lib/composeConfig').default;
-const mergeAssets = require('./lib/mergeAssets').default;
 const getPaths = require('./lib/getPaths').default;
 const removeEmpty = require('./lib/utils').removeEmpty;
 
 module.exports = {
   getPaths,
-  mergeAssets,
   removeEmpty,
   composeConfig,
   defaultConfig,
