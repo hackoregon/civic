69034f273fd019c7be0548b793c2eb0b5ca288a3
Keir Staple <keirstaple@comcast.net>
2017-04-29 18:05:34 -0700

    format rest of data on panel

--- src/components/MapPanel/index.js
+++ src/components/MapPanel/index.js
@@ -7,33 +7,62 @@ class MapPanel extends Component {
     console.log('rendered map panel');
     const stats = this.props.fmaPanelData.stats;
     return (
-      <div>
+      <div style={{ backgroundColor: '#DEDCDC', padding: '50px 25px 50px 25px', textAlign: 'left' }}>
         <h1>Fire Management Area {stats.fma}</h1>
-        <div className="panel-column-1">
-          <h3>Area Demographics</h3>
-          <ul style={{ listStyleType: 'none' }}>
-            <li><span>{stats.fma_population_total}</span> population</li>
-            <li><span>{stats.median_hh_income}</span> median income</li>
-            <li><span>{stats.percent_non_white}</span> non-white</li>
-            <li><span>{stats.percent_total_lesh}</span> limited english proficiency</li>
-            <li><span>{stats.percent_below_pov}</span> households below poverty line</li>
-            <li><span>{stats.percent_member_65plus}</span> households with someone over 65</li>
-          </ul>
-        </div>
-        <div className="panel-column-2">
-          <h3>Incidents</h3>
-          <ul style={{ listStyleType: 'none' }}>
-            <li><span>{stats.total_incidents_2016}</span> emergencies</li>
-            <li><span> fire emergencies </span></li>
-          </ul>
-        </div>
-        <div className="panel-column-3">
-          <h3>Median Response Time</h3>
-          <ul style={{ listStyleType: 'none' }}>
-            <li><span>{stats.median_response_time} min</span> total</li>
-            <li><span>{stats.median_resp_time_med} min</span> medical</li>
-            <li><span>{stats.median_resp_time_fire} min</span> fire</li>
-          </ul>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div className="panel-column-1" style={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }}>
+            <h3>Area Demographics</h3>
+            <div style={{ display: 'flex' }}>
+              <ul style={{ listStyleType: 'none', margin: '0 15px 0 0', padding: '0', textAlign: 'left', color: '#D5135F' }}>
+                <li>{stats.fma_population_total}</li>
+                <li>{stats.median_hh_income}</li>
+                <li>{(stats.percent_non_white * 100).toFixed(2)}%</li>
+                <li>{(stats.percent_total_lesh * 100).toFixed(2)}%</li>
+                <li>{(stats.percent_below_pov * 100).toFixed(2)}%</li>
+                <li>{(stats.percent_member_65plus * 100).toFixed(2)}%</li>
+              </ul>
+              <ul style={{ listStyleType: 'none', margin: '0', padding: '0', textAlign: 'left' }}>
+                <li>population</li>
+                <li>median income</li>
+                <li>non-white</li>
+                <li>limited english proficiency</li>
+                <li>households below poverty line</li>
+                <li>households with someone over 65</li>
+              </ul>
+            </div>
+          </div>
+
+          <div style={{ display: 'flex' }}>
+            <div className="panel-column-2" style={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }}>
+              <h3>Area Demographics</h3>
+              <div style={{ display: 'flex' }}>
+                <ul style={{ listStyleType: 'none', margin: '0 15px 0 0', padding: '0', textAlign: 'left', color: '#D5135F' }}>
+                  <li>{stats.total_incidents_2016}</li>
+                </ul>
+                <ul style={{ listStyleType: 'none', margin: '0', padding: '0', textAlign: 'left' }}>
+                  <li>emergencies</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div style={{ display: 'flex' }}>
+            <div className="panel-column-3" style={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }}>
+              <h3>Area Demographics</h3>
+              <div style={{ display: 'flex' }}>
+                <ul style={{ listStyleType: 'none', margin: '0 15px 0 0', padding: '0', textAlign: 'left', color: '#D5135F' }}>
+                  <li>{stats.median_response_time.toFixed(2)} min</li>
+                  <li>{stats.median_resp_time_med.toFixed(2)} min</li>
+                  <li>{stats.median_resp_time_fire.toFixed(2)} min</li>
+                </ul>
+                <ul style={{ listStyleType: 'none', margin: '0', padding: '0', textAlign: 'left' }}>
+                  <li>total</li>
+                  <li>medical</li>
+                  <li>fire</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
