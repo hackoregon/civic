d2877bd5f908bdc90865295a26c31e65d5abee85
Merritt Lawrenson <merrittlawrenson@gmail.com>
2017-04-29 18:15:42 -0700

    styling and copy changes. video is loaded. just waiting on new pie chart, timeline image, and map.

--- src/components/BagelShop/index.js
+++ src/components/BagelShop/index.js
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player';
 class BagelShop extends Component {
   render() {
     return (
-      <ReactPlayer url='https://vimeo.com/215352687' playing loop/>
+      <ReactPlayer url="https://vimeo.com/215352687" width="100%" />
     );
   }
 }
