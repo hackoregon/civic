f8f3c72009e46142779472382ddc9d3345c05322
David Daniel <davidedaniel@gmail.com>
2017-04-30 11:21:27 -0700

    remove isomorphic plugin from default

--- packages/webpacker/src/plugins.js
+++ packages/webpacker/src/plugins.js
@@ -1,5 +1,4 @@
 import webpack from 'webpack';
-// import IsomorphicLoaderPlugin from 'isomorphic-loader/lib/webpack-plugin';
 import AssetsPlugin from 'assets-webpack-plugin';
 import autoprefixer from 'autoprefixer';
 
@@ -7,14 +6,11 @@ import { removeEmpty, isProd } from './utils';
 import { BUNDLE_PATH } from './paths';
 
 const assetFileName = 'webpack-assets.json';
-// const isomorphicAssetsFile = 'isomorphic-assets.json';
+
 
 export default {
   plugins: removeEmpty([
-    // new IsomorphicLoaderPlugin({
-    //   keepExistingConfig: false,
-    //   assetsFile: isomorphicAssetsFile,
-    // }),
+
     new webpack.LoaderOptionsPlugin({ options: { postcss: [autoprefixer] } }),
     isProd && (
           new webpack.LoaderOptionsPlugin({
