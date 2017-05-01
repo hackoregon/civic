7957ebff5afd87e503e91dce603e3bca2ad60290
Evan Palmer <evjpalmer@gmail.com>
2017-05-01 07:39:45 -0700

    add dropdown options and bubble data

--- src/data.js
+++ src/data.js
@@ -29,6 +29,121 @@ export const serviceAreaOptions = [
     }
   ];
 
+export const bureauOptions = [
+  {
+      "value": "WA",
+      "label": "Portland Water Bureau"
+    },
+    {
+      "value": "HC",
+      "label": "Portland Housing Bureau"
+    },
+    {
+      "value": "MY",
+      "label": "Office of the Mayor"
+    },
+    {
+      "value": "PA",
+      "label": "Commissioner of Public Affairs"
+    },
+    {
+      "value": "PS",
+      "label": "Commissioner of Public Safety"
+    },
+    {
+      "value": "TR",
+      "label": "Portland Bureau of Transportation"
+    },
+    {
+      "value": "AT",
+      "label": "Office of the City Attorney"
+    },
+    {
+      "value": "OE",
+      "label": "Office of Equity & Human Rights"
+    },
+    {
+      "value": "AU",
+      "label": "Office of the City Auditor"
+    },
+    {
+      "value": "EC",
+      "label": "Bureau of Emergency Communications"
+    },
+    {
+      "value": "ES",
+      "label": "Bureau of Environmental Services"
+    },
+    {
+      "value": "PW",
+      "label": "Commissioner of Public Works"
+    },
+    {
+      "value": "BO",
+      "label": "City Budget Office"
+    },
+    {
+      "value": "NI",
+      "label": "Office of Neighborhood Involvement"
+    },
+    {
+      "value": "PL",
+      "label": "Portland Police Bureau"
+    },
+    {
+      "value": "GR",
+      "label": "Office of Government Relations"
+    },
+    {
+      "value": "MF",
+      "label": "Office of Management & Finance"
+    },
+    {
+      "value": "SD",
+      "label": "Office of Sustainable Development"
+    },
+    {
+      "value": "CB",
+      "label": "Office for Community Technology"
+    },
+    {
+      "value": "FR",
+      "label": "Portland Fire & Rescue"
+    },
+    {
+      "value": "PN",
+      "label": "Bureau of Planning & Sustainability"
+    },
+    {
+      "value": "PU",
+      "label": "Commissioner of Public Utilities"
+    },
+    {
+      "value": "PK",
+      "label": "Portland Parks & Recreation"
+    },
+    {
+      "value": "HN",
+      "label": "Office of Human Relations"
+    },
+    {
+      "value": "DS",
+      "label": "Bureau of Development Services"
+    },
+    {
+      "value": "ZD",
+      "label": "Portland Development Commission"
+    },
+    {
+      "value": "EM",
+      "label": "Portland Bureau of Emergency Management"
+    },
+    {
+      "value": "DR",
+      "label": "Bureau of Fire & Police Disability & Retirement"
+    }
+];
+
 export const serviceAreaBubbleDataFTW = {
   FY2013: [
       {
@@ -134,4 +249,175 @@ export const serviceAreaBubbleDataFTW = {
       },
 
  ],
+}
+
+export const bureauDataByYear = { PS: {
+  FY2013: [
+      {
+        title: 'Bureau of Emergency Communications',
+        id: 1,
+        total_amount: 23346735,
+        color: '#9bcebe ',
+        percentage: '5.14% of Public Safety Budget'
+      },
+      {
+        title: 'Bureau of Fire & Police Disability Retirement',
+        id: 1,
+        total_amount: 162389491,
+        color: '#4ba78a ',
+        percentage: '35.75% of Public Safety Budget'
+      },
+      {
+        title: 'Portland Bureau of Emergency Management',
+        id: 1,
+        total_amount: 510559,
+        color: '#afd8cb ',
+        percentage: '0.11% of Public Safety Budget'
+      },
+     {
+        title: 'Portland Fire & Rescue',
+        id: 1,
+        total_amount: 98589213,
+        color: '#73bba4 ',
+        percentage: '21.7% of Public Safety Budget'
+      },
+     {
+        title: 'Portland Police Bureau',
+        id: 1,
+        total_amount: 169435128,
+        color: '#389e7d ',
+        percentage: '37.3% of Service Area Budget'
+      },
+  ],
+   FY2014: [
+      {
+        title: 'Bureau of Emergency Communications',
+        id: 1,
+        total_amount: 25648759,
+        color: '#afd8cb ',
+        percentage: '5.37% of Public Safety Budget'
+      },
+      {
+        title: 'Bureau of Fire & Police Disability Retirement',
+        id: 1,
+        total_amount: 164286502,
+        color: '#4ba78a ',
+        percentage: '34.37% of Public Safety Budget'
+      },
+      {
+        title: 'Portland Bureau of Emergency Management',
+        id: 1,
+        total_amount: 435600, // using positive amount because circle radius can't be negative
+        color: '#F06D63 ',
+        percentage: '-0.09% of Public Safety Budget'
+      },
+     {
+        title: 'Portland Fire & Rescue',
+        id: 1,
+        total_amount: 106020212,
+        color: '#73bba4 ',
+        percentage: '22.18% of Public Safety Budget'
+      },
+     {
+        title: 'Portland Police Bureau',
+        id: 1,
+        total_amount: 182510373,
+        color: '#389e7d ',
+        percentage: '38.18% of Public Safety Budget'
+      },
+  ],
+}
+}
+
+export const psProgramData = { PS: { FR: {
+  FY2013: [
+      {
+        title: 'Administration & Support',
+        id: 1,
+        total_amount: 487590,
+        color: '#F06D63 ',
+        percentage: '-0.49% of Portland Fire & Rescue Budget'
+      },
+      {
+        title: 'Chief\'s Office',
+        id: 1,
+        total_amount: 1665024,
+        color: '#d7ece5 ',
+        percentage: '1.69% of Portland Fire & Rescue Budget'
+      },
+      {
+        title: 'Emergency Operations',
+        id: 1,
+        total_amount: 71427369,
+        color: '#389e7d ',
+        percentage: '72.45% of Portland Fire & Rescue Budget'
+      },
+     {
+        title: 'Management Services',
+        id: 1,
+        total_amount: 98589213,
+        color: '#9bcebe ',
+        percentage: '15.41% of Portland Fire & Rescue Budget'
+      },
+     {
+        title: 'Prevention',
+        id: 1,
+        total_amount: 169435128,
+        color: '#afd8cb ',
+        percentage: '6.49% of Portland Fire & Rescue Budget'
+      },
+         {
+        title: 'Training & Safety',
+        id: 1,
+        total_amount: 4385972,
+        color: '#afd8cb ',
+        percentage: '4.45% of Portland Fire & Rescue Budget'
+      },
+  ],
+  FY2014: [
+      {
+        title: 'Administration & Support',
+        id: 1,
+        total_amount: 950428,
+        color: '#F06D63 ',
+        percentage: '-0.90% of Portland Fire & Rescue Budget'
+      },
+      {
+        title: 'Chief\'s Office',
+        id: 1,
+        total_amount: 2027460,
+        color: '#d7ece5 ',
+        percentage: '1.91% of Portland Fire & Rescue Budget'
+      },
+      {
+        title: 'Emergency Operations',
+        id: 1,
+        total_amount: 75987979,
+        color: '#389e7d ',
+        percentage: '71.67% of Portland Fire & Rescue Budget'
+      },
+     {
+        title: 'Management Services',
+        id: 1,
+        total_amount: 16577985,
+        color: '#9bcebe ',
+        percentage: '15.64% of Portland Fire & Rescue Budget'
+      },
+     {
+        title: 'Prevention',
+        id: 1,
+        total_amount: 6943454,
+        color: '#afd8cb ',
+        percentage: '6.55% of Portland Fire & Rescue Budget'
+      },
+     {
+        title: 'Training & Safety',
+        id: 1,
+        total_amount: 5433762,
+        color: '#afd8cb ',
+        percentage: '5.13% of Portland Fire & Rescue Budget'
+      },
+  ],
+}
+}
 }
