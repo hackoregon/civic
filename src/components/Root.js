710192d6f3c4b60b3fccc9854380b28878d2b71a
Keir Staple <keirstaple@comcast.net>
2017-05-01 12:21:54 -0700

    remove redux dev tool

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,13 +1,12 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-import DevTools from './DevTools';
+// import DevTools from './DevTools';
 
 const Root = ({ store, routes, history }) => (
   <Provider store={store}>
     <div>
       <Router routes={routes} history={history} />
-      <DevTools />
     </div>
   </Provider>
 );
