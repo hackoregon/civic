d0009386c2652bdbac2dde26b7721f5896c761e5
Evan Palmer <evjpalmer@gmail.com>
2017-04-30 20:00:45 -0700

    more changes

---
+++ src/data.js
@@ -0,0 +1,137 @@
+export const serviceAreaOptions = [
+    {
+      "value": "LA",
+      "label": "Legislative, Administrative, & Support"
+    },
+    {
+      "value": "PS",
+      "label": "Public Safety"
+    },
+    {
+      "value": "PU",
+      "label": "Public Utilities"
+    },
+    {
+      "value": "CD",
+      "label": "Community Development"
+    },
+    {
+      "value": "PR",
+      "label": "Parks, Recreation, & Culture"
+    },
+    {
+      "value": "TP",
+      "label": "Transportation & Parking"
+    },
+    {
+      "value": "EO",
+      "label": "Elected Officials"
+    }
+  ];
+
+export const serviceAreaBubbleDataFTW = {
+  FY2013: [
+      {
+        title: 'Public Safety',
+        id: 1,
+        total_amount: 454271126,
+        color: '#73bba4',
+        percentage: '12.23% of Service Area Budget'
+      },
+      {
+        title: 'Community Development',
+        id: 1,
+        total_amount: 153415159,
+        color: '#afd8cb ',
+        percentage: '4.13% of Service Area Budget'
+      },
+      {
+        title: 'Public Utilities',
+        id: 1,
+        total_amount: 1582082768,
+        color: '#389e7d ',
+        percentage: '42.6% of Service Area Budget'
+      },
+     {
+        title: 'City Support Services',
+        id: 1,
+        total_amount: 1035142785,
+        color: '#4ba78a ',
+        percentage: '27.87% of Service Area Budget'
+      },
+     {
+        title: 'Transportation & Parking',
+        id: 1,
+        total_amount: 273179274,
+        color: '#9bcebe ',
+        percentage: '7.36% of Service Area Budget'
+      },
+     {
+        title: 'Parks,Recreation & Culture',
+        id: 1,
+        total_amount: 140838221,
+        color: '#afd8cb ',
+        percentage: '3.79% of Service Area Budget'
+      },
+     {
+        title: 'Elected Officials',
+        id: 1,
+        total_amount: 74853868,
+        color: '#d7ece5 ',
+        percentage: '2.02% of Service Area Budget'
+      },
+
+ ],
+  FY2014: [
+       {
+        title: 'Public Safety',
+        id: 1,
+        total_amount: 478030246,
+        color: '#73bba4',
+        percentage: '12.27% of Service Area Budget'
+      },
+      {
+        title: 'Community Development',
+        id: 1,
+        total_amount: '#73bba4' ,
+        color: '#afd8cb ',
+        percentage: '5.3% of Service Area Budget'
+      },
+      {
+        title: 'Public Utilities',
+        id: 1,
+        total_amount: 1728176859,
+        color: '#389e7d',
+        percentage: '44.35% of Service Area Budget'
+      },
+     {
+        title: 'City Support Services',
+        id: 1,
+        total_amount: 1001330891,
+        color: '#4ba78a',
+        percentage: '25.7% of Service Area Budget'
+      },
+     {
+        title: 'Transportation & Parking',
+        id: 1,
+        total_amount: 269267773,
+        color: '#9bcebe ',
+        percentage: '6.91% of Service Area Budget'
+      },
+     {
+        title: 'Parks,Recreation & Culture',
+        id: 1,
+        total_amount: 154066617,
+        color: '#afd8cb ',
+        percentage: '3.95% of Service Area Budget'
+      },
+     {
+        title: 'Elected Officials',
+        id: 1,
+        total_amount: 59516622,
+        color: '#d7ece5 ',
+        percentage: '1.53% of Service Area Budget'
+      },
+
+ ],
+}
