c9d0e568c2dc98976d46a06bd2afbc2fbdbe4db2
Evan Palmer <evjpalmer@gmail.com>
2017-05-13 11:57:26 -0700

    remove commented out loader rules

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -56,15 +56,11 @@ module.exports = {
       { test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file' },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
       { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-      // { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
-      // { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]' },
       { test: /\.ico$/, loader: 'file?name=[name].[ext]' },
-      // { test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'postcss'] },
       { test: /\.json$/, loader: 'json' },
       { test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'postcss'] },
       {
         test: /leaflet\.css$/,
-        // exclude: [path.resolve(__dirname, 'node_modules'), path.resolve(__dirname, '../src/assets')],
         loader: 'style-loader!css-loader',
       },
 
