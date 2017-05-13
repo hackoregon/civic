5c183056f17ba24ba978758c9eb8e61e58926c44
mikethecanuck <mikethecanuck@gmail.com>
2017-05-13 16:36:28 -0700

    Initial commit of the files necessary to build and test the Docker container in automated e.g. Travis settings

---
+++ bin/docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo  Running docker-entrypoint.sh...
+
+# Insert the commands here that are necessary to cause the app to start and stay running
---
+++ bin/test-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=$PATH:~/.local/bin
+
+# Insert here the commands that are necessary to cause the project to build and run tests that exit with 0 if successful
