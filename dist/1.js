82ab562a6b7771da5dc1167460017a982d406ca4
Megan McKissack <meganmckissack@gmail.com>
2017-05-16 16:16:00 -0700

    removed housing path from webpack config prod file

--- dist/1.js
+++ dist/1.js
@@ -1,6 +1,6 @@
 webpackJsonp([1],{
 
-/***/ 1695:
+/***/ 1415:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -11,7 +11,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = NotFoundPage;
 
-var _react = __webpack_require__(0);
+var _react = __webpack_require__(1);
 
 var _react2 = _interopRequireDefault(_react);
 
