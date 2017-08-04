848b98d014f815436c46ce9daec6f78d4c4e7838
David Daniel <davidedaniel@gmail.com>
2017-08-03 23:35:30 -0700

    add a dev file for real

---
+++ packages/emergency-response/src/client.dev.js
@@ -0,0 +1,21 @@
+import '@hackoregon/component-library/src/global.styles.css';
+
+// eslint-disable-next-line
+import '!style-loader!css-loader!@hackoregon/component-library/assets/leaflet.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore from './store';
+import { Root } from './components';
+import getRoutes from './index';
+
+const store = configureStore({}, browserHistory);
+const history = syncHistoryWithStore(browserHistory, store);
+const routes = getRoutes(history);
+
+ReactDOM.render(
+  <Root store={store} history={history} routes={routes} />,
+  document.getElementById('content'),
+);
