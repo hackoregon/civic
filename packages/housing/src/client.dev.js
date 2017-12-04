6ff8270f83d6a491823726e268b506ab0b9f5417
David Daniel <davidedaniel@gmail.com>
2017-12-03 16:34:59 -0800

    cleanup

--- packages/housing/src/client.dev.js
+++ packages/housing/src/client.dev.js
@@ -9,69 +9,9 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-// import { Provider } from 'react-redux';
 import configureStore from './configureStore';
-// import AppPage from './components/App';
-// import createRoutes from './index';
-//
-// const locationStateSelector = () => {
-//   let prevRoutingState;
-//
-//   return (state) => {
-//     const routingState = state.routing;
-//
-//     if (routingState !== prevRoutingState) {
-//       prevRoutingState = routingState;
-//     }
-//
-//     return prevRoutingState;
-//   };
-// };
-//
-// const initialState = {};
-// const store = configureStore(initialState, browserHistory);
-//
-// const history = syncHistoryWithStore(browserHistory, store, {
-//   selectLocationState: locationStateSelector(),
-// });
-//
-// const routes = createRoutes(store);
-//
-// const rootRoute = {
-//   component: AppPage,
-//   childRoutes: routes,
-// };
-//
-// const renderApp = () => {
-//   render(
-//     <AppContainer>
-//       <Provider store={store}>
-//         <Router history={history} routes={rootRoute} />
-//       </Provider>
-//     </AppContainer>,
-//     document.getElementById('content'),
-//   );
-// };
-//
-// renderApp();
-//
-// if (module.hot) {
-//   module.hot.accept('./components/App', () => {
-//     renderApp();
-//   });
-// }
-
-// import '@hackoregon/component-library/src/global.styles.css';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { browserHistory } from 'react-router';
-// import { syncHistoryWithStore } from 'react-router-redux';
-// import configureStore from './store';
-
 
 import getRoutes from './index';
-// const history = syncHistoryWithStore(browserHistory, store);
-// const routes = getRoutes(history);
 
 
 const store = configureStore({}, browserHistory);
