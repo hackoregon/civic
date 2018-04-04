08ce5aea93f7a843fa765eafa736bdfa398c78b6
Michael Lange <dingoeatingfuzz@gmail.com>
2018-04-04 09:30:12 -0700

    Convert Button from CSS Modules to Emotion
    This will eliminate the need for flaky regexs to switch between
    the normal css loader and the css modules loader.

--- packages/component-library/src/Button/Button.js
+++ packages/component-library/src/Button/Button.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import classNames from 'classnames/bind';
-import styles from './Button.styles.css';
+import { css } from 'emotion';
 
-const cx = classNames.bind(styles);
+const accentColor = 'rgb(238, 73, 80)';
+const commonTransition = 'all .2s ease-in-out';
+const buttonClass = css`
+  display: flex;
+  padding: 6px;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  transition: ${commonTransition};
+  font-size: 1em;
+  font-weight: 600;
+  color: ${accentColor};
+  background: #FFF;
+  cursor: pointer;
+  border: 2px solid ${accentColor};
 
-const className = cx({ base: true });
+  i {
+    margin-right: 12px;
+  }
+
+  span {
+    flex-wrap: nowrap;
+    transition: ${commonTransition};
+  }
+
+  &:hover {
+    background-color: ${accentColor};
+    color: #FFF;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 const Button = ({ children, onClick }) => (
-  <button className={className} onClick={onClick}>
+  <button className={buttonClass} onClick={onClick}>
     {children}
   </button>
 );
