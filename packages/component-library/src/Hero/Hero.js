da102d5f6c2fedd35a9fc3ac9a4bb66048c3ba5a
Michael Lange <dingoeatingfuzz@gmail.com>
2018-04-04 14:29:13 -0700

    Hero: CSS Modules --> Emotion

--- packages/component-library/src/Hero/Hero.js
+++ packages/component-library/src/Hero/Hero.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import styles from './Hero.css';
+import { css } from 'emotion';
+
+const heroClass = css`
+  display: flex;
+  background: rgb(34, 15, 37);
+  height: 75vh;
+  min-height: 420px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-size: cover;
+  background-position: center center;
+  z-index: -100;
+  align-items: center;
+  justify-content: center;
+`;
+
+const containerClass = css`
+  display: flex;
+  width: 100%;
+  max-width: 800px;
+
+  @media small {
+    padding: 0 15px;
+  }
+`;
+
+const contentClass = css`
+  position: relative;
+  padding-top: 0px;
+
+  & > h1 {
+    font-size: 3em;
+    line-height: 1.25em;
+    color: #FFF;
+    font-weight: 100;
+  }
+`;
 
 const DefaultChildren = () => (
   <h1>Data for the people,<br />by the people.</h1>
 );
 
 const Hero = ({ children }) => (
-  <div className={styles.hero}>
-    <div className={styles.container}>
-      <div className={styles.content}>
+  <div className={heroClass}>
+    <div className={containerClass}>
+      <div className={contentClass}>
         {children || <DefaultChildren />}
       </div>
     </div>
