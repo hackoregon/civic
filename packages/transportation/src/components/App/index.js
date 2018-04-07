4debb9a81ad63bac96e1437a29db0d10e426b453
Michael Lange <dingoeatingfuzz@gmail.com>
2018-04-07 14:55:44 -0700

    Remove unnecessary elements from the transportation main component

--- packages/transportation/src/components/App/index.js
+++ packages/transportation/src/components/App/index.js
@@ -1,8 +1,11 @@
+import '@hackoregon/component-library/assets/global.styles.css';
+import '@hackoregon/component-library/assets/vendor/leaflet.css';
+
 /* eslint-disable import/no-extraneous-dependencies */
 // This should probably be the core component, containing, nav etc
 import React from 'react';
 import styled from 'styled-components';
-import { Header, Footer, StoryCard } from '@hackoregon/component-library';
+import { StoryCard } from '@hackoregon/component-library';
 import ConstructionViews from '../ConstructionViews';
 import CrashData from '../CrashData/CrashData';
 // import L from 'leaflet';
@@ -17,7 +20,6 @@ const Container = styled.div`
 function App(props) {
   return (
     <Container>
-      <Header />
 
       <StoryCard title="Portland Road Works Explorer" collectionId="transportation" cardId="trans-construction-maps">
         <ConstructionViews />
@@ -27,7 +29,6 @@ function App(props) {
       <CrashData />
 
 
-      <Footer />
       {React.Children.toArray(props.children)}
     </Container>
   );
