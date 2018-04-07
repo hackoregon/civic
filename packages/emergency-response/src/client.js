2a2e82f33c2b3ac9bbe193496396584c83e0b3a2
Michael Lange <dingoeatingfuzz@gmail.com>
2018-04-07 14:53:48 -0700

    Update emergency-response

--- packages/emergency-response/src/client.js
+++ packages/emergency-response/src/client.js
@@ -1,8 +1,3 @@
-// import '@hackoregon/component-library/assets/global.styles.css';
-
-// eslint-disable-next-line
-// import '@hackoregon/component-library/assets/vendor/leaflet.css';
-
 import MockWrapper from '@hackoregon/mock-wrapper';
 import { App, Routes, Reducers } from './index';
 
