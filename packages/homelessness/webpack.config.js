561feaf6ef29d82370a5735fd90b3a9bd12e3769
Michael Lange <dingoeatingfuzz@gmail.com>
2018-04-08 00:43:48 -0700

    Move common webpack code into a new package
    This simplifies the process for making a new product and guards against
    config drift.

--- packages/homelessness/webpack.config.js
+++ packages/homelessness/webpack.config.js
@@ -1,63 +1,6 @@
-const webpack = require('webpack');
-const {
-  createConfig,
-  entryPoint,
-  setOutput,
-  babel,
-  file,
-  css,
-  postcss,
-  match,
-  setEnv,
-  env,
-  devServer,
-  uglify,
-  addPlugins,
-  sourceMaps,
-} = require('webpack-blocks');
+const path = require('path');
+const { standard } = require('@hackoregon/webpack-common');
 
-const autoprefixer = require('autoprefixer');
-const { resolve } = require('path');
-
-const path = filePath => resolve(__dirname, filePath);
-
-const isProd = process.env.NODE_ENV === 'production';
-const entryPoints = [path('src/client')];
-
-if (!isProd) {
-  entryPoints.unshift('webpack-hot-middleware/client?reload=true');
-}
-
-module.exports = createConfig([
-  entryPoint(entryPoints),
-  setOutput({
-    path: path(isProd ? 'dist' : 'build'),
-    publicPath: '/',
-    filename: '[name].bundle.js',
-  }),
-  match(['*.js'], [
-    babel(),
-  ]),
-  match(['*.css'], [
-    css(),
-    postcss({
-      plugins: [autoprefixer({ browsers: ['last 2 versions'] })],
-    }),
-  ]),
-  match(['*.svg', '*.png', '*.gif', '*.jpg', '*.jpeg'], [
-    file(),
-  ]),
-  setEnv({
-    NODE_ENV: process.env.NODE_ENV,
-  }),
-  env('development', [
-    devServer(),
-    sourceMaps(),
-  ]),
-  env('production', [
-    uglify(),
-    addPlugins([
-      new webpack.LoaderOptionsPlugin({ minimize: true }),
-    ]),
-  ]),
-]);
+module.exports = standard({
+  outputPrefix: path.resolve(__dirname),
+});
---
+++ packages/webpack-common/index.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+const {
+  createConfig,
+  entryPoint,
+  setOutput,
+  babel,
+  file,
+  css,
+  postcss,
+  match,
+  setEnv,
+  env,
+  devServer,
+  uglify,
+  addPlugins,
+  sourceMaps,
+} = require('webpack-blocks');
+
+const autoprefixer = require('autoprefixer');
+const { resolve } = require('path');
+
+const path = filePath => resolve(__dirname, filePath);
+
+const isProd = process.env.NODE_ENV === 'production';
+const entryPoints = [];
+
+if (!isProd) {
+  entryPoints.unshift('webpack-hot-middleware/client?reload=true');
+}
+
+module.exports = {
+  standard(opts) {
+    const options = Object.assign({
+      entryPoint: './src/client',
+      outputPrefix: path('./'),
+    }, opts);
+    return createConfig([
+      entryPoint([...entryPoints, options.entryPoint]),
+      setOutput({
+        path: `${options.outputPrefix}${isProd ? '/dist' : '/build'}`,
+        publicPath: '/',
+        filename: '[name].bundle.js',
+      }),
+      babel(),
+      match(['*.css'], [
+        css(),
+        postcss({
+          plugins: [autoprefixer({ browsers: ['last 2 versions'] })],
+        }),
+      ]),
+      match(['*.svg', '*.png', '*.gif', '*.jpg', '*.jpeg'], [
+        file(),
+      ]),
+      setEnv({
+        NODE_ENV: process.env.NODE_ENV,
+      }),
+      env('development', [
+        devServer(),
+        sourceMaps(),
+      ]),
+      env('production', [
+        uglify(),
+        addPlugins([
+          new webpack.LoaderOptionsPlugin({ minimize: true }),
+        ]),
+      ]),
+    ]);
+  },
+};
