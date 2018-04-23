b9552d94d28385974efe6dd6cda3d588f04d81f4
Michael Lange <dingoeatingfuzz@gmail.com>
2018-04-23 11:34:03 -0700

    Update navigation

--- packages/2018/src/components/RootPage.js
+++ packages/2018/src/components/RootPage.js
@@ -3,12 +3,26 @@ import { connect } from 'react-redux';
 import Typekit from 'react-typekit';
 import { Header } from '@hackoregon/component-library';
 
+const menu = [
+  {
+    name: 'Collections',
+    path: '/',
+    nestedMenu: [
+      { name: 'Disaster Resilience', path: '/disaster' },
+      { name: 'Housing Affordability', path: '/housing' },
+      { name: 'Local Elections', path: '/elections' },
+      { name: 'Neighborhood Development', path: '/neighborhood' },
+      { name: 'Transportation Systems', path: '/transportation' },
+    ],
+  },
+];
+
 const isRoot = ({ pathname }) => pathname === '/';
 
 export const RootPage = props => (
   <div>
     <Typekit kitId="mbf2sam" />
-    <Header title="Civic" />
+    <Header title="Civic" menu={menu} />
     {props.children}
   </div>
 );
