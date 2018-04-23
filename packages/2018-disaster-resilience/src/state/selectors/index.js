e88473624957f99428cb08b6392193fb36e5662e
Michael Lange <dingoeatingfuzz@gmail.com>
2018-04-23 11:45:41 -0700

    Customize the default selectors

--- packages/2018-disaster-resilience/src/state/selectors/index.js
+++ packages/2018-disaster-resilience/src/state/selectors/index.js
@@ -2,11 +2,11 @@ import { createSelector } from 'reselect';
 
 // The root state for a package is namespaced in the 2018 package
 // and the root state object when developing a package in isolation
-export const appState = state => state.disaster || state;
+export const rootState = state => state.disaster || state;
 
 // Creating all selectors off the appState selector eliminates the need
 // to think about state namespacing in each component
 export const getSomeData = createSelector(
-  appState,
+  rootState,
   ({ someData }) => someData,
 );
