26dab789721f78d32aa4916dc5e6f29b60ccc54d
Colby Aley <colby@aley.me>
2018-05-06 11:28:19 -0700

    Integrate theme, title, subtitle, working line

--- packages/component-library/src/LineChart/LineChart.js
+++ packages/component-library/src/LineChart/LineChart.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import { css } from 'emotion';
 import {
   VictoryChart,
   VictoryLine,
@@ -6,41 +7,73 @@ import {
   VictoryAxis,
 } from 'victory';
 
+import CivicVictoryTheme from '../VictoryTheme/VictoryThemeIndex';
+
+const linechartWrapper = css`
+  margin: 0 auto;
+  max-width: 900px;
+  width: 100%;
+`;
+
+const titleStyle = css`
+  font-family: 'filson-soft', sans-serif;
+  font-size: 40px;
+  font-weight: 500;
+  text-align: center;
+  margin: 40px 0 12px 0;
+`;
+
+const subtitleStyle = css`
+  display: block;
+  font-family: 'filson-soft', sans-serif;
+  font-size: 18px;
+  text-align: center;
+`;
+
 const LineChart = ({
+  title,
+  subtitle,
   data,
   xLabel,
-  yLabel,
   dataKey,
   dataValue,
   dataKeyLabel,
-  categories,
 }) => (
-  <VictoryChart
-    domainPadding={20}
-    theme={VictoryTheme.material}
-  >
-    <VictoryAxis
-      label={xLabel}
-      tickValues={data.map(d => d[dataKey])}
-    />
-    <VictoryAxis
-      dependentAxis
-      label={yLabel}
-      tickValues={data.map(d => d[dataValue])}
-    />
-    <VictoryLine
-      data={data.map(d => ({ dataKey: d[dataKey], dataValue: d[dataValue] }))}
-      categories={{ x: categories }}
-      x="dataKey"
-      y="dataValue"
-    />
-  </VictoryChart>
+  <div>
+    { title ? <h3 className={titleStyle}>{title}</h3> : null}
+    { subtitle ? <span className={subtitleStyle}>{subtitle}</span> : null}
+    <div className={linechartWrapper}>
+      <VictoryChart
+        domainPadding={20}
+        animate={{ duration: 300 }}
+        theme={CivicVictoryTheme.civic}
+      >
+        <VictoryAxis
+          label={xLabel}
+          tickValues={data.map(d => d[dataKey])}
+          tickFormat={data.map(d => d[dataKeyLabel])}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={x => `${x}`}
+          tickValues={data.map(d => d[dataValue])}
+        />
+        <VictoryLine
+          data={data.map(d => ({ dataKey: d[dataKey], dataValue: d[dataValue] }))}
+          x="dataKey"
+          y="dataValue"
+        />
+      </VictoryChart>
+    </div>
+  </div>
 );
 
 LineChart.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
   xLabel: PropTypes.string,
   yLabel: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
   dataKey: PropTypes.string.isRequired,
   dataValue: PropTypes.string.isRequired,
   dataKeyLabel: PropTypes.string,
--- packages/component-library/stories/LineChart.story.js
+++ packages/component-library/stories/LineChart.story.js
@@ -17,19 +17,23 @@ const description = `
 export default () => storiesOf(displayName, module).addDecorator(withKnobs)
   .add('Basic Usage', (() => {
     const data = object('Data', [
-      { year: 2015, population: 12 },
-      { year: 2016, population: 13 },
-      { year: 2017, population: 14 },
+      { year: 2015, population: 120000 },
+      { year: 2016, population: 130000 },
+      { year: 2017, population: 200000 },
     ]);
     const categories = array('Categories', data.map(d => `${d.year}`))
     const dataKey = text('Data key', 'year');
     const xLabel = text('Data key', 'Year');
     const yLabel = text('Data key', 'Population');
     const dataValue = text('Data values', 'population');
-    const dataKeyLabel = text('Data key labels', 'label');
+    const dataKeyLabel = text('Data key labels', 'year');
+    const title = text('Title', 'Population');
+    const subtitle = text('Subtitle', 'I\'m a subtitle');
 
     return (
       <LineChart
+        title={title}
+        subtitle={subtitle}
         data={data}
         xLabel={xLabel}
         yLabel={yLabel}
