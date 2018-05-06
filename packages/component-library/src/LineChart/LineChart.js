6d0d71ce65695be6dcbb2d7bef56050316be3675
Colby Aley <colby@aley.me>
2018-05-06 11:06:42 -0700

    Checkpoint

--- packages/component-library/src/LineChart/LineChart.js
+++ packages/component-library/src/LineChart/LineChart.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import {
   VictoryChart,
   VictoryLine,
+  VictoryTheme,
+  VictoryAxis,
 } from 'victory';
 
-const LineChart = ({ data, dataKey }) => (
+const LineChart = ({
+  data,
+  xLabel,
+  yLabel,
+  dataKey,
+  dataValue,
+  dataKeyLabel,
+  categories,
+}) => (
   <VictoryChart
     domainPadding={20}
+    theme={VictoryTheme.material}
   >
+    <VictoryAxis
+      label={xLabel}
+      tickValues={data.map(d => d[dataKey])}
+    />
+    <VictoryAxis
+      dependentAxis
+      label={yLabel}
+      tickValues={data.map(d => d[dataValue])}
+    />
     <VictoryLine
-      data={ data }
+      data={data.map(d => ({ dataKey: d[dataKey], dataValue: d[dataValue] }))}
+      categories={{ x: categories }}
+      x="dataKey"
+      y="dataValue"
     />
   </VictoryChart>
 );
+
+LineChart.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  xLabel: PropTypes.string,
+  yLabel: PropTypes.string,
+  dataKey: PropTypes.string.isRequired,
+  dataValue: PropTypes.string.isRequired,
+  dataKeyLabel: PropTypes.string,
+  dataValueLabel: PropTypes.string,
+};
+
+LineChart.defaultProps = {
+  data: [],
+};
 
 export default LineChart;
--- packages/component-library/stories/LineChart.story.js
+++ packages/component-library/stories/LineChart.story.js
@@ -14,27 +14,32 @@ const description = `
   This is some basic usage with the button with providing a label to show the text.
   Clicking should trigger an action.`;
 
-// const propDocs = { inline: true, propTables: [LineChart] };
-
 export default () => storiesOf(displayName, module).addDecorator(withKnobs)
   .add('Basic Usage', (() => {
-    const data = object('Data',[
-      { x: 0, y: 0 },
-      { x: 2, y: 2 },
-      { x: 4, y: 2 },
-      { x: 8, y: 3 },
+    const data = object('Data', [
+      { year: 2015, population: 12 },
+      { year: 2016, population: 13 },
+      { year: 2017, population: 14 },
     ]);
-    const dataKey = text('Data key', 'sortOrder');
+    const categories = array('Categories', data.map(d => `${d.year}`))
+    const dataKey = text('Data key', 'year');
+    const xLabel = text('Data key', 'Year');
+    const yLabel = text('Data key', 'Population');
     const dataValue = text('Data values', 'population');
     const dataKeyLabel = text('Data key labels', 'label');
 
-    return (<LineChart 
-      data={data}
-      dataKey={dataKey}
-      dataValue={dataValue}
-      dataKeyLabel={dataKeyLabel}
-    />);
+    return (
+      <LineChart
+        data={data}
+        xLabel={xLabel}
+        yLabel={yLabel}
+        categories={categories}
+        dataKey={dataKey}
+        dataValue={dataValue}
+        dataKeyLabel={dataKeyLabel}
+      />
+    );
   }))
   .add('no props', (() => (
     <LineChart />
-  )))
+  )));
