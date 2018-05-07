a1b792abbd4b7ccb08983bc6cfe3a2cc582cd949
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2018-05-07 15:28:46 -0700

    WIP: Adding legend

--- packages/component-library/src/Scatterplot/Scatterplot.js
+++ packages/component-library/src/Scatterplot/Scatterplot.js
@@ -3,7 +3,9 @@ import {
   VictoryAxis,
   VictoryChart,
   VictoryContainer,
+  VictoryCursorContainer,
   VictoryLabel,
+  VictoryLegend,
   VictoryScatter,
   VictoryTooltip,
   VictoryVoronoiTooltip,
@@ -27,12 +29,6 @@ const ScatterPlot = ({ data, domain, style, title }) => {
     ],
   };
 
-  const barchartWrapper = css`
-    margin: 0 auto;
-    max-width: 900px;
-    width: 100%;
-  `;
-
   const titleStyle = css`
     display: block;
     font-family: 'Roboto Condensed', 'Helvetica Neue', Helvetica, sans-serif;
@@ -50,6 +46,14 @@ const ScatterPlot = ({ data, domain, style, title }) => {
     margin: 0;
   `;
 
+  const legendStyle = css`
+    font-family: 'Roboto Condensed', 'Helvetica Neue', Helvetica, sans-serif;
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center;
+    margin: 10px 0 -40px 0;
+  `;
+
   const axisLabelStyle = {
     fontFamily: "'Roboto Condensed', 'Helvetica Neue', Helvetica, sans-serif",
     fontSize: '14px',
@@ -62,15 +66,55 @@ const ScatterPlot = ({ data, domain, style, title }) => {
 
   const CustomFlyout = flyoutProps => {
     const { datum, x, y, orientation } = flyoutProps;
-    console.log('flyoutProps', flyoutProps);
+    console.log('🐋🐋', CivicVictoryTheme.civic.legend);
     const newY = y - 25;
     // const newY = orientation === 'top' ? y - 25 : y + 25;
     return (
-      <g>
-        <circle cx={x} cy={newY} r="20" stroke="tomato" fill="none" />
-        <circle cx={x} cy={newY} r="25" stroke="orange" fill="none" />
-        <circle cx={x} cy={newY} r="30" stroke="gold" fill="none" />
+      <g transform={`translate(${x}, ${y})`} style={{ backgroundColor: 'red' }}>
+        <text textAnchor="middle">test</text>
+        <rect
+          x={x}
+          y={y}
+          width="50px"
+          height="50px"
+          stroke="tomato"
+          fill="red"
+        />
+        <circle r="25" stroke="orange" fill="none" />
+        <circle r="30" stroke="gold" fill="none" />
       </g>
+    );
+  };
+
+  const CustomLegend = data => {
+    const test = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
+    return (
+      <legend className={legendStyle}>
+        {test.map((group, idx) => (
+          <span
+            key={group}
+            className={css`
+              margin-left: 10px;
+            `}
+          >
+            <svg viewBox="0 0 10 10" width="10px">
+              <circle
+                cx="5"
+                cy="5"
+                r="5"
+                fill={CivicVictoryTheme.civic.group.colorScale[idx]}
+              />
+            </svg>
+            <span
+              className={css`
+                margin-left: 5px;
+              `}
+            >
+              {group}
+            </span>
+          </span>
+        ))}
+      </legend>
     );
   };
 
@@ -78,12 +122,45 @@ const ScatterPlot = ({ data, domain, style, title }) => {
     <div>
       {title ? <span className={titleStyle}>{title}</span> : null}
       {subtitle ? <span className={subtitleStyle}>{subtitle}</span> : null}
-      {/* <div className={barchartWrapper}>
-        <VictoryChart domain={chartDomain} theme={CivicVictoryTheme.civic}>
-          <VictoryScatter style={style} size={5} data={data} />
-        </VictoryChart>
-      </div> */}
+      <CustomLegend />
+
       <VictoryChart domain={chartDomain} theme={CivicVictoryTheme.civic}>
+        {/* <VictoryLegend
+          {...CivicVictoryTheme.civic.legend}
+          title="banana"
+          height={50}
+          // x={650 / 2 - 450 / 2}
+          containerComponent={
+            // Center the Legend
+            <VictoryContainer
+              style={{
+                width: 'fit-content',
+                margin: '0 auto',
+              }}
+            />
+          }
+          // width="auto"
+          centerTitle
+          orientation="horizontal"
+          symbolSpacer={7}
+          gutter={15}
+          data={[
+            { name: 'One' },
+            { name: 'Two' },
+            { name: 'Three' },
+            { name: 'Three' },
+          ]}
+          style={{
+            border: { stroke: 'black' },
+            margin: '0 auto',
+            labels: {
+              fontSize: 14,
+              fontWeight: 'bold',
+              fontFamily:
+                "'Roboto Condensed', 'Helvetica Neue', Helvetica, sans-serif",
+            },
+          }}
+        /> */}
         <VictoryAxis
           style={{ grid: { stroke: 'none' } }}
           animate={{ onEnter: { duration: 500 } }}
@@ -92,7 +169,11 @@ const ScatterPlot = ({ data, domain, style, title }) => {
           dependentAxis
           style={{
             grid: {
-              stroke: t => (t < chartDomain.y[1] ? 'lightgray' : 'none'),
+              ...CivicVictoryTheme.civic.axis.style.grid,
+              stroke: t =>
+                t < chartDomain.y[1]
+                  ? CivicVictoryTheme.civic.axis.style.grid.stroke
+                  : 'none',
             },
           }}
           labelComponent={<VictoryLabel text="banana" />}
@@ -116,15 +197,21 @@ const ScatterPlot = ({ data, domain, style, title }) => {
           style={axisLabelStyle}
           text={yLabel}
         />
+        {/* TODO: Pull this into it's own component */}
         <VictoryScatter
           style={style}
-          size={5}
+          size={4}
           data={data}
           labels={datum => `${xLabel}: ${datum.x} ${yLabel}: ${datum.y}`}
           labelComponent={
-            <VictoryTooltip x={325} y={50} flyoutComponent={<CustomFlyout />} />
+            <VictoryTooltip
+              x={325}
+              y={50}
+              flyoutComponent={<CustomFlyout style={{ background: 'red' }} />}
+            />
           }
-          animate={{ onEnter: { duration: 500 } }}
+          animate={{ onEnter: { duration: 500 }, onLoad: { duration: 500 } }}
+          // categories={{ x: ['x name', 'y name'] }}
         />
       </VictoryChart>
     </div>
