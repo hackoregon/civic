c48228298b5790a50fad7e44c79da4ccbd9ea576
David <mendoza.line@aim.com>
2018-05-18 21:45:47 +0000

    Use JSX to create deck.gl layer

--- packages/component-library/src/ScatterPlotMap/ScatterPlotMap.js
+++ packages/component-library/src/ScatterPlotMap/ScatterPlotMap.js
@@ -6,8 +6,8 @@ const ScatterPlotMap = (props) => {
     viewport,
     data,
     getPosition,
-    getFillColor,
     opacity,
+    getColor,
     getRadius,
     radiusScale,
     outline,
@@ -16,56 +16,57 @@ const ScatterPlotMap = (props) => {
     onLayerClick,
   } = props;
 
-  const layers = [
-    new ScatterplotLayer({
-      id: 'scatterplot-layer',
-      data: data,
-      getPosition: getPosition,
-      pickable: true,
-      getColor: getFillColor,
-      opacity: opacity,
-      getRadius: getRadius,
-      radiusScale: radiusScale,
-      radiusMinPixels: 1,
-      radiusMaxPixels: 500,
-      outline: outline,
-      strokeWidth: strokeWidth,
-      autoHighlight: autoHighlight,
-      onClick: onLayerClick,
-      parameters: {
-        depthTest: false,
-      },
-    }),
-  ];
-
   return (
     <div>
       <DeckGL
-          {...viewport}
-          layers={layers}
-      />
+        className={'DeckGL'}
+        {...viewport}
+      >
+        <ScatterplotLayer
+          className={'ScatterPlotMap'}
+          id={'scatterplot-layer'}
+          pickable={true}
+          data={data}
+          getPosition={getPosition}
+          opacity={opacity}
+          getColor={getColor}
+          getRadius={getRadius}
+          radiusScale={radiusScale}
+          radiusMinPixels={1}
+          radiusMaxPixels={500}
+          outline={outline}
+          strokeWidth={strokeWidth}
+          autoHighlight={autoHighlight}
+          onClick={onLayerClick}
+          parameters={{depthTest: false}}
+        />
+      </DeckGL>
     </div>
   );
 };
 
 ScatterPlotMap.propTypes = {
   viewport: PropTypes.object,
-  data: PropTypes.array.isRequired,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
   getPosition: PropTypes.func,
-  getColor: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.func,
-  ]),
   opacity: PropTypes.number,
-  getRadius: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.func,
-    ]),
+  getColor: PropTypes.func,
+  getRadius: PropTypes.func,
   radiusScale: PropTypes.number,
   outline: PropTypes.bool,
   strokeWidth: PropTypes.number,
   autoHighlight: PropTypes.bool,
   onLayerClick: PropTypes.func,
+};
+
+ScatterPlotMap.defaultProps = {
+  getPosition: d => d.geometry.coordinates,
+  opacity: 0.8,
+  getColor: d => [0,0,0],
+  getRadius: d => 50,
+  radiusScale: 1,
+  outline: false,
+  strokeWidth: 1,
 };
 
 export default ScatterPlotMap;
