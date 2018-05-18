82bef07d2d8ffc05e66bef5b5d3f69906ccaf4ca
David <mendoza.line@aim.com>
2018-05-18 20:36:31 +0000

    Use JSX to create deck.gl layer

--- packages/component-library/src/ScreenGridMap/ScreenGridMap.js
+++ packages/component-library/src/ScreenGridMap/ScreenGridMap.js
@@ -1,4 +1,4 @@
- import React, { PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import DeckGL, { ScreenGridLayer } from 'deck.gl';
 
 const ScreenGridMap = (props) => {
@@ -13,42 +13,47 @@ const ScreenGridMap = (props) => {
     onLayerClick,
   } = props;
 
-  const layers = [
-    new ScreenGridLayer({
-      id: 'screengrid-layer',
-      pickable: true,
-      data: data,
-      getPosition: getPosition,
-      opacity: opacity,
-      colorRange: colorRange,
-      cellSizePixels: cellSizePixels,
-      autoHighlight: autoHighlight,
-      onClick: onLayerClick,
-      updateTriggers: {
-        instanceColors: colorRange,
-      },
-    }),
-  ];
-
   return (
     <div>
       <DeckGL
-          {...viewport}
-          layers={layers}
-      />
+        className={'DeckGL'}
+        {...viewport}
+      >
+        <ScreenGridLayer
+          id={'screengrid-layer'}
+          className={'ScreenGridMap'}
+          pickable={true}
+          data={data}
+          getPosition={getPosition}
+          opacity={opacity}
+          colorRange={colorRange}
+          cellSizePixels={cellSizePixels}
+          autoHighlight={autoHighlight}
+          onClick={onLayerClick}
+          updateTriggers={{instanceColors: colorRange}}
+        />
+      </DeckGL>
     </div>
   );
 };
 
 ScreenGridMap.propTypes = {
   viewport: PropTypes.object,
-  data: PropTypes.array.isRequired,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
   getPosition: PropTypes.func,
   opacity: PropTypes.number,
   colorRange: PropTypes.array,
   cellSizePixels: PropTypes.number,
   autoHighlight: PropTypes.bool,
   onLayerClick: PropTypes.func,
+};
+
+ScreenGridMap.defaultProps = {
+  getPosition: d => d.geometry.coordinates,
+  opacity: 0.8,
+  colorRange: [[255,255,204],[161,218,180],[65,182,196],[34,94,168]],
+  cellSizePixels: 25,
+  autoHighlight: true,
 };
 
 export default ScreenGridMap;
