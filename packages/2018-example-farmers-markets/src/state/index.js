4513e4f6db600a1c21f2dbb7acb92d3e3085ff26
Michael Lange <dingoeatingfuzz@gmail.com>
2018-06-02 13:43:51 -0700

    Register farmers markets over time reducer

--- packages/2018-example-farmers-markets/src/state/index.js
+++ packages/2018-example-farmers-markets/src/state/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import farmersMarketsOverTime from './farmers-markets-over-time';
 
 export default function createReducer(asyncReducers) {
   return combineReducers({
     routing: routerReducer,
+    farmersMarketsOverTime,
     ...asyncReducers,
   });
 }
