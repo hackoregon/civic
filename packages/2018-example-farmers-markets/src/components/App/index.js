965e00c49224cb6c6325619b510abb5fada0f156
Michael Lange <dingoeatingfuzz@gmail.com>
2018-06-02 21:42:57 -0700

    Finishing commentary for the Farmers' Market example collection

--- packages/2018-example-farmers-markets/src/components/App/index.js
+++ packages/2018-example-farmers-markets/src/components/App/index.js
@@ -13,7 +13,7 @@ const App = () => (
     heroSubtitle="A look at Farmers' Markets in Portland and nationally"
     mainProjectColor="#5F9B4A"
   >
-    <h1>We’ve Hit Peak Farmers Market</h1>
+    <h1>Have We Hit Peak Farmers' Market?</h1>
 
     <p>
       There could be some stagnation in the number of new people buying local food.
@@ -24,7 +24,7 @@ const App = () => (
       There were 8,268 farmers’ markets operating in 2014, up 180 percent since 2006.
     </p>
 
-    <PullQuote quoteText="There were 8,268 farmers' markets operating in 2014, up 180% since 2006"/>
+    <PullQuote quoteText="There were 8,268 farmers' markets operating in 2014, up 180% since 2006" />
 
     <p>
       “In densely populated urban areas, farmers’ markets often compete with each other
@@ -50,7 +50,23 @@ const App = () => (
 
     <h2>How does Portland look today?</h2>
 
+    <p>
+      Portland has a nation-wide reputation for being progressive consumers. This label holds up
+      when you consider how many farmers' markets are available to Portlanders. It's clear that
+      there is something about this city that attracts grassroot and local initiatives.
+    </p>
+
     <PortlandFarmersMarkets />
+
+    <h2>There is room for more</h2>
+
+    <p>
+      If complete saturation of farmers' markets means having an accessible farmers' market
+      from every neighborhood in the city, then we have progress still to make. Portlanders
+      living in the innermost (and also most expensive) neighborhoods have their fill of
+      options, but neighborhoods on the outskirts of the city and in the suburbs need to
+      commute for the same options.
+    </p>
 
   </PageLayout>
 );
