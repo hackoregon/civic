cb7ab94db22a21c4b6c7ef06761b26b00c227d01
Michael Lange <dingoeatingfuzz@gmail.com>
2018-06-05 10:23:11 -0700

    Scaffold housing affordability story cards

---
+++ packages/2018-housing-affordability/src/components/PortlandNeedsAffordableRentalUnits/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { CivicStoryCard, Placeholder } from '@hackoregon/component-library';
+
+export class PortlandNeedsAffordableRentalUnits extends React.Component {
+  componentDidMount() {
+    // initialize data here
+  }
+
+  render() {
+    return (
+      <CivicStoryCard title="Portland Needs Affordable Rental Units">
+        <Placeholder />
+      </CivicStoryCard>
+    );
+  }
+}
+
+// Connect this to the redux store when necessary
+export default PortlandNeedsAffordableRentalUnits;
---
+++ packages/2018-housing-affordability/src/components/WhoCanAffordToBuyAHome/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { CivicStoryCard, Placeholder } from '@hackoregon/component-library';
+
+export class WhoCanAffordToBuyAHome extends React.Component {
+  componentDidMount() {
+    // initialize data here
+  }
+
+  render() {
+    return (
+      <CivicStoryCard title="Who Can Afford to Buy a Home in America?">
+        <Placeholder />
+      </CivicStoryCard>
+    );
+  }
+}
+
+// Connect this to the redux store when necessary
+export default WhoCanAffordToBuyAHome;
