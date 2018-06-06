ccf5723f9d3cd43588d408932c1585fd0dba8afc
Jaron Heard <jaronheard@gmail.com>
2018-06-06 16:32:42 -0700

    add issue functionality to placeholder

--- packages/component-library/stories/Placeholder.story.js
+++ packages/component-library/stories/Placeholder.story.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Placeholder } from '../src';
+import { number, withKnobs } from '@storybook/addon-knobs';
 import { checkA11y } from '@storybook/addon-a11y';
+import { Placeholder } from '../src';
+
+
+const sampleIssue = 56;
 
 export default () => storiesOf('Placeholder')
   .addDecorator(checkA11y)
+  .addDecorator(withKnobs)
   .add('default', () => <Placeholder />)
-  .add('with a custom message', () => (
-    <Placeholder>
-      <h1>Why is this still here?</h1>
-      <p>Shouldn't someone have done something by now?</p>
-    </Placeholder>
-  ));
+  .add('with an issue', () => {
+    const issue = number('Issue', sampleIssue);
+
+    return (<Placeholder issue={issue} />);
+  })
+  .add('with a custom message', () => {
+    return (
+      <Placeholder>
+        <h1>Why is this still here?</h1>
+        <p>Shouldn't someone have done something by now?</p>
+      </Placeholder>
+    );
+  })
+;
