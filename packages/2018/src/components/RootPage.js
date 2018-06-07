d451be3a623316d20629a28a3bcafb9b3d3c07ed
Daniel Tomás Ku <danieltomasku@gmail.com>
2018-06-07 09:29:52 -0700

    Added menu to 2017 package, cleanup

--- packages/2018/src/components/RootPage.js
+++ packages/2018/src/components/RootPage.js
@@ -1,25 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Header } from '@hackoregon/component-library';
-
-const menu = [
-  {
-    name: 'Collections',
-    path: '/',
-    nestedMenu: [
-      { name: 'Disaster Resilience', path: '/disaster' },
-      { name: 'Housing Affordability', path: '/housing' },
-      { name: 'Local Elections', path: '/elections' },
-      { name: 'Neighborhood Development', path: '/neighborhood' },
-      { name: 'Transportation Systems', path: '/transportation' },
-      { name: 'EXAMPLE Farmers Markets', path: '/farmers-markets' },
-    ],
-  },
-  {
-    name: 'Sandbox',
-    path: '/sandbox',
-  },
-];
 
 const isRoot = ({ pathname }) => pathname === '/';
 
