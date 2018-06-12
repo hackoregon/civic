f497b6627a3da938ffc9578e22a7601c71f1a132
jasonleonhard <devbrights@gmail.com>
2018-06-11 20:38:10 -0700

    add more tests for HexOverlay
    Co-authored-by: Doren Proctor <dorenproctor@gmail.com>
    Co-authored-by: Jason Leonhard <devbrights@gmail.com>

--- packages/component-library/src/HexOverlay/HexOverlay.test.js
+++ packages/component-library/src/HexOverlay/HexOverlay.test.js
@@ -33,7 +33,6 @@ describe('HexOverlay', () => {
   const wrapper = shallow( <HexOverlay {...defaultProps} />);
 
   it('should render a DeckGLOverlay component', () => {
-    const wrapper = shallow(<HexOverlay {...defaultProps} />);
     expect(wrapper.find('.HexOverlay')).to.have.length(1);
   });
 
@@ -43,5 +42,29 @@ describe('HexOverlay', () => {
 
   it('should render with the same class name', () => {
     expect(wrapper.find('.HexOverlay')).length(1)
+  });
+
+  it('should render with extruded', () => {
+    expect(wrapper.props().children.props.layers[0].props.extruded).to.equal(true)
+  });
+
+  it('should render with radius', () => {
+    expect(wrapper.props().children.props.layers[0].props.radius).to.equal(200)
+  });
+
+  it('should render with opacity of 0.8', () => {
+    expect(wrapper.props().children.props.layers[0].props.opacity).to.equal(0.8)
+  });
+
+  it('should render with an extrusion', () => {
+    expect(wrapper.props().children.props.layers[0].props.extruded).to.equal(true)
+  });
+
+  it('should render with data', () => {
+    expect(wrapper.props().children.props.layers[0].props.data).to.equal(data)
+  });
+
+  it('should render with an type string', () => {
+    expect(wrapper.props().children.props.layers[0].props.data[0].type).to.equal("Feature")
   });
 });
