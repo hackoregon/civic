22c018b6caffa24b05f034cbbcffa2c0d28e0f42
David <mendoza.line@aim.com>
2018-06-15 03:46:01 +0000

    Import CivicDashboard story

--- packages/component-library/stories/index.js
+++ packages/component-library/stories/index.js
@@ -28,6 +28,7 @@ import pathMapStory from './PathMap.story';
 import iconMapStory from './IconMap.story';
 import boundaryMapStory from './BoundaryMap.story';
 import civicSandboxMapStory from './CivicSandboxMap.story';
+import civicDashboardStory from './CivicDashboard.story';
 import { checkA11y } from '@storybook/addon-a11y';
 import '../assets/global.styles.css';
 
@@ -66,3 +67,4 @@ mapOverlayStory();
 hexOverlayStory();
 boundaryMapStory();
 civicSandboxMapStory();
+civicDashboardStory();
