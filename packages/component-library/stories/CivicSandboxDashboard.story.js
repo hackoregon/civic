ac3dcff304ca84b07ecf5dc56610d8b942e5dae9
David <mendoza.line@aim.com>
2018-06-16 19:44:24 +0000

    Simplify CivicSandboxDashboard story

--- packages/component-library/stories/CivicSandboxDashboard.story.js
+++ packages/component-library/stories/CivicSandboxDashboard.story.js
@@ -23,12 +23,8 @@ class LoadData extends React.Component {
     super(props);
     this.state = {
       foundation1: null,
-      foundation2: null,
-      foundation3: null,
       slide1: null,
       slide2: null,
-      slide3: null,
-      slide4: null,
       error: null,
     };
   }
@@ -39,31 +35,23 @@ class LoadData extends React.Component {
       .defer(d3.json, this.props.urls[0])
       .defer(d3.json, this.props.urls[1])
       .defer(d3.json, this.props.urls[2])
-      .defer(d3.json, this.props.urls[3])
-      .defer(d3.json, this.props.urls[4])
-      .defer(d3.json, this.props.urls[5])
-      .defer(d3.json, this.props.urls[6])
-      .await((error, foundation1, foundation2, foundation3, slide1, slide2, slide3, slide4) => {
+      .await((error, foundation1, slide1, slide2) => {
         if (error) { return this.setState({error: error}) }
-        cmp.setState({foundation1, foundation2, foundation3, slide1, slide2, slide3, slide4});
+        cmp.setState({foundation1, slide1, slide2});
       });
   }
 
   render() {
     if (this.state.data1 === null) { return null }
-    const { foundation1, foundation2, foundation3, slide1, slide2, slide3, slide4 } = this.state;
-    return this.props.children(foundation1, foundation2, foundation3, slide1, slide2, slide3, slide4);
+    const { foundation1, slide1, slide2 } = this.state;
+    return this.props.children(foundation1, slide1, slide2);
   }
 }
 
 const dataURLs = [
-  'https://gist.githubusercontent.com/mendozaline/f78b076ce13a9fd484f6b8a004065a95/raw/ff8bd893ba1890a6f6c20265f720587f9595a9c4/property-values.json',
   'https://gist.githubusercontent.com/mendozaline/f78b076ce13a9fd484f6b8a004065a95/raw/ff8bd893ba1890a6f6c20265f720587f9595a9c4/pop.json',
-  'https://gist.githubusercontent.com/mendozaline/f78b076ce13a9fd484f6b8a004065a95/raw/ff8bd893ba1890a6f6c20265f720587f9595a9c4/children.json',
-  'https://gist.githubusercontent.com/mendozaline/b3a75b40c9a60781b6adc77cebb9b400/raw/fa0aa13c75bfcc2fd92ccf1f3cc612af83d5d704/005-gardens.json',
   'https://gist.githubusercontent.com/mendozaline/b3a75b40c9a60781b6adc77cebb9b400/raw/fa0aa13c75bfcc2fd92ccf1f3cc612af83d5d704/002-bike-lanes.json',
   'https://gist.githubusercontent.com/mendozaline/b3a75b40c9a60781b6adc77cebb9b400/raw/fa0aa13c75bfcc2fd92ccf1f3cc612af83d5d704/010-grocery.json',
-  'https://gist.githubusercontent.com/mendozaline/b3a75b40c9a60781b6adc77cebb9b400/raw/fa0aa13c75bfcc2fd92ccf1f3cc612af83d5d704/017-building-permits.json',
 ];
 
 export default () => storiesOf(displayName, module)
@@ -72,7 +60,7 @@ export default () => storiesOf(displayName, module)
   .add('Simple usage', () => (
     <LoadData urls={dataURLs}>
       {
-        (foundation1, foundation2, foundation3, slide1, slide2, slide3, slide4) => {
+        (foundation1, slide1, slide2) => {
           if (foundation1 === null) { return null }
 
           const mapboxToken = 'pk.eyJ1IjoidGhlbWVuZG96YWxpbmUiLCJhIjoiY2o1aXdoem1vMWtpNDJ3bnpqaGF1bnlhNSJ9.sjTrNKLW9daDBIGvP3_W0w';
@@ -102,24 +90,8 @@ export default () => storiesOf(displayName, module)
             'Earth': '[[255,247,251,255],[236,226,240,255],[208,209,230,255],[166,189,219,255],[103,169,207,255],[54,144,192,255],[2,129,138,255],[1,108,89,255],[1,70,54,255]]',
             'Ocean': '[[255,255,217,255],[237,248,177,255],[199,233,180,255],[127,205,187,255],[65,182,196,255],[29,145,192,255],[34,94,168,255],[37,52,148,255],[8,29,88,255]]',
           };
-          const colorScheme = selectV2('Color Schemes:', colorOptions, colorOptions['Earth']);
+          const colorScheme = selectV2('Color Schemes:', colorOptions, colorOptions['Ocean']);
           const colorSchemeArray = JSON.parse(colorScheme);
-
-          const screenGridcolorScheme = selectV2('ScreenGrid Color Schemes:', colorOptions, colorOptions['Thermal']);
-          const screenGridcolorSchemeArray = JSON.parse(screenGridcolorScheme);
-
-          const propertyValueGetColor = f => {
-            const propertyValue = parseFloat(f.properties.prop_value); 
-            return propertyValue < 250000 ? colorSchemeArray[0] :
-              propertyValue < 500000 ? colorSchemeArray[1] :
-              propertyValue < 750000 ? colorSchemeArray[2] :
-              propertyValue < 1000000 ? colorSchemeArray[3] :
-              propertyValue < 1250000 ? colorSchemeArray[4] :
-              propertyValue < 1500000 ? colorSchemeArray[5] :
-              propertyValue < 1750000 ? colorSchemeArray[6] :
-              propertyValue < 2000000 ? colorSchemeArray[7] :
-              colorSchemeArray[8];
-          };
 
           const populationGetColor = f => {
             const population = parseFloat(f.properties.total_population); 
@@ -134,113 +106,35 @@ export default () => storiesOf(displayName, module)
               colorSchemeArray[8];
           };
 
-          const householdChildrenGetColor = f => {
-            const householdChildren = parseFloat(f.properties.pc_household_with_children_under_18); 
-            return householdChildren < 0.04 ? colorSchemeArray[0] :
-              householdChildren < 0.08 ? colorSchemeArray[1] :
-              householdChildren < 0.12 ? colorSchemeArray[2] :
-              householdChildren < 0.16 ? colorSchemeArray[3] :
-              householdChildren < 0.2 ? colorSchemeArray[4] :
-              householdChildren < 0.24 ? colorSchemeArray[5] :
-              householdChildren < 0.28 ? colorSchemeArray[6] :
-              householdChildren < 0.32 ? colorSchemeArray[7] :
-              colorSchemeArray[8];
-          };
-
-          const foundations = {
-            '006-property-values': {
-              mapType: 'ChoroplethMap',
-              id: 'choropleth-layer-foundation-property-values',
-              pickable: true,
-              data: foundation1.slide_data.features,
-              opacity: opacity,
-              getPolygon: f => f.geometry.coordinates,
-              getLineColor: f => [0,0,0,255],
-              getLineWidth: f => 50,
-              stroked: true,
-              getFillColor: propertyValueGetColor,
-              filled: true,
-              onClick: info => action('Layer clicked:', { depth: 2 })(info, info.object),
-              autoHighlight: true,
-              highlightColor: [200,200,200,150],
-              updateTriggers: {getFillColor: propertyValueGetColor},
-            },
-            '007-population': {
-              mapType: 'ChoroplethMap',
-              id: 'choropleth-layer-foundation-population',
-              pickable: true,
-              data: foundation2.slide_data.features,
-              opacity: opacity,
-              getPolygon: f => f.geometry.coordinates,
-              getLineColor: f => [0,0,0,255],
-              getLineWidth: f => 50,
-              stroked: true,
-              getFillColor: populationGetColor,
-              filled: true,
-              onClick: info => action('Layer clicked:', { depth: 2 })(info, info.object),
-              autoHighlight: true,
-              highlightColor: [200,200,200,150],
-              updateTriggers: {getFillColor: populationGetColor},
-            },
-            '015-household-children': {
-              mapType: 'ChoroplethMap',
-              id: 'choropleth-layer-foundation-household-children',
-              pickable: true,
-              data: foundation3.slide_data.features,
-              opacity: opacity,
-              getPolygon: f => f.geometry.coordinates,
-              getLineColor: f => [255,255,255,255],
-              getLineWidth: f => 50,
-              stroked: true,
-              getFillColor: householdChildrenGetColor,
-              filled: true,
-              onClick: info => action('Layer clicked:', { depth: 2 })(info, info.object),
-              autoHighlight: true,
-              highlightColor: [200,200,200,150],
-              updateTriggers: {getFillColor: householdChildrenGetColor},
-            },
+          const foundation = {
+            mapType: 'ChoroplethMap',
+            id: 'choropleth-layer-foundation-population',
+            pickable: true,
+            data: foundation1.slide_data.features,
+            opacity: opacity,
+            getPolygon: f => f.geometry.coordinates,
+            getLineColor: f => [0,0,0,255],
+            getLineWidth: f => 50,
+            stroked: true,
+            getFillColor: populationGetColor,
+            filled: true,
+            onClick: info => action('Layer clicked:', { depth: 2 })(info, info.object),
+            autoHighlight: true,
+            highlightColor: [200,200,200,150],
+            updateTriggers: {getFillColor: populationGetColor},
           };
 
           //SLIDES
-          //005 Community Gardens
-          const gardensBoundary = {
-            mapType: 'PolygonPlotMap',
-            id: 'boundary-layer-gardens-slide',
-            data: slide1.slide_meta.boundary,
-            opacity: 1,
-            filled: false,
-            getPolygon: f => f.coordinates,
-            getLineColor: f => [25,183,170,255],
-            getLineWidth: f => 45,
-            lineWidthScale: 1,
-            lineJointRounded: false,
-          };
-          const gardensMap = {
-            mapType: 'SmallPolygonMap',
-            id: 'polygon-layer-gardens-slide',
-            pickable: true,
-            data: slide1.slide_data.features,
-            opacity: 1,
-            getPolygon: f => f.geometry.coordinates,
-            getLineColor: f => [25,183,170,255],
-            getLineWidth: f => 50,
-            stroked: true,
-            getFillColor: f => [25,183,170,255],
-            filled: true,
-            autoHighlight: true,
-            highlightColor: [100,100,100,255],
-          };
-
           //002 Bike Lanes
           const bikeLanesBoundary = {
             mapType: 'PolygonPlotMap',
             id: 'boundary-layer-bike-lanes',
-            data: slide2.slide_meta.boundary,
+            data: slide1.slide_meta.boundary,
             opacity: 1,
             filled: false,
             getPolygon: f => f.coordinates,
             getLineColor: f => [220,69,86,255],
-            getLineWidth: f => 45,
+            getLineWidth: f => 55,
             lineWidthScale: 1,
             lineJointRounded: false,
           };
@@ -248,7 +142,7 @@ export default () => storiesOf(displayName, module)
             mapType: 'PathMap',
             id: 'path-layer-bike-lanes',
             pickable: true,
-            data: slide2.slide_data.features,
+            data: slide1.slide_data.features,
             opacity: 1,
             getColor: f => [220,69,86,255],
             getPath: f => f.geometry.coordinates,
@@ -262,7 +156,7 @@ export default () => storiesOf(displayName, module)
           const groceryBoundary = {
             mapType: 'PolygonPlotMap',
             id: 'boundary-layer-grocery',
-            data: slide3.slide_meta.boundary,
+            data: slide2.slide_meta.boundary,
             opacity: 1,
             filled: false,
             getPolygon: f => f.coordinates,
@@ -275,7 +169,7 @@ export default () => storiesOf(displayName, module)
             mapType: 'ScatterPlotMap',
             id: 'scatterplot-layer-grocery',
             pickable: true,
-            data: slide3.slide_data.features,
+            data: slide2.slide_data.features,
             getPosition: f => f.geometry.coordinates,
             opacity: 0.9,
             getColor: f => [138,43,226,255],
@@ -287,41 +181,13 @@ export default () => storiesOf(displayName, module)
             highlightColor: [200,200,200,255],
           };
 
-          //017 Building Permits
-          const buildingPermitsMap = {
-            mapType: 'ScreenGridMap',
-            id: 'screengrid-layer-building-permits',
-            pickable: true,
-            data: slide4.slide_data.features,
-            getPosition: f => f.geometry.coordinates,
-            opacity: 0.75,
-            colorRange: screenGridcolorSchemeArray,
-            cellSizePixels: 40,
-            autoHighlight: true,
-            highlightColor: [200,200,200,150],
-            updateTriggers: {instanceColors: screenGridcolorSchemeArray},
-          };
-
-          const foundationOptions = {
-            "Property Values": "006-property-values",
-            "Population": "007-population",
-            "Households with Children": "015-household-children",
-          };
-          const foundationSelected = selectV2("Foundations:", foundationOptions, foundationOptions["Population"]);
-
-          const buildingPermitsSlideVisible = boolean('Building Permits:', false);
           const bikeLanesSlideVisible = boolean('Bike Lanes:', true);
-          const gardensSlideVisible = boolean('Community Gardens:', true);
           const grocerySlideVisible = boolean('Grocery Stores:', true);
 
           const allMapLayers = [
             {
-              "data": foundations[foundationSelected],
+              "data": foundation,
               "visible": true,
-            },
-            {
-              "data": buildingPermitsMap,
-              "visible": buildingPermitsSlideVisible,
             },
             {
               "data": bikeLanesBoundary,
@@ -330,14 +196,6 @@ export default () => storiesOf(displayName, module)
             {
               "data": bikeLanesMap,
               "visible": bikeLanesSlideVisible,
-            },
-            {
-              "data": gardensBoundary,
-              "visible": gardensSlideVisible,
-            },
-            {
-              "data": gardensMap,
-              "visible": gardensSlideVisible,
             },
             {
               "data": groceryBoundary,
