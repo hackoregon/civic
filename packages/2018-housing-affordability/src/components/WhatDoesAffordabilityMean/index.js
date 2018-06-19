bfe8c0191c0c31f86bbdd1718c7950f2b876e4c0
kodi thompson <kodirthompson@gmail.com>
2018-06-19 16:53:43 -0700

    updated housing card content review

--- packages/2018-housing-affordability/src/components/WhatDoesAffordabilityMean/index.js
+++ packages/2018-housing-affordability/src/components/WhatDoesAffordabilityMean/index.js
@@ -22,18 +22,19 @@ export class WhatDoesAffordabilityMean extends React.Component {
           spent on housing. The ‘rule of thumb’ for affordability is that your monthly rent shouldn’t 
           exceed 30% of your income.  If you made $1000 a month, your apartment would be considered 
           affordable if it’s under $300 a month. 
-          <br />
-          <br />
+        </p>
+        <p>
           Not everyone makes the same amount of money or has the same size family.
           For example, if you have more dependents on one person’s salary, that can make 
           it harder to afford the rent. The Department of Housing and Urban Development publishes
           an annual data set defining <a href="https://www.huduser.gov/portal/datasets/il.html#2018_dat">median family incomes</a> and affordability. 
-          <br />
-          <br />
+        </p>
+        <p>
           In Portland, <a href="https://www.portlandoregon.gov/phb/article/651806">the median family income for a family of four is $74,700.</a> 
           Applying our 30% rule of thumb, rent is affordable under $1,307 for this family making the median amount of income. 
           The City of Portland’s State of Housing Report in 2017 defines “affordable” as $1,375.
-          (<a href="https://www.portlandoregon.gov/phb/article/681253">https://www.portlandoregon.gov/phb/article/681253</a> - page 30)</p>
+          (<a href="https://www.portlandoregon.gov/phb/article/681253">https://www.portlandoregon.gov/phb/article/681253</a> - page 30)
+        </p>
           
 
 
