7b9536786212ee16d443f1b4c1f8cb8ab0d3a2b0
Clem Freeman <clemfree@gmail.com>
2018-06-19 18:51:23 -0700

    Basic props working with scatter chart

--- packages/component-library/src/GradientScale/GradientScale.js
+++ packages/component-library/src/GradientScale/GradientScale.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { VictoryChart, VictoryStack, VictoryArea } from 'victory';
+import { VictoryChart, VictoryScatter } from 'victory';
 
 import CivicVictoryTheme from '../VictoryTheme/VictoryThemeIndex';
 
-const GradientScale = ({ data }) => (
-  <VictoryChart theme={CivicVictoryTheme.civic}>
-    <VictoryStack>
-      <VictoryArea data={data} />
-    </VictoryStack>
-  </VictoryChart>
-);
+const colorScale = [
+  '#ffffd2',
+  '#ffefac',
+  '#fee296',
+  '#fec375',
+  '#fd9b54',
+  '#fc6443',
+  '#e63638',
+  '#c51f40',
+];
+
+const GradientScale = ({ domain, primary, secondary = [] }) => {
+  const height = 200;
+  const width = 600;
+  const data = [
+    { x: primary, y: 0, type: 'primary' },
+    ...secondary.map(num => ({ x: num, y: 0, type: 'secondary' })),
+  ];
+  return (
+    <VictoryChart
+      theme={CivicVictoryTheme.civic}
+      domain={{ x: domain, y: [0, 1] }}
+      height={height}
+      width={width}
+    >
+      <VictoryScatter data={data} />
+    </VictoryChart>
+  );
+};
 
 GradientScale.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.object).isRequired,
-  // domain: PropTypes.objectOf(PropTypes.array),
-  // rank: PropTypes.number
+  domain: PropTypes.arrayOf(PropTypes.number).isRequired,
+  primary: PropTypes.number.isRequired,
+  secondary: PropTypes.arrayOf(PropTypes.number),
 };
 
 export default GradientScale;
--- packages/component-library/stories/GradientScale.story.js
+++ packages/component-library/stories/GradientScale.story.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, object } from '@storybook/addon-knobs';
+import { withKnobs, number, array } from '@storybook/addon-knobs';
 import { GradientScale } from '../src';
 
 const displayName = 'GradientScale';
@@ -10,52 +10,8 @@ export default () =>
   storiesOf(displayName, module)
     .addDecorator(withKnobs)
     .add('Basic Usage', () => {
-      const data = object('Data', [
-        {
-          id: 178419,
-          date: '2015-01-01',
-          datapoint: 'Portland-Vancouver-Hillsboro, OR-WA',
-          datatype: 'Moderately Burdened Owners',
-          source: 'W-13',
-          valuetype: 'count',
-          value: '86.50000',
-          rank: 79,
-          total: 101,
-        },
-        {
-          id: 178425,
-          date: '2015-01-01',
-          datapoint: 'Portland-Vancouver-Hillsboro, OR-WA',
-          datatype: 'Moderately Burdened Owners, Share of All Households',
-          source: 'W-13',
-          valuetype: 'percent',
-          value: '15.59000',
-          rank: 79,
-          total: 101,
-        },
-        {
-          id: 178422,
-          date: '2015-01-01',
-          datapoint: 'Portland-Vancouver-Hillsboro, OR-WA',
-          datatype: 'Moderately Burdened Renters',
-          source: 'W-13',
-          valuetype: 'count',
-          value: '81.20000',
-          rank: 79,
-          total: 101,
-        },
-        {
-          id: 178428,
-          date: '2015-01-01',
-          datapoint: 'Portland-Vancouver-Hillsboro, OR-WA',
-          datatype: 'Moderately Burdened Renters, Share of All Households',
-          source: 'W-13',
-          valuetype: 'percent',
-          value: '23.23000',
-          rank: 58,
-          total: 101,
-        },
-      ]);
+      const domain = array('domain', [50, 90]);
+      const primary = number('primary', 63);
 
-      return <GradientScale data={data} />;
+      return <GradientScale domain={domain} primary={primary} />;
     });
