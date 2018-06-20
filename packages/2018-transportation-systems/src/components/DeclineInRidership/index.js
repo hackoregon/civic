6e1d2faa209f7a00254ab5eafff74a54fd819351
Jaron Heard <jaronheard@gmail.com>
2018-06-19 19:31:18 -0700

    working architecture for card 3

--- packages/2018-transportation-systems/src/components/DeclineInRidership/index.js
+++ packages/2018-transportation-systems/src/components/DeclineInRidership/index.js
@@ -12,21 +12,6 @@ import {
   getRidershipOverTimeData,
 } from '../../state/decline-in-ridership/selectors';
 
-const cardLoading = css`
-  width: 100%;
-  padding: 50px;
-  text-align: center;
-  background: #EEE;
-`;
-
-const cardError = css`
-  width: 100%;
-  padding: 50px;
-  text-align: center;
-  background: #FDD;
-  border: 1px solid #C99;
-`;
-
 export class DeclineInRidership extends React.Component {
   componentDidMount() {
     this.props.init();
@@ -39,20 +24,17 @@ export class DeclineInRidership extends React.Component {
       ridershipOverTime,
     } = this.props;
 
-    if (isLoading) {
-      return <div className={cardLoading}>Loading...</div>;
-    } else if (!ridershipOverTime) {
-      return <div className={cardError}>{error ? `API ${error}` : 'Could not render Plateau in Ridership.'}</div>;
-    }
-
     return (
       <CivicStoryCard
         title="Plateau in Ridership"
         slug="plateau-in-ridership"
+        loading={isLoading}
+        error={error}
       >
           <p>
 Newly released findings from TriMet shows a slow decline in public transit ridership relative to population growth over the last 10 years, a pattern which appears to be consistent across the nation.  While the cause of decline in ridership doesn't point to a single variable, it's been suggested that housing affordability and economic displacement may play a role in this phenomenon.
           </p>
+          { ridershipOverTime &&
             <LineChart
               title="Public Transit Ridership"
               subtitle="Average daily ridership for TriMet bus and rail (unlinked trips)"
@@ -64,6 +46,7 @@ Newly released findings from TriMet shows a slow decline in public transit rider
               dataSeries="type"
               xNumberFormatter={d => `${d}`}
             />
+          }
       </CivicStoryCard>
     );
   }
