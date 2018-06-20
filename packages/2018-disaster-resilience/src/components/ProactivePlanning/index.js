4bbc933eff1756927ee4d760bc89b3d43ff56ee3
Jaron Heard <jaronheard@gmail.com>
2018-06-20 15:06:08 -0700

    working redux

--- packages/2018-disaster-resilience/src/components/ProactivePlanning/index.js
+++ packages/2018-disaster-resilience/src/components/ProactivePlanning/index.js
@@ -1,25 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { css } from 'emotion';
 
-import { CivicStoryCard, Placeholder } from '@hackoregon/component-library';
+import { CivicStoryCard, LineChart } from '@hackoregon/component-library';
+
+import { fetchProactivePlanning } from '../../state/proactive-planning/actions';
+import {
+  isProactivePlanningPending,
+  catchProactivePlanningErrors,
+  getProactivePlanningData,
+} from '../../state/proactive-planning/selectors';
 
 export class ProactivePlanning extends React.Component {
   componentDidMount() {
-    // initialize data here
+    this.props.init();
   }
 
   render() {
+    const {
+      isLoading,
+      error,
+      proactivePlanning,
+    } = this.props;
+
     return (
       <CivicStoryCard
-        title="Proactive Planning for City-Wide Resilience"
-        slug="proactive-planning-for-city-wide-resilience"
+        title="Proactive Planning for Citywide Resiliance"
+        slug="proactive-planning"
+        loading={isLoading}
+        error={error && 'Error loading data'}
       >
-        <Placeholder />
+          <p>
+Newly released findings from TriMet shows a slow decline in public transit ridership relative to population growth over the last 10 years, a pattern which appears to be consistent across the nation.  While the cause of decline in ridership doesn't point to a single variable, it's been suggested that housing affordability and economic displacement may play a role in this phenomenon.
+          </p>
+          { proactivePlanning &&
+            <LineChart
+              title="Public Transit Ridership"
+              subtitle="Average daily ridership for TriMet bus and rail (unlinked trips)"
+              data={proactivePlanning}
+              xLabel="Year"
+              yLabel="Ridership"
+              dataKey="year"
+              dataValue="ons"
+              dataSeries="type"
+              xNumberFormatter={d => `${d}`}
+            />
+          }
       </CivicStoryCard>
     );
   }
 }
+ProactivePlanning.displayName = 'proactivePlanning';
+ProactivePlanning.propTypes = {
+  init: PropTypes.func,
+  isLoading: PropTypes.bool,
+  error: PropTypes.string,
+  proactivePlanning: PropTypes.arrayOf(PropTypes.object),
+};
 
-ProactivePlanning.displayName = 'ProactivePlanning';
-
-// Connect this to the redux store when necessary
-export default ProactivePlanning;
+export default connect(
+  state => ({
+    isLoading: isProactivePlanningPending(state),
+    error: catchProactivePlanningErrors(state),
+    proactivePlanning: getProactivePlanningData(state),
+  }),
+  dispatch => ({
+    init() {
+      dispatch(fetchProactivePlanning());
+    },
+  }),
+)(ProactivePlanning);
--- packages/2018-disaster-resilience/src/state/index.js
+++ packages/2018-disaster-resilience/src/state/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import proactivePlanning from './proactive-planning';
 
 export default function createReducer(asyncReducers) {
   return combineReducers({
     routing: routerReducer,
+    proactivePlanning,
     ...asyncReducers,
   });
 }
