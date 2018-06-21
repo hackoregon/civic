3b60d3c47fd6863e9b74d274b9d78194ee00d084
Michael Lange <dingoeatingfuzz@gmail.com>
2018-06-20 18:16:04 -0700

    Beginning of the rent burden card

--- packages/2018-housing-affordability/src/components/RentBurdenedHouseholds/index.js
+++ packages/2018-housing-affordability/src/components/RentBurdenedHouseholds/index.js
@@ -1,24 +1,83 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { loader, error, inputClass, emphasis } from '../css-utils';
 
-import { CivicStoryCard, Placeholder } from '@hackoregon/component-library';
+import { CivicStoryCard, Dropdown } from '@hackoregon/component-library';
+
+import {
+  fetchAllRentBurdenCities,
+  fetchRentBurdenCity,
+  setRentBurdenCity,
+} from '../../state/rent-burden/actions';
+import {
+  isCityLoading,
+  getCityError,
+  getSelectedCity,
+  getSelectedCityData,
+  getSelectedCityRank,
+  getAllCities,
+} from '../../state/rent-burden/selectors';
 
 export class RentBurdenedHouseholds extends React.Component {
   componentDidMount() {
-    // initialize data here
+    this.props.fetchAllCities();
+    this.props.setCity();
   }
 
   render() {
+    const {
+      isLoading,
+      isError,
+      allCities,
+      selectedCity,
+      selectedCityData,
+      selectedCityRank,
+      setCity,
+    } = this.props;
+
+    const cityOptions = allCities && allCities.map(c => ({ value: c, label: c }));
+
+    const Loader = () => <div className={loader}>Loading...</div>;
+    const ErrorMessage = () => <div className={error}>Could not load data for this city.</div>;
+
     return (
       <CivicStoryCard
         title="Rent Burdened Households"
         slug="rent-burdened-households"
       >
         <p>A household is severely burdened when its monthly
-           rent exceeds 50% of monthly income. In 2015, 24% of all 
+           rent exceeds 50% of monthly income. In 2015, 24% of all
            households in the Portland region were severely burdened, up 8
-           percentage points from 2014. Nationally, Portland ranks 50 out of 
+           percentage points from 2014. Nationally, Portland ranks 50 out of
            100 in terms of cost burdened renters </p>
-        <Placeholder issue = "194"/>
+
+        <div>
+          <strong>Search An Area</strong>
+          {allCities && allCities.length && <Dropdown
+            value={cityOptions.find(c => c.value === selectedCity)}
+            onChange={setCity}
+            options={cityOptions}
+          />}
+        </div>
+
+        <section>
+          {isLoading && <Loader />}
+          {isError && <ErrorMessage />}
+          {selectedCityRank && (<div>
+            <p>
+              {selectedCity} ranks <strong className={emphasis}>{selectedCityRank.rank}/{selectedCityRank.total} </strong>
+              for the median home price to median income ratio in <strong className={emphasis}>2014</strong>
+            </p>
+            <input
+              disabled
+              className={inputClass}
+              type="range"
+              min="1"
+              value={selectedCityRank.rank}
+              max={selectedCityRank.total}
+            />
+          </div>)}
+        </section>
       </CivicStoryCard>
     );
   }
@@ -27,4 +86,22 @@ export class RentBurdenedHouseholds extends React.Component {
 RentBurdenedHouseholds.displayName = 'RentBurdenedHouseholds';
 
 // Connect this to the redux store when necessary
-export default RentBurdenedHouseholds;
+export default connect(
+  state => ({
+    isLoading: isCityLoading(state),
+    allCities: getAllCities(state),
+    selectedCity: getSelectedCity(state),
+    selectedCityData: getSelectedCityData(state),
+    selectedCityRank: getSelectedCityRank(state),
+    isError: getCityError(state),
+  }),
+  dispatch => ({
+    fetchAllCities() {
+      dispatch(fetchAllRentBurdenCities());
+    },
+    setCity(city = {}) {
+      dispatch(fetchRentBurdenCity(city.value));
+      dispatch(setRentBurdenCity(city.value));
+    },
+  })
+)(RentBurdenedHouseholds);
