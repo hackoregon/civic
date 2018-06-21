ee90e1b26af8d0632dc70220ef5879f656731d60
kodi thompson <kodirthompson@gmail.com>
2018-06-20 21:53:27 -0700

    Transportation Systems Card Copy

--- packages/2018-transportation-systems/src/components/ExploreBusServiceAndEquity/index.js
+++ packages/2018-transportation-systems/src/components/ExploreBusServiceAndEquity/index.js
@@ -13,6 +13,17 @@ export class ExploreBusServiceAndEquity extends React.Component {
         title="Explore Bus Service and Equity"
         slug="explore-bus-service-and-equity"
       >
+        <p>Increasing Ridership is a common KPI for Transit Agencies
+           across the country, and yet, according to the data, decreasing 
+           Ridership may not be a trend we can stop. How can Public-Private 
+           Partnerships like Trimet set appropriate objectives for a Smart 
+           City in the context of this decreasing Ridership? Below we explore
+           how decreasing Transit Availability has evolved alongside changes in Equity 
+           Statistics, and we ask the question: What is more important? Full busses--or 
+           the ability for everyone to get from Point A to Point B at a low cost. What
+            will a City look like when one of its Key Success Measures is Equitable Transit 
+          Availability?
+        </p>
         <Placeholder>
           <h1>Sandbox Card</h1>
           <p>Don't worry about this one</p>
--- packages/2018-transportation-systems/src/components/HistoricalChangesToBusService/index.js
+++ packages/2018-transportation-systems/src/components/HistoricalChangesToBusService/index.js
@@ -13,6 +13,13 @@ export class HistoricalChangesToBusService extends React.Component {
         title="Historical Changes to Bus Service"
         slug="historical-changes-to-bus-service"
       >
+        <p>During the Recession, we saw a Nationwide decrease in the number of 
+          busses on the roads. This drop in Service Availability was an Operational
+          Reality for many Transit Organizations, and the possible consequences for 
+          Ridership are only recently being considered. In this CIVIC Platform Sandbox
+          feature, we show how Ridership and Transit Frequency have changed in Portland 
+          between 2009 and 2017.
+        </p>
         <Placeholder />
       </CivicStoryCard>
     );
