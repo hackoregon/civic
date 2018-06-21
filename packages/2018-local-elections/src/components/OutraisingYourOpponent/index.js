cf2b5f406e60b63f66ed17afebfe6c02c5c30b3e
Jaron Heard <jaronheard@gmail.com>
2018-06-21 16:57:03 -0700

    fix

--- packages/2018-local-elections/src/components/OutraisingYourOpponent/index.js
+++ packages/2018-local-elections/src/components/OutraisingYourOpponent/index.js
@@ -3,62 +3,61 @@ import React from 'react';
 import { CivicStoryCard, Placeholder, HorizontalBarChart } from '@hackoregon/component-library';
 import { percentage } from '@hackoregon/component-library/src/utils/formatters'
 
-const data = [
-  {
-    category: "amount_contrib",
-    importance: 0.128552254,
-    sortOrder: 9
-  },
-  {
-    category: "amount_expend",
-    importance: 0.134080107,
-    sortOrder: 10
-  },
-  {
-    category: "amount_groots",
-    importance: 0.043643585,
-    sortOrder: 7
-  },
-  {
-    category: "Influential contributers",
-    importance: 0.532761433,
-    sortOrder: 11
-  },
-  {
-    category: "most_votes_primary",
-    importance: 0.114947699,
-    sortOrder: 8
-  },
-  {
-    category: "party_affiliation_Democrat",
-    importance: 0.012570137,
-    sortOrder: 5
-  },
-  {
-    category: "party_affiliation_Republican",
-    importance: 0.015123368,
-    sortOrder: 6
-  },
-  {
-    category: "race_Attorney General",
-    importance: 0.00065277,
-    sortOrder: 1
-  },
-  {
-    category: "race_Governor",
-    importance: 0.002783337,
-    sortOrder: 2
-  },
-  {
-    category: "race_State Representative",
-    importance: 0.00391163,
-    sortOrder: 3
-  },
-  {
-    category: "race_State Senator",
-    importance: 0.01097368,
-    sortOrder: 4
-  },
+const data =
+[{
+     category: "Influencers",
+  importance: 0.532761433,
+  sortOrder: 11
+},
+{
+category: "$ Expenditures",
+  importance: 0.2577329977621656,
+  sortOrder: 10
+},
+{ category: "$ Received",
+  importance: 0.22949919907509095,
+  sortOrder: 9
+},
+{
+    category: "Primary Vote Count",
+  importance: 0.22878431536282723,
+  sortOrder: 8
+},
+{
+category: "Grassroots",
+  importance: 0.12021850170825318,
+  sortOrder: 7
+},
+{
+ category: "Republican",
+  importance: 0.05830959262159894,
+  sortOrder: 6
+},
+{
+  category: "Democrat",
+  importance: 0.019450051372393994,
+  sortOrder: 5
+},
+{
+  category: "St. Senator",
+  importance: 0.03578981589773275,
+  sortOrder: 4
+},
+{
+    category: "St. Representative",
+  importance: 0.033471046408828774,
+  sortOrder: 3
+},
+{
+category: "Governor",
+  importance: 0.012904292200920951,
+  sortOrder: 2
+},
+{
+category: "Attorney General",
+  importance: 0.00384018759018759,
+  sortOrder: 1
+},
 ];
 
 function compareNumbers(a, b) {
