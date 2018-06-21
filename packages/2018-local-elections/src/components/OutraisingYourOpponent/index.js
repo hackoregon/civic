8a1175422225c50b9fc19b34711505a0872d477e
Jaron Heard <jaronheard@gmail.com>
2018-06-21 11:25:53 -0700

    outraising your opponent

--- packages/2018-local-elections/src/components/OutraisingYourOpponent/index.js
+++ packages/2018-local-elections/src/components/OutraisingYourOpponent/index.js
@@ -1,6 +1,71 @@
 import React from 'react';
 
-import { CivicStoryCard, Placeholder } from '@hackoregon/component-library';
+import { CivicStoryCard, Placeholder, HorizontalBarChart } from '@hackoregon/component-library';
+import { percentage } from '@hackoregon/component-library/src/utils/formatters'
+
+const data = [
+  {
+    category: "amount_contrib",
+    importance: 0.128552254,
+    sortOrder: 9
+  },
+  {
+    category: "amount_expend",
+    importance: 0.134080107,
+    sortOrder: 10
+  },
+  {
+    category: "amount_groots",
+    importance: 0.043643585,
+    sortOrder: 7
+  },
+  {
+    category: "Influential contributers",
+    importance: 0.532761433,
+    sortOrder: 11
+  },
+  {
+    category: "most_votes_primary",
+    importance: 0.114947699,
+    sortOrder: 8
+  },
+  {
+    category: "party_affiliation_Democrat",
+    importance: 0.012570137,
+    sortOrder: 5
+  },
+  {
+    category: "party_affiliation_Republican",
+    importance: 0.015123368,
+    sortOrder: 6
+  },
+  {
+    category: "race_Attorney General",
+    importance: 0.00065277,
+    sortOrder: 1
+  },
+  {
+    category: "race_Governor",
+    importance: 0.002783337,
+    sortOrder: 2
+  },
+  {
+    category: "race_State Representative",
+    importance: 0.00391163,
+    sortOrder: 3
+  },
+  {
+    category: "race_State Senator",
+    importance: 0.01097368,
+    sortOrder: 4
+  },
+];
+
+function compareNumbers(a, b) {
+  return b - a;
+}
+
+data.sort(compareNumbers);
 
 export class OutraisingYourOpponent extends React.Component {
   componentDidMount() {
@@ -13,7 +78,18 @@ export class OutraisingYourOpponent extends React.Component {
         title="Outraising Your Opponent"
         slug="outraising-your-opponent"
       >
-        <Placeholder />
+        <HorizontalBarChart
+          xLabel="Importance"
+          yLabel="Feature"
+          title="What Really Predicts Who Wins Election"
+          subtitle="Feature importance for Oregon elections"
+          dataLabel="category"
+          dataValue="importance"
+          xAxisLabel="Importance"
+          sortOrder="sortOrder"
+          data={data}
+          dataValueFormatter={percentage}
+        />
       </CivicStoryCard>
     );
   }
