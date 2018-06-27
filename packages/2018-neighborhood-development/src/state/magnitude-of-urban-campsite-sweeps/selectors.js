0c6d0adf5aff788149b0750d259f4d7e16a7bcd5
Jaron Heard <jaronheard@gmail.com>
2018-06-27 14:41:29 -0700

    Card/magnitudeofurbancampsites (#347)
    * scaffold redux architecture, and begin implementing component.
    
    * add magnitude of urban campsite sweeps
    
    * loading error state, remove unused code
    
    * remove additional unused code
    
    * additional fixes to error state
    
    * remove more unused code

---
+++ packages/2018-neighborhood-development/src/state/magnitude-of-urban-campsite-sweeps/selectors.js
@@ -0,0 +1,32 @@
+import { createSelector } from 'reselect';
+import { rootState } from '../selectors';
+
+export const getMagnitudeOfUrbanCampsiteSweepsRequest = createSelector(
+  rootState,
+  ({ magnitudeCampsiteSweeps }) => magnitudeCampsiteSweeps,
+);
+
+const formatData = arr => arr.map(obj => ({
+  date: new Date(obj.report_time),
+  count: obj.count,
+}));
+
+export const getMagnitudeOfUrbanCampsiteSweepsData = createSelector(
+  getMagnitudeOfUrbanCampsiteSweepsRequest,
+  ({ data }) => data && formatData(data),
+);
+
+export const isMagnitudeOfUrbanCampsiteSweepsPending = createSelector(
+  getMagnitudeOfUrbanCampsiteSweepsRequest,
+  ({ pending }) => !!pending,
+);
+
+export const catchMagnitudeOfUrbanCampsiteSweepsErrors = createSelector(
+  getMagnitudeOfUrbanCampsiteSweepsRequest,
+  ({ error }) => error || error,
+);
+
+export const formatDateData = createSelector(
+  getMagnitudeOfUrbanCampsiteSweepsData,
+  data => data,
+);
