459cb1608a47a7c8b40b1338bae51543703a2f37
Jaron Heard <jaronheard@gmail.com>
2018-06-28 18:19:39 -0700

    quick fix

--- packages/2018-neighborhood-development/src/components/UnderstandingStaffCuts/index.js
+++ packages/2018-neighborhood-development/src/components/UnderstandingStaffCuts/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { CivicStoryCard, DataTable, Collapsable } from '@hackoregon/component-library';
-import { monthYear } from '@hackoregon/component-library/src/utils/formatters';
 
 import { fetchUnderstandingStaffCuts } from '../../state/understanding-staff-cuts/actions';
 
@@ -92,14 +91,13 @@ export class UnderstandingStaffCuts extends React.Component {
         <Collapsable>
           <Collapsable.Section>
             <div>
-              <p>Portland Public Schools (PPS) parents received an email explaining proposed staffing changes in mid-April, 2018. Hack Oregon conducted an analysis of these changes using PPS’s stated goals listed below.
+              <p>Portland Public Schools (PPS) parents received an email explaining proposed staffing changes in mid-April, 2018. Hack Oregon conducted an analysis of these changes using PPS’s stated goals listed below.</p>
               <ul>
                 <li>Ensure schools have adequate staffing to maintain reasonable class sizes.</li>
                 <li>Support the district’s equity goals.</li>
                 <li>Provide baseline academic program offerings at every school.</li>
                 <li>Provide greater stability over time.</li>
               </ul>
-              </p>
               <p>See the results of our analysis in the sortable table below.</p>
             </div>
           </Collapsable.Section>
@@ -114,13 +112,12 @@ export class UnderstandingStaffCuts extends React.Component {
             <p>Students per FTE 2018 - 19 = 18.82 - 2.69*(Historically Underserved %) + 1.58*(% Changes in Enrollment)  - 10.91*(2018-19 % Free Meals by Direct Cert)</p>
             <p>This is not to say that this was the model that PPS ultimately made their decision on. Our model accounts for about 80% of the variation in the data. Our goal here is to use an outside methodology to assess PPS changes against their stated goals. This is a first step in understanding these changes. We encourage you to explore the rest of the cards in the education model to further inform your understanding of PPS data. </p>
             <p>A high level interpretation of this model is that Historically Underserved % and 2018 -19 Free Meals by Direct Cert are negatively correlated with Class Room Size and Change in Enrollment is positively correlated with Class Room Size. That is to say that of the goals stated by PPS are consistent with our preliminary findings. The average ratio of Students to FTE across schools is about 19, an increase in Historically Underserved % or 2018-19 % Free Meals by Direct Cert is correlated with a decrease in the ratio of Students to FTE. (TLDW; more staff in schools with higher % of Direct Cert or Historically underserved populations). In the table, you can explore which schools are outliers in our model. </p>
-            <p><strong>Definitions</strong>
+            <p><strong>Definitions</strong></p>
             <ul>
               <li>FTE (Full Time Employment): This includes full time teachers, classroom assistants, secretaries etc.</li>
               <li>Direct Certification: Direct certification requires states and local educational agencies to automatically enroll students from households already participating in the Supplemental Nutrition Assistance Program (SNAP) or the Food Distribution Program on Indian Reservations, without an additional application from the child’s family to assist low-income households with children eligible for free meals at school (<a href="https://www.fns.usda.gov/tags/direct-certification">source</a>)</li>
               <li>Historically Underserved: This is a combination of Special Education, English Language Learner, and Racial Minority Students</li>
             </ul>
-            </p>
             </div>
           </Collapsable.Section>
         </Collapsable>
@@ -133,7 +130,7 @@ UnderstandingStaffCuts.propTypes = {
   init: PropTypes.func,
   isLoading: PropTypes.bool,
   error: PropTypes.string,
-  understandingStaffCuts: PropTypes.arrayOf(PropTypes.object),
+  understandingStaffCuts: PropTypes.object,
 };
 
 export default connect(
