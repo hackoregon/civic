895249c2c56835da1f65506977f0b2abbbfcf5cf
Jaron Heard <jaronheard@gmail.com>
2018-07-04 21:02:52 -0700

    update pullquote component and housing links

--- packages/2018-housing-affordability/src/components/App/index.js
+++ packages/2018-housing-affordability/src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import '@hackoregon/component-library/assets/global.styles.css';
-import { PageLayout } from '@hackoregon/component-library';
+import { PageLayout, PullQuote } from '@hackoregon/component-library';
 import BuildingBoomInPortland from '../BuildingBoomInPortland';
 import WhatDoesAffordabilityMean from '../WhatDoesAffordabilityMean';
 import AffordableRentalUnitsDwindling from '../AffordableRentalUnitsDwindling';
@@ -23,15 +23,15 @@ const App = () => (
     {/* <WhatDoesAffordabilityMean /> */}
     <p className="transition">In the Portland Region, affordable means a monthly housing budget of $1,375 for a family of four.</p>
     <AffordableRentalUnitsDwindling />
-    <p className="transition">From 2005 to 2015, the Portland Metro lost 39,645 units below $1000/month.</p>
+    <PullQuote quoteText="From 2005 to 2015, the Portland Metro lost 39,645 units below $1000/month." />
     <RentBurdenedHouseholds />
     <p className="transition">24% of all Portland Metro households were severely burdened in 2015</p>
     <PortlandNeedsAffordableRentalUnits />
-    <p className="transition">The Portland region needs 78,806 NEW affordable units</p>
+    <PullQuote quoteText="The Portland region needs 78,806 NEW affordable units" />
     <WhoCanAffordToBuyAHome />
     <p className="transition">In 2016, Portland metro ranked in the bottom 10% of metropolitan areas in regards to home ownership affordability.</p>
     <PacificNorthwestTopsNationInSurgingHomePrices />
-    <p className="transition">Portland had the second highest growth rate in home prices out of 120 U.S metro-areas in 2016, exceeded only by Seattle</p>
+    <PullQuote quoteText="Portland had the second highest growth rate in home prices out of 120 U.S metro-areas in 2016, exceeded only by Seattle" />
     <MeasuringMarketValueOfHomesInPortland />
     {/* <AffordabilityInAComplexHousingMarket /> */}
     {/* <ExploreHousingPolicyImplementation /> */}
--- packages/component-library/src/PullQuote/PullQuote.js
+++ packages/component-library/src/PullQuote/PullQuote.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { css } from 'emotion';
+import { TwitterShareButton, TwitterIcon } from 'react-share';
 
 const quoteClass = css`
   font-family: 'Merriweather', serif;
   font-size: 40px;
+  color: #eb4d5f;
+  margin-bottom: 20px;
 `;
 
 const attributionClass = css`
@@ -11,14 +14,31 @@ const attributionClass = css`
   text-align: center;
   display: flex;
   justify-content: center;
-  line-height: 56px;
+  line-height: 40px;
+`;
+
+const iconClass = css`
+  display: inline-block;
+`;
+
+const wrapperClass = css`
+  max-width: 1000px;
+  text-align: center;
+  margin: 1.5em auto;
 `;
 
 const PullQuote = ({ quoteText, quoteAttribution }) => (
-  <blockquote className={quoteClass} >
-      &#8220;{ quoteText }&#8221;<br/>
-    { quoteAttribution ? <span className={attributionClass}>&#8212; { quoteAttribution }</span> : null }
-  </blockquote>
+  <div className={wrapperClass}>
+    <TwitterShareButton url={window.location.href} title={quoteText}>
+      <blockquote className={quoteClass} >
+          &#8220;{ quoteText }&#8221;<br />
+        { quoteAttribution ? <span className={attributionClass}>&#8212; { quoteAttribution }</span> : null }
+      </blockquote>
+      <div className={iconClass}>
+        <TwitterIcon size={40} round iconBgStyle={{ fill: '#eb4d5f' }} />
+      </div>
+    </TwitterShareButton>
+  </div>
 );
 
 PullQuote.displayName = 'PullQuote';
