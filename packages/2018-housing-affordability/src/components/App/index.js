ce4b9aebc3052439fc840b8e44d4987a85ee79a3
Jaron Heard <jaronheard@gmail.com>
2018-07-06 09:16:52 -0700

    Housing page clean-up (#362)
    * remove card and switch to tweetblocks
    
    * remove tweetblock above card

--- packages/2018-housing-affordability/src/components/App/index.js
+++ packages/2018-housing-affordability/src/components/App/index.js
@@ -19,17 +19,17 @@ const App = () => (
     heroTitle="Synthesizing Complex Information to Better Understand Affordable Housing Trends and Policy Dynamics"
   >
     <p>Over the past 10 years, the United States housing market has been dominated by two major trends: a surging demand in the rental market and a crash in the rate of homeownership.  The 2017 State of the Nation's Housing Report by the Joint Center of Housing Studies at Harvard University shows these trends are particularly evident in Portland.</p>
-    <BuildingBoomInPortland />
+    {/* <BuildingBoomInPortland /> */}
     {/* <WhatDoesAffordabilityMean /> */}
-    <p className="transition">In the Portland Region, affordable means a monthly housing budget of $1,375 for a family of four.</p>
+    {/* <PullQuote quoteText="In the Portland Region, affordable means a monthly housing budget of $1,375 for a family of four." /> */}
     <AffordableRentalUnitsDwindling />
     <PullQuote quoteText="From 2005 to 2015, the Portland Metro lost 39,645 units below $1000/month." />
     <RentBurdenedHouseholds />
-    <p className="transition">24% of all Portland Metro households were severely burdened in 2015</p>
+    <PullQuote quoteText="24% of all Portland Metro households were severely burdened in 2015" />
     <PortlandNeedsAffordableRentalUnits />
     <PullQuote quoteText="The Portland region needs 78,806 new affordable units" />
     <WhoCanAffordToBuyAHome />
-    <p className="transition">In 2016, Portland metro ranked in the bottom 10% of metropolitan areas in regards to home ownership affordability.</p>
+    <PullQuote quoteText="In 2016, Portland metro ranked in the bottom 10% of metropolitan areas in regards to home ownership affordability." />
     <PacificNorthwestTopsNationInSurgingHomePrices />
     <PullQuote quoteText="Portland had the second highest growth rate in home prices out of 120 U.S metro-areas in 2016, exceeded only by Seattle" />
     <MeasuringMarketValueOfHomesInPortland />
