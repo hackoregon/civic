34128a855ba3c7933af88fae1cbb3d7d16b1f0f7
Jaron Heard <jaronheard@gmail.com>
2018-07-10 11:13:29 -0700

    update geocoder

--- packages/2018-disaster-resilience/src/components/YouAndYourNeighbors/index.js
+++ packages/2018-disaster-resilience/src/components/YouAndYourNeighbors/index.js
@@ -25,6 +25,7 @@ export class YouAndYourNeighbors extends React.Component {
         slug="you-and-your-neighbors-in-the-earthquake"
       >
         <div>
+        <p>It will be critical for individuals to understand their location relative to key resources immediately following an earthquake. The <a href="https://www.portlandoregon.gov/pbem/59630" target="_blank" rel="noopener noreferrer">BEECN site</a> is a place to go in Portland after a major earthquake to ask for emergency assistance or report severe damage/injury. Places like hospitals, fire stations and schools will be rallying areas for the community and crucial for recovery efforts. Input your address, or a friend/family member’s address below to generate a personalized map.</p>
           <BaseMap
             initialLongitude={LONG}
             initialLatitude={LAT}
