d064f83854579a4109af8e55fa1dd1932a4e7b73
Jaron Heard <jaronheard@gmail.com>
2018-07-12 15:48:19 -0700

    update to collapsable small multiples

--- packages/2018-neighborhood-development/src/components/VotersOnTheMove/index.js
+++ packages/2018-neighborhood-development/src/components/VotersOnTheMove/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { css } from 'emotion';
 
-import { CivicStoryCard, Scatterplot } from '@hackoregon/component-library';
+import { CivicStoryCard, Scatterplot, Collapsable, CivicVictoryTheme } from '@hackoregon/component-library';
 
 import { fetchVotersOnTheMove, fetchAwayVotersOnTheMove } from '../../state/voters-on-the-move/actions';
 
@@ -34,6 +34,22 @@ const ageGroupLabels = [
   { category: 50, label: '50+' },
 ];
 
+const ageLabels = {
+  18: { category: 0, label: '18-25' },
+  26: { category: 1, label: '26-32' },
+  33: { category: 2, label: '33-39' },
+  40: { category: 3, label: '40-49' },
+  50: { category: 4, label: '50+' },
+};
+
+const legendStyle = css`
+  font-family: 'Roboto Condensed', 'Helvetica Neue', Helvetica, sans-serif;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  margin: 10px 0 0 0;
+`;
+
 export class VotersOnTheMove extends React.Component {
   componentDidMount() {
     this.props.init();
@@ -51,6 +67,37 @@ export class VotersOnTheMove extends React.Component {
 
     const data = { center: votersOnTheMove, away: awayVotersOnTheMove };
 
+    function legendComponent(age) {
+      const customLegend = (legendData) => {
+        return (
+          <legend className={legendStyle}>
+            <span
+              className={css`
+                margin-left: 5px;
+              `}
+            >
+              <svg viewBox="0 0 10 10" width="10px">
+                <circle
+                  cx="5"
+                  cy="5"
+                  r="5"
+                  fill={CivicVictoryTheme.civic.group.colorScale[ageLabels[age].category]}
+                />
+              </svg>
+              <span
+                className={css`
+                  margin-left: 5px;
+                `}
+              >
+                {`Ages ${ageLabels[age].label}`}
+              </span>
+            </span>
+          </legend>
+        );
+      };
+      return customLegend;
+    }
+
     const voterScatterplot = (age, direction) => (
       <Scatterplot
         data={data[direction].results.filter(({ age_group }) => age_group === age)}
@@ -61,6 +108,7 @@ export class VotersOnTheMove extends React.Component {
         xLabel="SE"
         yLabel="NW"
         domain={{ x: [-19.5, 19.5], y: [-10.5, 10.5] }}
+        legendComponent={legendComponent(age)}
       />
     );
 
@@ -71,26 +119,46 @@ export class VotersOnTheMove extends React.Component {
         loading={(isLoading || isAwayLoading)}
         error={(error || awayError) && 'error'}
       >
-        { (votersOnTheMove && awayVotersOnTheMove) &&
-          <div className={smallMultiples}>
-            <div className={chartColumn}>
-              <h2>Towards City Center</h2>
-              {voterScatterplot(18, 'center')}
-              {voterScatterplot(26, 'center')}
-              {voterScatterplot(33, 'center')}
-              {voterScatterplot(40, 'center')}
-              {voterScatterplot(50, 'center')}
+        <Collapsable>
+          <Collapsable.Section>
+            <div>
+            <p>For moves within Portland, these visualizations look at the distance and directeion towards or away from the city center that voters are moving, broken up by age group.</p>
+            { (votersOnTheMove && awayVotersOnTheMove) &&
+              <div className={smallMultiples}>
+                <div className={chartColumn}>
+                  <h2>Moves Towards City Center</h2>
+                  {voterScatterplot(18, 'center')}
+                  {voterScatterplot(26, 'center')}
+                </div>
+                <div className={chartColumn}>
+                  <h2>Moves Away From City Center</h2>
+                  {voterScatterplot(18, 'away')}
+                  {voterScatterplot(26, 'away')}
+                </div>
+              </div>
+            }
             </div>
-            <div className={chartColumn}>
-              <h2>Away From City Center</h2>
-              {voterScatterplot(18, 'away')}
-              {voterScatterplot(26, 'away')}
-              {voterScatterplot(33, 'away')}
-              {voterScatterplot(40, 'away')}
-              {voterScatterplot(50, 'away')}
+          </Collapsable.Section>
+          <Collapsable.Section hidden>
+            <div>
+            { (votersOnTheMove && awayVotersOnTheMove) &&
+              <div className={smallMultiples}>
+                <div className={chartColumn}>
+                  {voterScatterplot(33, 'center')}
+                  {voterScatterplot(40, 'center')}
+                  {voterScatterplot(50, 'center')}
+                </div>
+                <div className={chartColumn}>
+                  {voterScatterplot(33, 'away')}
+                  {voterScatterplot(40, 'away')}
+                  {voterScatterplot(50, 'away')}
+                </div>
+              </div>
+            }
+            <p>Age is calculated at the time of relocation. Portland city center is defined as the center of the Burnside Bridge. Change in distance from Portland city center is calculated by taking the displacement from the individual’s previous address. The scatterplots show when people relocate from their previous address, the direction and the distance moved either toward or away from the city center.</p>
             </div>
-          </div>
-        }
+          </Collapsable.Section>
+        </Collapsable>
       </CivicStoryCard>
     );
   }
@@ -102,6 +170,9 @@ VotersOnTheMove.propTypes = {
   isLoading: PropTypes.bool,
   error: PropTypes.string,
   votersOnTheMove: PropTypes.arrayOf(PropTypes.object),
+  isAwayLoading: PropTypes.bool,
+  awayError: PropTypes.string,
+  awayVotersOnTheMove: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default connect(
