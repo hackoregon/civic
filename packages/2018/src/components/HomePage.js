39f5779c4e41fd8d06ec1d460b4e1f3090880c5d
Jaron Heard <jaronheard@gmail.com>
2018-10-07 01:43:06 -0700

    card stacks

--- packages/2018/src/components/HomePage.js
+++ packages/2018/src/components/HomePage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Carousel from 'nuka-carousel';
 import { Link } from 'react-router';
 import { css } from 'emotion';
 import {
   PageLayout,
   Button,
   CivicStoryCard,
+  CivicCardStack,
   BarChart,
   Scatterplot,
   GradientScale,
@@ -16,7 +16,6 @@ import {
   percentage,
   year,
 } from '@hackoregon/component-library/src/utils/formatters';
-import PagingDots from './PagingDots';
 import {
   magnitudeOfUrbanCampsiteSweeps,
   proactivePlanning,
@@ -46,16 +45,11 @@ const initialTextContainer = css`
   min-height: 1000px;
   z-index: -1;
   mask-image: linear-gradient(to top, transparent 25%, black 240%);
-  background-image: url(https://previews.dropbox.com/p/thumb/AAOYBm53Mtbtaya0lADcAsWoWBJNlyNMdvt3m1i8dlZgujDYGBz66349riSzNqo0e5eVJ_u7nwVWsWXZaKKjs67W6HAr0C7RfbM_e4O_JW8cj9WsnpO-7Y-OEN0jo1R_g94XFmqfCXuKWwp8AgHloSo0noUYDRWACG05gDQuApdzmvpY7KIGuwr93rnuN2Sj4IlF3ZAqU2yWbt-JHAbGyX-NlWUSZ0GBfPUYiLX9zXEXJQ/p.png?size=1600x1200&size_mode=3);
 `;
 
-const buttonContainer = css`
-  position: fixed;
-  bottom: 10px;
-  right: 35px;
+const buttonContainerStatic = css`
+  padding: 8px 0;
 `;
-
-const carouselContainer = css``;
 
 const titleStyle = css`
   font-size: 50px;
@@ -80,26 +74,15 @@ const subtitleStyle = css`
 
 const contentContainer = css`
   padding: 25px 0;
-`;
-
-const emphasis = css`
-  color: #000;
-`;
-
-const gradientLabel = css`
-  ${emphasis} position: relative;
-  bottom: -10px;
-  @media (max-width: 640px) {
-    bottom: -5px;
-  }
+  margin: 0 auto;
+  max-width: 900px;
 `;
 
 const issueStyle = index => css`
   font-size: 21px;
-  line-height: 1.2;
+  line-height: 0;
   font-weight: 500;
   font-family: 'Rubik', sans-serif;
-  margin-bottom: -20px;
   color: ${colors[index]};
   text-align: right;
   @media (max-width: 640px) {
@@ -107,8 +90,47 @@ const issueStyle = index => css`
   }
 `;
 
-class HomePage extends Component {
+const gridContainer = css`
+  position: relative;
+  display: grid;
+  grid-template: 50% 50% / 50% 50%;
+  row-gap: 20px;
+  column-gap: 20px;
+  padding: 40px 0 0 0;
+  @media (max-width: 640px) {
+    grid-template: 100% / 100%;
+  }
+`;
 
+const gridItem = css`
+  position: relative;
+  width: 30vw;
+  max-width: 400px;
+  height: 25vw;
+  max-height: 300px;
+  @media (max-width: 640px) {
+    width: 100%;
+    height: 70vw;
+  }
+`;
+
+const chartWrapper = css`
+`;
+
+const sampleChart = (
+  <div className={chartWrapper}>
+    <BarChart
+      data={magnitudeOfUrbanCampsiteSweeps}
+      xLabel="Month"
+      yLabel="Sweeps"
+      dataKey="date"
+      dataValue="count"
+      xNumberFormatter={monthYear}
+    />
+  </div>
+);
+
+class HomePage extends Component {
   render() {
     return (
       <PageLayout>
@@ -120,47 +142,19 @@ class HomePage extends Component {
             Public Knowledge
           </div>
           <div className={subtitleStyle}>For issues that matter</div>
-          <div className={carouselContainer}>
-            <Carousel
-              autoplay
-              autoplayInterval={4000}
-              speed={1000}
-              autoGenerateStyleTag={false}
-              framePadding={'10px 0px'}
-              renderTopCenterControls={props => <PagingDots {...props} />}
-              renderCenterLeftControls={() => null}
-              renderCenterRightControls={() => null}
-              renderBottomCenterControls={() => null}
-              transitionMode={'fade'}
-              wrapAround
-            >
-              <div>
-                <div className={issueStyle(0)}>Homelessness</div>
-                <CivicStoryCard
-                  footer={false}
-                  title="Magnitude of Urban Campsite Sweeps"
-                >
-                  <BarChart
-                    title="Portland's Urban Campsite Sweeps"
-                    subtitle="Total count of campsite clean-ups by month, 2016-2018"
-                    data={magnitudeOfUrbanCampsiteSweeps}
-                    xLabel="Month"
-                    yLabel="Sweeps"
-                    dataKey="date"
-                    dataValue="count"
-                    xNumberFormatter={monthYear}
-                  />
-                </CivicStoryCard>
-              </div>
-              <div>
-                <div className={issueStyle(1)}>Disaster Resilience</div>
-                <CivicStoryCard
-                  footer={false}
-                  title="Proactive Planning For Citywide Resilience"
-                >
+          <div className={buttonContainerStatic}>
+            <Button>EXPLORE CIVIC ✨🚀✨</Button>
+          </div>
+          <div className={gridContainer}>
+            <div className={gridItem}>
+              <div className={issueStyle(1)}>Homelessness</div>
+              <CivicCardStack cards={3}>{sampleChart}</CivicCardStack>
+            </div>
+            <div className={gridItem}>
+              <div className={issueStyle(2)}>Disaster Resilience</div>
+              <CivicCardStack cards={3}>
+                <div className={chartWrapper}>
                   <Scatterplot
-                    title="Resilience and Displacement"
-                    subtitle="Resilience as measured by census non-response rate and expected displacement in a 9.0 earthquake by neighborhood"
                     data={proactivePlanning}
                     xLabel="Census Non-Response Rate"
                     yLabel="Per Capita Displacement"
@@ -168,45 +162,21 @@ class HomePage extends Component {
                     dataKeyLabel="resilienceLabel"
                     dataValue="displaced_percap"
                     dataValueLabel="displacementLabel"
-                    dataSeries="quadrant"
                     size={{ key: 'total_population', minSize: 2, maxSize: 10 }}
                     xNumberFormatter={percentage}
                     yNumberFormatter={percentage}
                   />
-                </CivicStoryCard>
-              </div>
-              <div>
-                <div className={issueStyle(2)}>Affordable Housing</div>
-                <CivicStoryCard footer={false} title="Rent Burdened Households">
-                  <section>
-                    <div>
-                      <p>
-                        <strong className={gradientLabel}>Less burdened</strong>
-                        <GradientScale
-                          domain={[1, selectedCityRank.total]}
-                          primary={selectedCityRank.rank}
-                          height={50}
-                          colorScale="ocean"
-                        />
-                      </p>
-                    </div>
-                  </section>
-                  <section>
-                    <PieChart
-                      title="Cost Burden Rates for Renters in 2015"
-                      subtitle={'Portland-Vancouver-Hillsboro, OR-WA'}
-                      data={chartData}
-                      innerRadius={90}
-                      dataLabel="label"
-                      dataValue="value"
-                    />
-                  </section>
-                </CivicStoryCard>
-              </div>
-            </Carousel>
-          </div>
-          <div className={buttonContainer}>
-            <Button>EXPLORE CIVIC ✨🚀✨</Button>
+                </div>
+              </CivicCardStack>
+            </div>
+            <div className={gridItem}>
+              <div className={issueStyle(3)}>Affordable Housing</div>
+              <CivicCardStack cards={3}>{sampleChart}</CivicCardStack>
+            </div>
+            <div className={gridItem}>
+              <div className={issueStyle(4)}>Transportation</div>
+              <CivicCardStack cards={3}>{sampleChart}</CivicCardStack>
+            </div>
           </div>
           <h1>
             CIVIC is an art project that could transform the way that we use
