528e910282498ef0430c5701e3d0758ad0972f48
David Mendoza <david@themendozaline.org>
2019-03-27 08:04:20 +0000

    Add Data Collection, Base Map, and Slide menus to SandboxDrawer component

--- packages/component-library/src/Sandbox/SandboxDrawer.js
+++ packages/component-library/src/Sandbox/SandboxDrawer.js
@@ -3,17 +3,29 @@ import { css } from "emotion";
 import Dropdown from "../Dropdown/Dropdown";
 import SandboxDateSelector from "./SandboxDateSelector";
 
-const title = css(`
-border-top: 1px solid #ddd;
-padding: .3rem .5rem;
-background: #eee;
-text-transform: capitalize;
-font-weight: bold;
+const menuOpen = css(`
+  flex-grow: 0;
+  width: 25%;
+  position: absolute;
+  right: 0;
+  z-index: 5;
+  top: 4.5%;
+  transition: 0.5s;
+`);
+
+const menuClosed = css(`
+  flex-grow: 0;
+  width: 5%;
+  position: absolute;
+  right: 0;
+  z-index: 5;
+  top: 4.5%;
+  transition: 0.5s;
 `);
 
 const SandboxDrawer = ({
   data,
-  onChange,
+  onChangeCheckbox,
   selectedSlide,
   selectedPackage,
   toggleDrawer,
@@ -22,78 +34,118 @@ const SandboxDrawer = ({
   slideData,
   fetchSlideByDate,
   foundationData,
-  defaultFoundation
+  defaultFoundation,
+  allSlides,
+  updatePackage,
+  selectedFoundation,
+  updateFoundation
 }) => {
-  const options = data.packages[selectedPackage].slides.map(slide => ({
-    value: slide,
-    label: data.slides[slide].name
-  }));
   return (
     <div
-      className={css(`
-    flex-grow: 0;
-    width: 20%;
-    position:relative;
-  `)}
+      className={drawerVisible ? menuOpen : menuClosed}
     >
+      {/* TOGGLE BUTTON  */}
       <div onClick={toggleDrawer}>
-        <div
-          className={css(`
+        <div className={css(`
           display: flex;
           align-items: center;
           justify-content: space-between;
-          padding: .2rem;
           text-transform: uppercase;
-          font-size: .8rem;
+          font-size: 1rem;
           cursor: pointer;
-        `)}
-        >
-          <span>data overlays</span>{" "}
-          <span className={css("font-size: 1.2rem; color: rgb(237,73,91);")}>
-            {drawerVisible ? "<" : ">"}
-          </span>
+          background: #EE495C;
+          color: #F3F2F3;
+          height: auto;
+        `)}>
+          <strong className={css(`
+            font-size: 1.1rem;
+            color: #F3F2F3;
+            padding: 5px 0 5px 5px;
+            line-height: 1.5;
+          `)}>
+            {drawerVisible ? "Close Menu" : "Open Menu"}
+          </strong>
         </div>
       </div>
+      {/* SIDE MENU */}
       {drawerVisible && (
-        <div
-          className={css(`
-        position: absolute;
-        display: block;
-        width: 250px;
-        z-index: 100;
-        right: 0;
-        top: 80px;
-        right: 15px;
-        background: white;
-
-        .Select.is-open { position: relative; z-index: 1000; }
-        .Select-menu-outer {
-          z-index: 9999;
-        }
-      `)}
-        >
-          <div className={css("position: relative; z-index: 5000;")}>
+        <div className={css(`
+          position: absolute;
+          display: block;
+          width: 100%;
+          z-index: 100;
+          background: #EEE;
+          height: 72vh;
+          overflow-y: auto;
+          background: #F3F2F3;
+          .Select.is-open { position: relative; z-index: 1000; }
+          .Select-menu-outer { z-index: 9999; }
+        `)}>
+          {/* DATA COLLECTION SELECTOR */}
+          <div className={css(`
+            position: relative;
+            z-index: 900;
+          `)}
+          >
+            <h2 className={css(`
+              color: #555;
+              text-transform: uppercase;
+              margin: 0 10px;
+            `)}>
+              Data Collections
+            </h2>
             <Dropdown
-              value={selectedSlide.join(",")}
-              options={options}
-              onChange={onChange}
-              multi
+              value={selectedPackage}
+              options={Object.keys(data.packages).map(p => ({
+                value: p,
+                label: p,
+              }))}
+              onChange={updatePackage}
               simpleValue
             />
           </div>
+          {/* BASE LAYER SELECTOR */}
+          <div className={css(`
+            position: relative;
+            z-index: 400;
+          `)}>
+            <h2 className={css(`
+              color: #555;
+              text-transform: uppercase;
+              margin: 0 10px;
+            `)}>
+              Base Layers
+            </h2>
+            <Dropdown
+              value={selectedFoundation}
+              options={data.packages[selectedPackage].foundations.map(
+                foundation => ({
+                  value: foundation,
+                  label: data.foundations[foundation].name,
+                })
+              )}
+              onChange={updateFoundation}
+              simpleValue
+            />
+          </div>
+          {/* BASE LAYER YEAR SELCTOR */}
           {foundationData && (
             <div>
-              <div className={title}>{defaultFoundation.name}</div>
-              <div
-                className={css(`
-              padding: .5rem;
-              font-size: .75rem;
-              color: #333;
-              position: relative;
-              z-index: 10
-              `)}
-              >
-                Date info driven by dropdown
+              <div className={css(`
+                border-top: 1px solid #ddd;
+                padding: .3rem .5rem;
+                text-transform: capitalize;
+                font-weight: bold;
+              `)}>
+                {"Year"}
+              </div>
+              <div className={css(`
+                padding: .5rem;
+                font-size: .75rem;
+                color: #333;
+                position: relative;
+                z-index: 300;
+              `)}>
                 <SandboxDateSelector
                   slide={defaultFoundation}
                   selectedSlideData={foundationData}
@@ -103,34 +155,65 @@ const SandboxDrawer = ({
               </div>
             </div>
           )}
-
-          {defaultSlides.map((slide, index) => {
-            let selectedSlideData = {};
-            if (slideData.length) {
-              selectedSlideData = slideData.find(sData => {
-                return sData[slide.name];
-              })[slide.name];
-            }
+          {/* SLIDE CHECKBOX INPUTS */}
+          <div className={css(`
+            position: relative;
+            z-index: 200;
+          `)}>
+            <h2 className={css(`
+              color: #555;
+              text-transform: uppercase;
+              margin: 0 10px;
+            `)}>
+              Slide Layers
+            </h2>
+          </div>
+          {allSlides.map((slide, index) => {
+            const selectedSlideData = !!slideData.find(slideDatum => slideDatum[slide.label])
+              ? slideData.find(slideDatum => slideDatum[slide.label])[slide.label]
+              : {};
+            const defaultGray = [238, 238, 238, 255];
+            const backgroundSlideColor = slide.color;
+            const formatBackgroundColor = arr => arr.reduce((acc,cur,i) => i < 3 ? acc + cur +"," : acc +"1)", "rgba(");
+            const slideBackGroundColor = formatBackgroundColor(backgroundSlideColor);
+            const blackTextColor = "rgba(0,0,0,1)";
+            const whiteTextColor = "rgba(255,255,255,1)";
+            const textColor = slideBackGroundColor === defaultGray ? blackTextColor : whiteTextColor;
             return (
               <div key={"slide-selector" + index}>
-                <div className={title}>{slide.name}</div>
-                <div
-                  className={css(`
-                  padding: .5rem;
-                  font-size: .75rem;
-                  color: #333;
-                  position: relative;
-                  z-index: ${5 - index}
-                  `)}
-                >
-                  Date info driven by dropdown
-                  <SandboxDateSelector
-                    selectedSlideData={selectedSlideData}
-                    slide={slide}
-                    fetchSlideByDate={fetchSlideByDate}
-                    type="slide"
+                <div className={css(`
+                  border-top: 1px solid #ddd;
+                  padding: .3rem .5rem;
+                  text-transform: capitalize;
+                  font-weight: bold;
+                  background: ${slideBackGroundColor};
+                  color:${textColor}
+                `)}>
+                  <input
+                    type="checkbox"
+                    name={slide.slideNumber}
+                    checked={slide.checked}
+                    onChange={onChangeCheckbox}
+                    className="input-checkbox"
                   />
+                  {`${slide.label} - Polygon`}
                 </div>
+                {selectedSlideData.slide_data && (
+                  <div className={css(`
+                    padding: .5rem;
+                    font-size: .75rem;
+                    color: #333;
+                    position: relative;
+                    z-index: ${10 - index}
+                  `)}>
+                    <SandboxDateSelector
+                      selectedSlideData={selectedSlideData}
+                      slide={slide}
+                      fetchSlideByDate={fetchSlideByDate}
+                      type="slide"
+                    />
+                  </div>
+                )}
               </div>
             );
           })}
