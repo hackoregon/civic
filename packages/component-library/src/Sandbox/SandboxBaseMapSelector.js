6c358495b73bd9c0a4f0938d23b224a7a15ab2ae
Paige Williams <paw145@humboldt.edu>
2019-04-21 18:57:09 -0700

    Refactor to functional component that accepts baseMapStyle na onBaseMapStyleChange as props; trigger onBaseMapStyleChange onChange.

--- packages/component-library/src/Sandbox/SandboxBaseMapSelector.js
+++ packages/component-library/src/Sandbox/SandboxBaseMapSelector.js
@@ -7,54 +7,31 @@ const baseMapMenu = css(`
   padding: 10px;
   bottom: 0;
   right: 0;
-  z-index: 4;
-  font-family: "Roboto Condensed",, sans-serif;
+  z-index: 2;
+  font-family: "Roboto Condensed", sans-serif;
 `);
 
-class SandboxBaseMapSelector extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      baseMapStyle: "light"
-    };
-    this.handleBaseMapStyleChange = this.handleBaseMapStyleChange.bind(this);
-  }
-
-  handleBaseMapStyleChange = baseMapStyleChangeEvent => {
-    console.log(baseMapStyleChangeEvent.target.value);
-    this.setState({
-      baseMapStyle: baseMapStyleChangeEvent.target.value
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <div className={baseMapMenu}>
-          <form>
-            <radiogroup>
-              <input
-                checked={this.state.baseMapStyle === "light"}
-                id="styles/hackoregon/cjiazbo185eib2srytwzleplg"
-                type="radio"
-                value="light"
-                onChange={this.handleBaseMapStyleChange}
-              />
-              <label for="light">Light</label>
-              <input
-                checked={this.state.baseMapStyle === "dark"}
-                id="mapbox://styles/mapbox/dark-v10"
-                type="radio"
-                value="dark"
-                onChange={this.handleBaseMapStyleChange}
-              />
-              <label for="dark">Dark</label>
-            </radiogroup>
-          </form>
-        </div>
-      </>
-    );
-  }
-}
+const SandboxBaseMapSelector = ({ onBaseMapStyleChange, baseMapStyle }) => {
+  return (
+    <>
+      <div className={baseMapMenu}>
+        <input
+          checked={baseMapStyle === "light"}
+          type="radio"
+          value="light"
+          onChange={onBaseMapStyleChange}
+        />
+        <label for="light">Light</label>
+        <input
+          checked={baseMapStyle === "dark"}
+          type="radio"
+          value="dark"
+          onChange={onBaseMapStyleChange}
+        />
+        <label for="dark">Dark</label>
+      </div>
+    </>
+  );
+};
 
 export default SandboxBaseMapSelector;
