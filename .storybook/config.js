a646534acc19adbab89f81eebd457a50bc2905ab
Jaron Heard <jaronheard@gmail.com>
2019-04-30 02:39:24 -0700

    update storybook to 5.0.11, add viewport add-on

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,10 +2,13 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
 import { configure } from "@storybook/react";
+import { addParameters } from "@storybook/react";
+
 import "../packages/component-library/assets/global.styles.css";
 
 function loadStories() {
   require("../packages/component-library/stories");
+  addParameters({ viewport: options });
 }
 
 configure(loadStories, module);
