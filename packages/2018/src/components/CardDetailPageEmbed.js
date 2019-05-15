9e24a625a2bc6eae8b7ffe8d4d42de386ae8b3af
Jaron Heard <jaronheard@gmail.com>
2019-05-15 02:18:40 -0700

    Embeddable Story Cards
    - Added route cards/slug/embed
    - Added embeddable component, similar to card view but without layout

---
+++ packages/2018/src/components/CardDetailPageEmbed.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router";
+import { PageLayout } from "@hackoregon/component-library";
+import CardRegistry from "../card-registry";
+
+const CardDetailPageEmbed = ({ params }) => {
+  const card = CardRegistry.find(params.slug);
+
+  if (card && card.component) {
+    const CardComponent = card.component;
+    return <CardComponent />;
+  }
+
+  return (
+    <div>
+      <h1>Card not found</h1>
+      <p>The card you are looking for doesn&apos;t exist.</p>
+      <Link to="/">Find more on CIVIC</Link>
+    </div>
+  );
+};
+
+CardDetailPageEmbed.propTypes = {
+  params: PropTypes.string
+};
+
+CardDetailPageEmbed.displayName = "CardDetailPage";
+
+export default CardDetailPageEmbed;
