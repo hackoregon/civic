e1624a0797a944224a49c03eae001e5ae9201799
Jaron Heard <jaronheard@gmail.com>
2019-05-15 23:24:43 -0700

    add story-card-generator

---
+++ _templates/story-card/with-prompt/prompt.js
@@ -0,0 +1,10 @@
+// see types of prompts:
+// https://github.com/enquirer/enquirer/tree/master/examples
+//
+module.exports = [
+  {
+    type: "input",
+    name: "message",
+    message: "What's your message?"
+  }
+];
