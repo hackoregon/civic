f453e7124e7a10afc4b2db718879e099c20bd942
Jaron Heard <jaronheard@gmail.com>
2019-05-19 01:15:30 -0700

    Lint 2018-local-elections package (#522)
    * lintfix 2018-local-elections
    
    * lint package - 2018-local-elections

--- packages/2018-local-elections/src/state/selectors/index.js
+++ packages/2018-local-elections/src/state/selectors/index.js
@@ -1,5 +1,4 @@
-import { createSelector } from "reselect";
-
+/* eslint-disable import/prefer-default-export */
 // The root state for a package is namespaced in the 2018 package
 // and the root state object when developing a package in isolation
 export const rootState = state => state.elections || state;
