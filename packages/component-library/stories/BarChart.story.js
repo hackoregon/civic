ba55fa48b4ff6a47525d5283e7ea531983d24e9c
Janice Bradford <janice.bradford@outlook.com>
2019-05-21 11:50:10 -0700

    WIP on Button story.

--- packages/component-library/stories/BarChart.story.js
+++ packages/component-library/stories/BarChart.story.js
@@ -1,55 +1,146 @@
 import React from "react";
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/react";
+import civicFormat from "../src/utils/civicFormat";
 
-import { withKnobs, text, object } from "@storybook/addon-knobs";
+import {
+  withKnobs,
+  text,
+  object,
+  boolean,
+  number,
+  optionsKnob as options
+} from "@storybook/addon-knobs";
+import notes from "./barchart.notes.md";
 import { BarChart } from "../src";
-
-const sampleSimpleData = [
-  { x: 5, y: 20 },
-  { x: 10, y: 30 },
-  { x: 15, y: 50 },
-  { x: 20, y: 40 }
-];
 
 export default () =>
   storiesOf("Component Lib|Charts/Bar Chart", module)
     .addDecorator(withKnobs)
-    .add("Standard", () => {
-      const GROUP_IDS = {
-        LABELS: "Labels",
-        DATA: "Data"
-      };
-      const title = text("Title", "Dogs with Money", GROUP_IDS.LABELS);
-      const subtitle = text(
-        "Subtitle",
-        "Dogs in Portland with a net worth greater than $1,000",
-        GROUP_IDS.LABELS
-      );
-      const xLabel = text("X-axis", "Year", GROUP_IDS.LABELS);
-      const yLabel = text("Y-axis", "Dogs", GROUP_IDS.LABELS);
-      const dataKey = text("Data key", "ye", GROUP_IDS.DATA);
-      const dataValue = text("Data values", "population", GROUP_IDS.DATA);
-      const data = object(
-        "Data",
-        [
-          { ye: 1994, population: 2000 },
-          { ye: 1995, population: 8000 },
-          { ye: 1996, population: 6000 },
-          { ye: 1997, population: 3000 },
-          { ye: 1998, population: 1000 }
-        ],
-        GROUP_IDS.DATA
-      );
-      return (
-        <BarChart
-          data={data}
-          dataKey={dataKey}
-          dataValue={dataValue}
-          title={title}
-          subtitle={subtitle}
-          xLabel={xLabel}
-          yLabel={yLabel}
-        />
-      );
-    });
+    .add(
+      "Standard",
+      () => {
+        const GROUP_IDS = {
+          LABELS: "Labels",
+          DATA: "Data"
+        };
+        const title = text("Title", "Dogs with Money", GROUP_IDS.LABELS);
+        const subtitle = text(
+          "Subtitle",
+          "Dogs in Portland with a net worth greater than $1,000",
+          GROUP_IDS.LABELS
+        );
+        const xLabel = text("X-axis label", "Year", GROUP_IDS.LABELS);
+        const yLabel = text("Y-axis label", "Dogs", GROUP_IDS.LABELS);
+        const dataKey = text("Data key", "ye", GROUP_IDS.DATA);
+        const dataValue = text("Data values", "population", GROUP_IDS.DATA);
+        const data = object(
+          "Data",
+          [
+            { ye: 1994, population: 2000 },
+            { ye: 1995, population: 8000 },
+            { ye: 1996, population: 6000 },
+            { ye: 1997, population: 3000 },
+            { ye: 1998, population: 1000 }
+          ],
+          GROUP_IDS.DATA
+        );
+        return (
+          <BarChart
+            data={data}
+            dataKey={dataKey}
+            dataValue={dataValue}
+            title={title}
+            subtitle={subtitle}
+            xLabel={xLabel}
+            yLabel={yLabel}
+          />
+        );
+      },
+      { notes }
+    )
+    .add(
+      "Custom",
+      () => {
+        const GROUP_IDS = {
+          LABELS: "Labels",
+          DATA: "Data",
+          CUSTOM: "Custom"
+        };
+        const title = text("Title", "Dogs with Money", GROUP_IDS.LABELS);
+        const subtitle = text(
+          "Subtitle",
+          "Dogs in Portland with a net worth greater than $1,000",
+          GROUP_IDS.LABELS
+        );
+        const xLabel = text("X-axis label", "Year", GROUP_IDS.LABELS);
+        const yLabel = text("Y-axis label", "Dogs", GROUP_IDS.LABELS);
+        const dataKey = text("Data key", "ye", GROUP_IDS.DATA);
+        const dataValue = text("Data values", "population", GROUP_IDS.DATA);
+        const data = object(
+          "Data",
+          [
+            { ye: 1994, population: 2000 },
+            { ye: 1995, population: 8000 },
+            { ye: 1996, population: 6000 },
+            { ye: 1997, population: 3000 },
+            { ye: 1998, population: 1000 }
+          ],
+          GROUP_IDS.DATA
+        );
+        const barWidth = number("Bar width", 37, {}, GROUP_IDS.CUSTOM);
+        const loading = boolean("Loading...", false, GROUP_IDS.CUSTOM);
+        const error = boolean("Error", false, GROUP_IDS.CUSTOM);
+
+        const xFormatterOptions = {
+          numeric: civicFormat.numeric,
+          year: civicFormat.year
+          // Other format types:
+          //  percentage: civicFormat.percentage,
+          //  dollars: civicFormat.dollars,
+          //  titleCase: civicFormat.titleCase,
+          //  unformatted: civicFormat.unformatted,
+          //  monthYear: civicFormat.monthYear
+        };
+        const optionRadio = options(
+          "Data key format",
+          xFormatterOptions,
+          civicFormat.year,
+          { display: "radio" },
+          GROUP_IDS.CUSTOM
+        );
+
+        // Y is "Data value format"
+
+        // var civicFormat = {
+        //   numeric: numeric,
+        //   year: year,
+        //   percentage: percentage,
+        //   dollars: dollars,
+        //   titleCase: titleCase,
+        //   unformatted: unformatted,
+        //   monthYear: monthYear
+        // };
+
+        // xNumberFormatter={x => civicFormat.year(x)}
+        // yNumberFormatter={y => civicFormat.dollars(y)}
+
+        return (
+          <BarChart
+            data={data}
+            dataKey={dataKey}
+            dataValue={dataValue}
+            title={title}
+            subtitle={subtitle}
+            xLabel={xLabel}
+            yLabel={yLabel}
+            barWidth={barWidth}
+            loading={loading}
+            error={error}
+            // This works:
+            xNumberFormatter={value => `${value}%`}
+          />
+        );
+      },
+      { notes }
+    );
---
+++ packages/component-library/stories/barchart.notes.md
@@ -0,0 +1,23 @@
+# BarChart Component
+
+## Standard
+
+The Standard story shows the standard usage of BarChart for the CIVIC platform; it includes standard styling. The properties that can be passed to BarChart are:
+
+- Title
+- Subtitle
+- X-axis label
+- Y-axis label
+- Data key
+- Data value
+- Data
+
+## Custom
+
+The Custom story shows all possible properties that can be passed to the BarChart for customization. They include the properties in the Standard, plus these:
+
+- Loading message
+- Error message
+- xNumberFormatter (year format) (use an options list)
+- yNumberFormatter (number format) (use an options list)
+- Bar width
