bad30db0cdb775664e01920e3e2fedbdab9c59db
Janice Bradford <janice.bradford@outlook.com>
2019-05-21 13:09:03 -0700

    Button style guide.

--- packages/component-library/stories/styleGuideStories/UIComponentsStyle.story.js
+++ packages/component-library/stories/styleGuideStories/UIComponentsStyle.story.js
@@ -15,6 +15,17 @@ const UIComponentsStyle = () => (
     <h4>Text Input Field</h4>
     <h2 id="button">Button</h2>
     <p>Style information for buttons.</p>
+    <p>Buttons are a call to action. They should not be used for links.</p>
+    <p>
+      Button labels should be an active verb. Write labels in sentence case.
+    </p>
+    <p>
+      Sometimes an image, such as an icon or emoji, better represents what the
+      button does. Always put descriptive text near the button if the label is
+      non-textual. To meet accessibility requirements, use an aria-label tag.
+    </p>
+    <p />
+
     <h2>Checkbox</h2>
     <p>Style information for checkboxes.</p>
     <h2>Dropdown List</h2>
