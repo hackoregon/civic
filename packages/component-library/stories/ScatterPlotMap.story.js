1c941d0e370672f26325a2f376b3047a70b4fb0b
Paige Williams <paw145@humboldt.edu>
2019-05-24 19:24:47 -0700

    Refactor story to include notes and group knobs.

--- packages/component-library/stories/ScatterPlotMap.story.js
+++ packages/component-library/stories/ScatterPlotMap.story.js
@@ -1,10 +1,16 @@
 import React from "react";
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/react";
-import { withKnobs, number, boolean } from "@storybook/addon-knobs";
+import { withKnobs, number, boolean, object } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 import { checkA11y } from "@storybook/addon-a11y";
 import { BaseMap, ScatterPlotMap, MapTooltip, DemoJSONLoader } from "../src";
+import notes from "./ScatterPlotMap.notes.md";
+
+const GROUP_IDS = {
+  MARKER: "Marker",
+  DATA: "Data"
+};
 
 const opacityOptions = {
   range: true,
@@ -44,78 +50,115 @@ const mapData = [
   "https://service.civicpdx.org/neighborhood-development/sandbox/slides/bikecounts/"
 ];
 
-const demoMap = () => (
-  <DemoJSONLoader urls={mapData}>
-    {data => {
-      const opacity = number("Opacity:", 0.1, opacityOptions);
-      const radiusScale = number("Radius Scale:", 1, radiusScaleOptions);
-      const stroked = boolean("Stroke Only:", false);
-      const getLineWidth = number("Line Width:", 1, lineWidthOptions);
-      return (
-        <BaseMap>
-          <ScatterPlotMap
-            data={data.slide_data.features}
-            getPosition={getPosition}
-            opacity={opacity}
-            getFillColor={getFillColor}
-            getLineColor={getLineColor}
-            getRadius={getCircleRadius}
-            radiusScale={radiusScale}
-            stroked={stroked}
-            getLineWidth={getLineWidth}
-            autoHighlight
-            highlightColor={highlightColor}
-            onLayerClick={info =>
-              action("Layer clicked:", { depth: 2 })(info, info.object)
-            }
-          />
-        </BaseMap>
-      );
-    }}
-  </DemoJSONLoader>
-);
-
-const tooltipMap = () => (
-  <DemoJSONLoader urls={mapData}>
-    {data => {
-      const opacity = number("Opacity:", 0.1, opacityOptions);
-      const radiusScale = number("Radius Scale:", 1, radiusScaleOptions);
-      const stroked = boolean("Stroke Only:", false);
-      const getLineWidth = number("Line Width:", 1, lineWidthOptions);
-      return (
-        <BaseMap>
-          <ScatterPlotMap
-            data={data.slide_data.features}
-            getPosition={getPosition}
-            opacity={opacity}
-            getFillColor={getFillColor}
-            getLineColor={getLineColor}
-            getRadius={getCircleRadius}
-            radiusScale={radiusScale}
-            stroked={stroked}
-            getLineWidth={getLineWidth}
-            autoHighlight
-            highlightColor={highlightColor}
-            onLayerClick={info =>
-              action("Layer clicked:", { depth: 2 })(info, info.object)
-            }
-          >
-            <MapTooltip
-              primaryName="Count Time"
-              primaryField="count_time"
-              secondaryName="Bike Count"
-              secondaryField="year_2017"
-            />
-          </ScatterPlotMap>
-        </BaseMap>
-      );
-    }}
-  </DemoJSONLoader>
-);
-
 export default () =>
   storiesOf("Component Lib|Maps/Scatterplot Map", module)
     .addDecorator(withKnobs)
     .addDecorator(checkA11y)
-    .add("Simple usage", demoMap)
-    .add("With tooltip", tooltipMap);
+    .add(
+      "Standard",
+      () => {
+        const opacity = number(
+          "Opacity:",
+          0.1,
+          opacityOptions,
+          GROUP_IDS.MARKER
+        );
+        const radiusScale = number(
+          "Radius Scale:",
+          1,
+          radiusScaleOptions,
+          GROUP_IDS.MARKER
+        );
+        const stroked = boolean("Stroke Only:", false, GROUP_IDS.MARKER);
+        const getLineWidth = number(
+          "Line Width:",
+          1,
+          lineWidthOptions,
+          GROUP_IDS.MARKER
+        );
+        return (
+          <DemoJSONLoader urls={mapData}>
+            {allData => {
+              const getPositionKnob = number(
+                "Position",
+                getPosition,
+                GROUP_IDS.DATA
+              );
+              const data = object(
+                "Data",
+                allData.slide_data.features,
+                GROUP_IDS.DATA
+              );
+              return (
+                <BaseMap>
+                  <ScatterPlotMap
+                    data={data}
+                    getPosition={getPositionKnob}
+                    opacity={opacity}
+                    getFillColor={getFillColor}
+                    getLineColor={getLineColor}
+                    getRadius={getCircleRadius}
+                    radiusScale={radiusScale}
+                    stroked={stroked}
+                    getLineWidth={getLineWidth}
+                    autoHighlight
+                    highlightColor={highlightColor}
+                    onLayerClick={info =>
+                      action("Layer clicked:", { depth: 2 })(info, info.object)
+                    }
+                  />
+                </BaseMap>
+              );
+            }}
+          </DemoJSONLoader>
+        );
+      },
+      { notes }
+    )
+    .add(
+      "Examples: With Tooltip",
+      () => {
+        return (
+          <DemoJSONLoader urls={mapData}>
+            {data => {
+              const opacity = number("Opacity:", 0.1, opacityOptions);
+              const radiusScale = number(
+                "Radius Scale:",
+                1,
+                radiusScaleOptions
+              );
+              const stroked = boolean("Stroke Only:", false);
+              const getLineWidth = number("Line Width:", 1, lineWidthOptions);
+              return (
+                <BaseMap>
+                  <ScatterPlotMap
+                    data={data.slide_data.features}
+                    getPosition={getPosition}
+                    opacity={opacity}
+                    getFillColor={getFillColor}
+                    getLineColor={getLineColor}
+                    getRadius={getCircleRadius}
+                    radiusScale={radiusScale}
+                    stroked={stroked}
+                    getLineWidth={getLineWidth}
+                    autoHighlight
+                    highlightColor={highlightColor}
+                    onLayerClick={info =>
+                      action("Layer clicked:", { depth: 2 })(info, info.object)
+                    }
+                  >
+                    <MapTooltip
+                      primaryName="Count Time"
+                      primaryField="count_time"
+                      secondaryName="Bike Count"
+                      secondaryField="year_2017"
+                    />
+                  </ScatterPlotMap>
+                </BaseMap>
+              );
+            }}
+          </DemoJSONLoader>
+        );
+      },
+      { notes }
+    );
