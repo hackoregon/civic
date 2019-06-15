5e7b0fab09abd740163d43d21956495802e48fc9
Dianna Faulk <diannafaulk@gmail.com>
2019-06-15 14:55:11 -0700

    Add simple emotion style to Orb

--- packages/project-disaster-trail/src/components/Game/Orb.js
+++ packages/project-disaster-trail/src/components/Game/Orb.js
@@ -1,5 +1,33 @@
-import React from "react";
+/** @jsx jsx */
+import PropTypes from "prop-types";
+import { jsx, css } from "@emotion/core";
 
-const Orb = () => <div>Orb</div>;
+const defaultOrb = css`
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  background-color: blue;
+  position: absolute;
+`;
+
+const Orb = ({ x, y }) => {
+  const location = css`
+    top: ${y}px;
+    left: ${x}px;
+  `;
+  return (
+    <div
+      css={css`
+        ${defaultOrb};
+        ${location};
+      `}
+    />
+  );
+};
+
+Orb.propTypes = {
+  x: PropTypes.number,
+  y: PropTypes.number
+};
 
 export default Orb;
