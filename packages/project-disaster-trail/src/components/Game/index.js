315ff3167a9e3522ae6c5b6cd83a18420aec4483
Dianna Faulk <diannafaulk@gmail.com>
2019-06-18 15:19:11 -0700

    Add example orb

--- packages/project-disaster-trail/src/components/Game/index.js
+++ packages/project-disaster-trail/src/components/Game/index.js
@@ -1,9 +1,15 @@
 /* eslint-disable import/no-named-as-default */
 import React from "react";
+import Orb from "./Orb";
 
 import "@hackoregon/component-library/assets/global.styles.css";
 
-const Game = () => <h1>This is the game</h1>;
+const Game = () => (
+  <div>
+    <h1>This is the game</h1>
+    <Orb x={200} y={400} />
+  </div>
+);
 
 Game.displayName = "Game";
 
