0831c8eb53293bb8c606a9b753beb759dfac2474
Jaron Heard <jaronheard@gmail.com>
2019-07-04 13:29:38 -0700

    revise template storycard

--- packages/2019-template/src/components/TemplateCard/index.js
+++ packages/2019-template/src/components/TemplateCard/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { CivicStoryCard, LineChart } from "@hackoregon/component-library";
-import { css } from "emotion";
 
 import { fetchTemplateData } from "../../state/template-data/actions";
 import {
@@ -10,20 +9,53 @@ import {
   getTemplateData
 } from "../../state/template-data/selectors";
 
-const cardLoading = css`
-  width: 100%;
-  padding: 50px;
-  text-align: center;
-  background: #eee;
-`;
+const TemplateCardVisualization = ({ isLoading, templateData }) => (
+  <React.Fragment>
+    {!isLoading && templateData && (
+      <LineChart
+        data={templateData}
+        dataKey="year"
+        dataValue="ridership"
+        dataSeries="series"
+        title="Template Plot"
+      />
+    )}
+  </React.Fragment>
+);
 
-const cardError = css`
-  width: 100%;
-  padding: 50px;
-  text-align: center;
-  background: #fdd;
-  border: 1px solid #c99;
-`;
+const TemplateCardFull = {
+  title: "(Story Card #1, Issue #000) Template Card Title",
+  slug: "template-card-slug",
+  introText: (
+    <p>
+      Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
+      accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab
+      illo inventore veritatis et quasi architecto beatae vitae dicta sunt,
+      explicabo.
+    </p>
+  ),
+  visualization: TemplateCardVisualization,
+  shareText: "The future of story cards is now! Find out more at this link",
+  tags: ["transportation", "bus", "rail", "portland"],
+  selector: null,
+  analysis: (
+    <p>
+      Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
+      accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab
+      illo inventore veritatis et quasi architecto beatae vitae dicta sunt,
+      explicabo.
+    </p>
+  ),
+  metadata: "https://link.to/metadata",
+  resources: [
+    { link: "http://www.hackoregon.org", description: "Hack Oregon" },
+    {
+      link: "https://www.civicsoftwarefoundation.org",
+      description: "Civic Software Foundation"
+    },
+    { link: "https://www.civicplatform.org", description: "Civic Platform" }
+  ]
+};
 
 class TemplateCard extends Component {
   componentDidMount() {
@@ -34,39 +66,26 @@ class TemplateCard extends Component {
   render() {
     const { isLoading, templateData } = this.props;
 
-    if (isLoading) {
-      return <div className={cardLoading}>Loading...</div>;
-    }
-
-    if (!templateData) {
-      return <div className={cardError}>Could not render Template data</div>;
-    }
-
     return (
       <CivicStoryCard
-        title="(Story Card #1, Issue #000) Template Card Title"
-        slug="template-card-slug"
+        title={TemplateCardFull.title}
+        slug={TemplateCardFull.slug}
       >
-        <p>
-          Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt, explicabo.
-        </p>
-
-        <LineChart
-          data={templateData}
-          dataKey="year"
-          dataValue="ridership"
-          dataSeries="series"
-          title="Template Plot"
-        />
+        <React.Fragment>
+          {TemplateCardFull.introText}
+          {TemplateCardFull.selector}
+          <TemplateCardFull.visualization
+            isLoading={isLoading}
+            templateData={templateData}
+          />
+        </React.Fragment>
       </CivicStoryCard>
     );
   }
 }
 
 TemplateCard.displayName = "TemplateCard";
+
 TemplateCard.propTypes = {
   init: PropTypes.func,
   isLoading: PropTypes.bool,
