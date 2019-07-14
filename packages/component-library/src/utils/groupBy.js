23424f0571e4fe5fdf0cc377cf0c6170895c9632
Paige Williams <paw145@humboldt.edu>
2019-07-14 12:48:45 -0700

    Sort objects by key; tests passing

--- packages/component-library/src/utils/groupBy.js
+++ packages/component-library/src/utils/groupBy.js
@@ -2,11 +2,12 @@
 function groupBy
  @data: array of objects with the same properties
  @groupByKey: the key that the objects should be grouped by
+ @sortByKey: the key that the objects should be sorted by
 
  This component is used by the horizontal bar chart to transform data for the stacked bar chart. It takes an array of objects and transforms it into an array of arrays of objects where each array containes one of each of the different values based on the groupByKey.
  */
 
-const groupBy = (data, groupByKey) => {
+const groupBy = (data, groupByKey, sortByKey) => {
   const groupedObj = data.reduce(
     (result, item) => ({
       ...result,
@@ -14,7 +15,10 @@ const groupBy = (data, groupByKey) => {
     }),
     {}
   );
-  return Object.keys(groupedObj).map(key => groupedObj[key]);
+  return Object.keys(groupedObj).map(key => {
+    const arr = groupedObj[key];
+    return arr.sort((a, b) => (a[sortByKey] > b[sortByKey] ? 1 : -1));
+  });
 };
 
 export default groupBy;
--- packages/component-library/src/utils/groupBy.test.js
+++ packages/component-library/src/utils/groupBy.test.js
@@ -15,17 +15,17 @@ const SAMPLE_DATA = [
 const FORMATTED_DATA = [
   [
     { name: "Poodle", x: "2017", y: 7 },
-    { name: "Terrier", x: "2017", y: 2 },
-    { name: "Bulldog", x: "2017", y: 3 }
-  ],
-  [
     { name: "Poodle", x: "2018", y: 2 },
-    { name: "Terrier", x: "2018", y: 5 },
-    { name: "Bulldog", x: "2018", y: 4 }
+    { name: "Poodle", x: "2019", y: 1 }
   ],
   [
-    { name: "Poodle", x: "2019", y: 1 },
-    { name: "Terrier", x: "2019", y: 2 },
+    { name: "Terrier", x: "2017", y: 2 },
+    { name: "Terrier", x: "2018", y: 5 },
+    { name: "Terrier", x: "2019", y: 2 }
+  ],
+  [
+    { name: "Bulldog", x: "2017", y: 3 },
+    { name: "Bulldog", x: "2018", y: 4 },
     { name: "Bulldog", x: "2019", y: 3 }
   ]
 ];
@@ -40,34 +40,36 @@ const UNSTRUCTURED_DATA = [
   { name: "Terrier", x: "2017", y: 2 },
   { name: "Bulldog", x: "2017", y: 3 },
   { name: "Bulldog", x: "2019", y: 3 },
-  { name: "Poodle", x: "2025", y: 6 }
+  { name: "Poodle", x: "2035", y: 10 }
 ];
 
 const FORMATTED_UNSTRUCTURED_DATA = [
   [
     { name: "Poodle", x: "2017", y: 7 },
-    { name: "Terrier", x: "2017", y: 2 },
-    { name: "Bulldog", x: "2017", y: 3 }
-  ],
-  [
     { name: "Poodle", x: "2018", y: 2 },
-    { name: "Terrier", x: "2018", y: 5 },
-    { name: "Bulldog", x: "2018", y: 4 }
+    { name: "Poodle", x: "2019", y: 1 },
+    { name: "Poodle", x: "2035", y: 10 }
   ],
   [
-    { name: "Terrier", x: "2019", y: 2 },
-    { name: "Poodle", x: "2019", y: 1 },
-    { name: "Bulldog", x: "2019", y: 3 }
+    { name: "Terrier", x: "2017", y: 2 },
+    { name: "Terrier", x: "2018", y: 5 },
+    { name: "Terrier", x: "2019", y: 2 }
   ],
-  [{ name: "Poodle", x: "2025", y: 6 }]
+  [
+    { name: "Bulldog", x: "2017", y: 3 },
+    { name: "Bulldog", x: "2018", y: 4 },
+    { name: "Bulldog", x: "2019", y: 3 }
+  ]
 ];
 
 describe("groupBy", () => {
   it("should format data properly", () => {
-    expect(groupBy(SAMPLE_DATA, "x")).to.eql(FORMATTED_DATA);
+    expect(groupBy(SAMPLE_DATA, "name", "x")).to.eql(FORMATTED_DATA);
   });
 
-  it("should format unstructured data properly", () => {
-    expect(groupBy(UNSTRUCTURED_DATA, "x")).to.eql(FORMATTED_UNSTRUCTURED_DATA);
+  it.only("should format unstructured data properly", () => {
+    expect(groupBy(UNSTRUCTURED_DATA, "name", "x")).to.eql(
+      FORMATTED_UNSTRUCTURED_DATA
+    );
   });
 });
