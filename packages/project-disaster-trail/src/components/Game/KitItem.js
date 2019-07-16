b633da123c0efa31eeee2ef5027770146712cff6
Dianna Faulk <diannafaulk@gmail.com>
2019-07-15 17:56:50 -0700

    Add fill kit item method

--- packages/project-disaster-trail/src/components/Game/KitItem.js
+++ packages/project-disaster-trail/src/components/Game/KitItem.js
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import { Component } from "react";
 import { css, jsx } from "@emotion/core";
 import { PropTypes } from "prop-types";
 
@@ -15,46 +16,65 @@ const KitItemStyle = css`
   position: absolute;
 `;
 
-const KitItem = ({ emptySvg, fullSvg }) => {
-  const EmptyKitItem = css`
-    background-image: url(${emptySvg});
-    background-size: cover;
-    opacity: 1;
-    transition: all 1s ease;
+class KitItem extends Component {
+  state = {
+    filledItem: false
+  };
 
-    &:hover {
-      opacity: 0;
-    }
-  `;
+  fillKitItem = () => {
+    this.setState({ filledItem: true });
+  };
 
-  const ColorKitItem = css`
-    background-image: url(${fullSvg});
-    background-size: cover;
-    opacity: 0;
-    transition: all 1s ease;
+  render() {
+    const { emptySvg, fullSvg } = this.props;
+    const { filledItem } = this.state;
 
-    &:hover {
+    const EmptyKitItem = css`
+      background-image: url(${emptySvg});
+      background-size: cover;
       opacity: 1;
-    }
-  `;
+      transition: all 1s ease;
 
-  return (
-    <div css={ImagesContainer}>
-      <div
-        css={css`
-          ${KitItemStyle};
-          ${EmptyKitItem}
-        `}
-      />
-      <div
-        css={css`
-          ${KitItemStyle};
-          ${ColorKitItem}
-        `}
-      />
-    </div>
-  );
-};
+      &.filled-item {
+        opacity: 0;
+      }
+    `;
+
+    const ColorKitItem = css`
+      background-image: url(${fullSvg});
+      background-size: cover;
+      opacity: 0;
+      transition: all 1s ease;
+
+      &.filled-item {
+        opacity: 1;
+      }
+    `;
+
+    /* eslint-disable jsx-a11y/click-events-have-key-events */
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
+    return (
+      <div css={ImagesContainer} onClick={this.fillKitItem}>
+        <div
+          css={css`
+            ${KitItemStyle};
+            ${EmptyKitItem}
+          `}
+          className={filledItem ? "filled-item" : ""}
+        />
+        <div
+          css={css`
+            ${KitItemStyle};
+            ${ColorKitItem}
+          `}
+          className={filledItem ? "filled-item" : ""}
+        />
+      </div>
+    );
+    /* eslint-enable jsx-a11y/click-events-have-key-events */
+    /* eslint-enable jsx-a11y/no-static-element-interactions */
+  }
+}
 
 KitItem.propTypes = {
   emptySvg: PropTypes.node,
