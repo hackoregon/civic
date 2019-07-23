3e074a606a5b501d1eee623c7319706a9fc055ac
Jaron Heard <jaronheard@gmail.com>
2019-07-22 21:07:34 -0700

    Add 2019-transportation package (#672)
    * Change default civic-card layout to full, simplify 2019-template index
    
    * Disable no-unused-expressions for test files
    
    https://stackoverflow.com/questions/37558795/nice-way-to-get-rid-of-no-unused-expressions-linter-error-with-chai
    
    * add 2019-transportation package files
    
    * add 2019-transportation package to 2018 package

--- packages/2019-transportation/src/index.js
+++ packages/2019-transportation/src/index.js
@@ -3,17 +3,12 @@ import Routes from "./routes";
 import Reducers from "./state";
 
 // TODO: Update me before commiting a new card
-import TemplateCard from "./components/TemplateCard";
-import DemoCard from "./components/DemoCard";
+import TransportationCard from "./components/TransportationCard";
 
 const CardRegistry = [
   {
-    slug: "template-card",
-    component: TemplateCard
-  },
-  {
-    slug: "demo-card",
-    component: DemoCard
+    slug: "transportation-card",
+    component: TransportationCard
   }
 ];
 
