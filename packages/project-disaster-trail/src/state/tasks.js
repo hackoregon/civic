9fa9f91a3e7fb2171d1e6e2471c85779020f2c03
Dianna Faulk <diannafaulk@gmail.com>
2019-07-26 15:28:26 -0700

    Add basic actions and refactor

--- packages/project-disaster-trail/src/state/tasks.js
+++ packages/project-disaster-trail/src/state/tasks.js
@@ -1,27 +1,55 @@
 import { createReducer } from "redux-starter-kit";
+import { shuffle } from "lodash";
 
-import TASKS from "../constants/tasks";
+import { tasks, tasksForEnvironment, URBAN } from "../constants/tasks";
 
-const defaultEnv = "urban";
-const defaultSaveYourself = [
-  TASKS.tasksForEnvironment[defaultEnv].saveYourself
-];
+const defaultEnv = URBAN;
+const defaultSaveYourself = [tasksForEnvironment[defaultEnv].saveYourself];
 
 // INITIAL STATE
 const initialState = {
-  ...TASKS,
+  tasks,
+  tasksForEnvironment,
   activeEnvironment: defaultEnv,
-  taskOrder: defaultSaveYourself,
+  taskOrder: shuffle(defaultSaveYourself),
   activeTask: 0
 };
 
 // CONSTANTS
+const actionTypes = {
+  CHANGE_ENVIRONMENT: "CHANGE_ENVIRONMENT",
+  DO_NEXT_TASK: "DO_NEXT_TASK"
+};
 
 // ACTIONS
+export const changeEnvironment = nextEnvironmentId => dispatch => {
+  dispatch({ type: actionTypes.CHANGE_ENVIRONMENT, nextEnvironmentId });
+};
+export const doNextTask = taskChoice => dispatch => {
+  dispatch({ type: actionTypes.DO_NEXT_TASK, taskChoice });
+};
 
 // REDUCERS
-export const settings = createReducer(initialState, {});
+/* eslint-disable no-param-reassign */
+export const tasksReducer = createReducer(initialState, {
+  [actionTypes.CHANGE_ENVIRONMENT]: (state, action) => {
+    const newTasks = tasksForEnvironment[action.nextEnvironmentId];
 
-export default settings;
+    state.activeEnvironment = action.nextEnvironmentId;
+    state.taskOrder = shuffle(newTasks);
+    state.activeTask = 0;
+  },
+  [actionTypes.DO_NEXT_TASK]: (state, action) => {
+    state.activeTask += 1;
+    const noNextTask = state.taskOrder[state.activeTask];
+
+    if (noNextTask) {
+      state.taskOrder.push(action.taskChoice);
+    }
+  }
+});
+/* eslint-enable no-param-reassign */
+
+export default tasksReducer;
 
 // SELECTORS
