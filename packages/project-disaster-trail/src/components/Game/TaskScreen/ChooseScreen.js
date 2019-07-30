17cdcb7616bac4bac9ca3882d4208be8583a1f5d
Dianna Faulk <diannafaulk@gmail.com>
2019-07-29 22:03:56 -0700

    Choose next task

--- packages/project-disaster-trail/src/components/Game/TaskScreen/ChooseScreen.js
+++ packages/project-disaster-trail/src/components/Game/TaskScreen/ChooseScreen.js
@@ -1,33 +1,73 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { PropTypes } from "prop-types";
 import { connect } from "react-redux";
 import {
   getTaskOrder,
   getActiveTask,
+  getActiveEnvironment,
+  getTasksForEnvironment,
   getCompletedTasks
 } from "../../../state/tasks";
 
-const ChooseScreen = ({ taskOrder, activeTask, completedTasks, goToTask }) => {
-  let nextTask = taskOrder[activeTask];
-  if (!nextTask) {
-    nextTask = "fire";
-  }
+const ChooseScreen = ({
+  taskOrder,
+  activeTask,
+  activeEnvironment,
+  tasksForEnvironment,
+  completedTasks,
+  goToTask
+}) => {
+  const mapTasksToButton = task => (
+    <button
+      key={task}
+      type="button"
+      onClick={() => {
+        goToTask(task);
+      }}
+    >
+      {task}
+    </button>
+  );
+
+  const tasksTodo = taskOrder.join(", ").toString();
+  const nextTask = taskOrder[activeTask];
+
+  // All possible tasks for the game environment
+  const saveYourselfTasks = tasksForEnvironment[
+    activeEnvironment
+  ].saveYourself.map(mapTasksToButton);
+  const saveOthersTasks = tasksForEnvironment[activeEnvironment].saveOthers.map(
+    mapTasksToButton
+  );
+  const possibleTaskButtons = [].concat(saveYourselfTasks, saveOthersTasks);
 
   return (
     <div>
       <h2>Choose Screen</h2>
-      <h3>Tasks to be done: [ {taskOrder.join(", ").toString()} ]</h3>
-      <h3>Next Task: {nextTask}</h3>
+      {nextTask && (
+        <h3>
+          Tasks to be done first in {activeEnvironment} environment: [{" "}
+          {tasksTodo} ]
+        </h3>
+      )}
+      <h3>Have you saved yourself yet? {nextTask ? "nope" : "yep!"}</h3>
+      <h3>Next Task: {nextTask || "vote below!"}</h3>
+      {nextTask ? (
+        <button
+          type="button"
+          onClick={() => {
+            goToTask(nextTask);
+          }}
+        >
+          Go to task
+        </button>
+      ) : (
+        <Fragment>
+          <h3>Choose next task for {activeEnvironment} environment:</h3>
+          {possibleTaskButtons}
+        </Fragment>
+      )}
       <h3>Complete Tasks: [{completedTasks.join(", ").toString()} ]</h3>
-
-      <button
-        type="button"
-        onClick={() => {
-          goToTask(nextTask);
-        }}
-      >
-        Go to task
-      </button>
     </div>
   );
 };
@@ -35,6 +75,8 @@ const ChooseScreen = ({ taskOrder, activeTask, completedTasks, goToTask }) => {
 ChooseScreen.propTypes = {
   taskOrder: PropTypes.arrayOf(PropTypes.string),
   activeTask: PropTypes.number,
+  activeEnvironment: PropTypes.string,
+  tasksForEnvironment: PropTypes.shape({}),
   completedTasks: PropTypes.arrayOf(PropTypes.string),
   goToTask: PropTypes.func
 };
@@ -42,6 +84,8 @@ ChooseScreen.propTypes = {
 const mapStateToProps = state => ({
   taskOrder: getTaskOrder(state),
   activeTask: getActiveTask(state),
+  activeEnvironment: getActiveEnvironment(state),
+  tasksForEnvironment: getTasksForEnvironment(state),
   completedTasks: getCompletedTasks(state)
 });
 
