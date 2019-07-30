a04a9b400f9a785f31794722f4241b3ded7286c7
Janice Bradford <janice.bradford@outlook.com>
2019-07-30 15:40:39 -0700

    WIP on RadioButtonGroup component.

--- packages/component-library/src/RadioButtonGroup/RadioButtonGroup.js
+++ packages/component-library/src/RadioButtonGroup/RadioButtonGroup.js
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { jsx, css } from "@emotion/core";
-// import React from 'react'; why isn't this needed
 import PropTypes from "prop-types";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import MaterialRadio from "@material-ui/core/Radio";
@@ -13,21 +12,11 @@ const radioButtonGroupClass = css`
   cursor: pointer;
 `;
 
-/*
-Use the map function to read an array of labels and create a radio button for each one.
-*/
-/*
-const labels = ["Label 1", "Label 2", "Label 3"];
-const buildGroup = labels.map(label => <li>{label}</li>);
-*/
-// RadioGroup has a prop aria-label="?label"
-
 const RadioButtonGroup = ({
-  // labels, an array of labels - create a radio btn for each label
-  label, // use labels instead of label?
-  onChange,
-  disabled,
-  value, // value of the selected radio button - make this a knob
+  labels, // an array of labels - create a radio btn for each label
+  onChange, // for the group
+  disabled, // disable the group of btns
+  value, // the value of the selected btn
   grpLabel,
   labelPlacement
 }) => (
@@ -37,45 +26,30 @@ const RadioButtonGroup = ({
     value={value}
     inputProps={{ "aria-label": { grpLabel } }}
   >
-    <FormControlLabel
-      // value={labels}
-      value={label}
-      control={
-        <MaterialRadio
-          checked={value} // how do I set one btn to be selected? required
-          disabled={disabled} // can I disable a specific btn? Do I want to?
-          css={radioButtonGroupClass}
-          inputProps={{ "aria-label": { label } }}
-        />
-      }
-      // label={labels} take from the array labels for this btn
-      label={label}
-      labelPlacement={labelPlacement} // all btns in group have same placement
-    />
-
-    <FormControlLabel
-      value={label}
-      control={
-        <MaterialRadio
-          checked={value}
-          onChange={onChange}
-          disabled={disabled}
-          css={radioButtonGroupClass}
-          inputProps={{ "aria-label": { label } }}
-        />
-      }
-      label={label}
-      labelPlacement={labelPlacement}
-    />
+    {labels.map(label => (
+      <FormControlLabel
+        key={label.id}
+        value={label} // select the label of this btn from the array
+        control={
+          <MaterialRadio
+            disabled={disabled} // can I disable a specific btn? Do I want to?
+            css={radioButtonGroupClass}
+            inputProps={{ "aria-label": { label } }} // label of this btn
+          />
+        }
+        label={label} // select the label of this btn from the array
+        labelPlacement={labelPlacement} // all btns in group have same placement
+      />
+    ))}
   </MaterialRadioGroup>
 );
 
 RadioButtonGroup.displayName = "RadioButtonGroup";
 
 RadioButtonGroup.propTypes = {
-  // labels: PropTypes.arrayOf(PropTypes.string),
-  label: PropTypes.string,
-  value: PropTypes.bool,
+  labels: PropTypes.arrayOf(PropTypes.string),
+  // label: PropTypes.string,
+  value: PropTypes.string,
   grpLabel: PropTypes.string,
   labelPlacement: PropTypes.string,
   disabled: PropTypes.bool,
@@ -83,9 +57,9 @@ RadioButtonGroup.propTypes = {
 };
 
 RadioButtonGroup.defaultProps = {
-  // labels: ["Label 1", "Label 2", "Label 3"],
-  label: "Label",
-  value: false,
+  labels: ["Label 1", "Label 2", "Label 3"],
+  // label: "Label",
+  value: "Label 1",
   grpLabel: "Group Label",
   labelPlacement: "end",
   disabled: false
