f13f7ed2e3b619c74045bf3d6f81b79080928397
dreamer <christopherlu.pdx@gmail.com>
2019-08-11 13:48:06 -0700

    deleted that  commented out chip in civiccardlayoutfull (tests still passing, components still rendering)

--- packages/component-library/src/CivicCard/CivicCardLayoutFull.js
+++ packages/component-library/src/CivicCard/CivicCardLayoutFull.js
@@ -32,28 +32,6 @@ const authorPhoto = css`
   cursor: pointer;
 `;
 
-// function Chip({ tag, index }) {
-//   return (
-//     <span
-//       className={css`
-//         display: inline-block;
-//         padding: 0 2em;
-//         height: 2em;
-//         line-height: 2em;
-//         border-radius: 1em;
-//         background-color: ${CivicVictoryTheme.group.colorScale[
-//           index % CivicVictoryTheme.group.colorScale.length
-//         ]};
-//         margin: 0.5em 0.5em;
-//         font-family: "Roboto Condensed", "Helvetica Neue", Helvetica, sans-serif;
-//         font-weight: bold;
-//         color: white;
-//         cursor: pointer;
-//       `}
-//     >{`#${tag}`}</span>
-//   );
-// }
-
 Chip.propTypes = {
   tag: PropTypes.string,
   index: PropTypes.number
