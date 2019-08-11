9ecfdf670ce16ea3fda1d4d33d16689997cce94a
dreamer <christopherlu.pdx@gmail.com>
2019-08-11 13:52:28 -0700

    applied page layout to /cards, goodness it's pretty

--- packages/2018/src/components/CardList.js
+++ packages/2018/src/components/CardList.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { PageLayout } from "@hackoregon/component-library";
 import CardRegistry from "../card-registry";
 
 const CardList = () => {
   const { tags } = CardRegistry;
 
   return (
-    <div>
-      <h1>Tags</h1>
-      <ul>
-        {Object.keys(tags).map(tag => (
-          <li>{`#${tag} (${tags[tag]})`}</li>
-        ))}
-      </ul>
-    </div>
+    <PageLayout>
+      <div>
+        <h1>Tags</h1>
+        <ul>
+          {Object.keys(tags).map(tag => (
+            <li>{`#${tag} (${tags[tag]})`}</li>
+          ))}
+        </ul>
+      </div>
+    </PageLayout>
   );
 };
 
