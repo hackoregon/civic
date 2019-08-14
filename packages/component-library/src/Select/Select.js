a626d0ffa0dc9f06d428d380aaa404ddcbbd05fa
Janice Bradford <janice.bradford@outlook.com>
2019-08-14 14:48:01 -0700

    Update the Select component API to generate list items from an object.

--- packages/component-library/src/Select/Select.js
+++ packages/component-library/src/Select/Select.js
@@ -1,6 +1,8 @@
 /** @jsx jsx */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { jsx, css } from "@emotion/core";
+import React from "react";
+import { camelCase, has } from "lodash";
 import PropTypes from "prop-types";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -9,71 +11,92 @@ import OutlinedInput from "@material-ui/core/OutlinedInput";
 import MenuItem from "@material-ui/core/MenuItem";
 
 const Select = ({
-  // autoWidth,
+  autoWidth,
   fullWidth,
   displayEmpty,
   onChange,
   value,
   variant,
   inputLabel,
-  disabled
-}) => (
-  <FormControl
-    // autoWidth={autoWidth}
-    fullWidth={fullWidth}
-    displayEmpty={displayEmpty}
-    variant={variant}
-    disabled={disabled}
-    // className={classes.formControl}
-  >
-    <InputLabel
-      ref={inputLabel}
-      htmlFor="outlined-age-simple"
+  // labelWidth,
+  disabled,
+  options
+}) => {
+  const valueLabels = options.map(item =>
+    has(item, "value") && has(item, "label")
+      ? item
+      : { value: item, label: item }
+  );
+
+  const inputLabelRef = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabelRef.current.offsetWidth);
+  }, []);
+
+  return (
+    <FormControl
+      autoWidth={autoWidth}
+      fullWidth={fullWidth}
+      displayEmpty={displayEmpty}
       variant={variant}
+      disabled={disabled}
+      // className={classes.formControl}
     >
-      {inputLabel}
-    </InputLabel>
-    <MaterialSelect
-      value={value}
-      onChange={onChange}
-      input={
-        <OutlinedInput
-          // labelWidth={labelWidth}
-          name="age"
-          id="outlined-age-simple"
-        />
-      }
-    >
-      <MenuItem value="">
-        <em>None</em>
-      </MenuItem>
-      <MenuItem value={10}>Ten</MenuItem>
-      <MenuItem value={20}>Twenty</MenuItem>
-      <MenuItem value={30}>Thirty</MenuItem>
-    </MaterialSelect>
-  </FormControl>
-);
+      <InputLabel
+        ref={inputLabelRef}
+        htmlFor={camelCase(inputLabel)}
+        variant={variant}
+      >
+        {inputLabel}
+      </InputLabel>
+      <MaterialSelect
+        value={value}
+        onChange={onChange}
+        input={
+          <OutlinedInput
+            labelWidth={labelWidth}
+            name={inputLabel}
+            id={camelCase(inputLabel)}
+          />
+        }
+      >
+        {valueLabels.map(item => (
+          <MenuItem value={item.value}>{item.label}</MenuItem>
+        ))}
+      </MaterialSelect>
+    </FormControl>
+  );
+};
 
 Select.displayName = "Select";
 
 Select.propTypes = {
-  // autoWidth: PropTypes.bool,
+  autoWidth: PropTypes.bool,
   fullWidth: PropTypes.bool,
   displayEmpty: PropTypes.bool,
   onChange: PropTypes.func,
   value: PropTypes.string,
   variant: PropTypes.string,
   inputLabel: PropTypes.string,
-  disabled: PropTypes.bool
+  // labelWidth: PropTypes.number,
+  disabled: PropTypes.bool,
+  options: PropTypes.oneOf(
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.arrayOf(
+      PropTypes.shape({ value: PropTypes.any, label: PropTypes.string })
+    )
+  ).isRequired
 };
 
 Select.defaultProps = {
-  // autoWidth: true,
+  autoWidth: true,
   fullWidth: false,
   displayEmpty: true,
   value: "List item",
   variant: "outlined",
   inputLabel: "Input label",
+  // labelWidth: 20,
   disabled: false
 };
 
--- packages/component-library/stories/Select.story.js
+++ packages/component-library/stories/Select.story.js
@@ -11,11 +11,11 @@ import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormHelperText from "@material-ui/core/FormHelperText";
-import { Select } from "@material-ui/core";
 import { Checkbox } from "../src";
 import { storybookStyles } from "./storyStyles";
 import StatefulWrapper from "../src/utils/StatefulWrapper";
 import notes from "./checkbox.notes.md";
+import Select from "../src/Select/Select";
 
 const GROUP_IDS = {
   LABELS: "Labels",
@@ -39,18 +39,25 @@ export default () =>
       () => {
         const label = text("Label", "Label", GROUP_IDS.LABELS);
         const disabled = boolean("Disabled", false, GROUP_IDS.STATE);
+        const options = [
+          { value: 10, label: "Ten" },
+          { value: 20, label: "Twenty" },
+          { value: 30, label: "Thirty" }
+        ];
+        const optionsTwo = ["ten", "twenty", "thirty"];
         return (
-          <StatefulWrapper initialState={{ value: false }}>
+          <StatefulWrapper initialState={{ value: "ten" }}>
             {({ get, set }) => {
               return (
                 <Select
                   label={label}
                   onChange={event => {
-                    set({ value: event.target.checked });
+                    set({ value: event.target.value });
                     action("onChange")(event);
                   }}
                   value={get("value")}
                   disabled={disabled}
+                  options={optionsTwo}
                 />
               );
             }}
