6aa018eec994ca812bb04d601571a115c4740d0e
Dianna Faulk <diannafaulk@gmail.com>
2019-08-15 21:57:30 -0700

    Create _Themes from existing themes

---
+++ packages/component-library/src/_Themes/index.js
@@ -0,0 +1,7 @@
+import Brand from "./BrandTheme";
+import DataVisualization from "./DataVisualizationTheme";
+import CrazyVisualization from "./CrazyVisualizationTheme";
+
+export const BrandTheme = Brand;
+export const DataVisualizationTheme = DataVisualization;
+export const CrazyVisualizationTheme = CrazyVisualization;
