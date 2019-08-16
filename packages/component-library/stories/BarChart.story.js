d9c27f5c0dfb2ba41be2eefc8278702e0afbf8cd
Dianna Faulk <diannafaulk@gmail.com>
2019-08-15 22:01:41 -0700

    Use ThemeProvider in BarChart

--- packages/component-library/stories/BarChart.story.js
+++ packages/component-library/stories/BarChart.story.js
@@ -14,6 +14,7 @@ import civicFormat from "../src/utils/civicFormat";
 import { getKeyNames } from "./shared";
 import notes from "./barchart.notes.md";
 import { BarChart } from "../src";
+import { CrazyVisualizationTheme } from "../src/_Themes/index";
 
 const GROUP_IDS = {
   LABELS: "Labels",
@@ -75,6 +76,7 @@ export default () =>
             yLabel={yLabel}
             xNumberFormatter={x => civicFormat[optionSelectX](x)}
             yNumberFormatter={y => civicFormat[optionSelectY](y)}
+            theme={CrazyVisualizationTheme}
           />
         );
       },
