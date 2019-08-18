6b5abd10fc18c2222974f33bbec0553acf4161de
Cosmo Kent <cosmolightfoot@gmail.com>
2019-08-18 16:23:26 -0700

    created side by side story card visualization #796

--- packages/component-library/src/index.js
+++ packages/component-library/src/index.js
@@ -68,7 +68,9 @@ export {
 export {
   default as CivicCardLayoutVisualizationOnly
 } from "./CivicCard/CivicCardLayoutVisualizationOnly";
-
+export {
+  default as CivicCardLayoutSideBySide
+} from "./CivicCard/CivicCardLayoutSideBySide";
 export { default as civicFormat } from "./utils/civicFormat";
 export { default as ungroupBy } from "./utils/ungroupBy";
 export { default as DemoJSONLoader } from "./DemoJSONLoader/DemoJSONLoader";
--- packages/component-library/stories/CivicCard.story.js
+++ packages/component-library/stories/CivicCard.story.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { storiesOf } from "@storybook/react";
 import {
@@ -7,7 +7,9 @@ import {
   LineChart,
   Collapsable,
   CivicCardLayoutClassic,
-  CivicCardLayoutVisualizationOnly
+  CivicCardLayoutVisualizationOnly,
+  CivicCardLayoutSideBySide,
+  RadioButtonGroup
 } from "../src";
 import { civicFormat } from "../src/utils";
 
@@ -34,6 +36,123 @@ const demoData = [
   }
 ];
 
+const sideBySideDemoData = {
+  14: [
+    {
+      year: 2009,
+      weekday_sum_ons: 6958
+    },
+    {
+      year: 2010,
+      weekday_sum_ons: 6497
+    },
+    {
+      year: 2011,
+      weekday_sum_ons: 6167
+    },
+    {
+      year: 2012,
+      weekday_sum_ons: 6253
+    },
+    {
+      year: 2013,
+      weekday_sum_ons: 6082
+    },
+    {
+      year: 2014,
+      weekday_sum_ons: 6326
+    },
+    {
+      year: 2015,
+      weekday_sum_ons: 6139
+    },
+    {
+      year: 2016,
+      weekday_sum_ons: 5778
+    },
+    {
+      year: 2017,
+      weekday_sum_ons: 5742
+    }
+  ],
+  72: [
+    {
+      year: 2009,
+      weekday_sum_ons: 17691
+    },
+    {
+      year: 2010,
+      weekday_sum_ons: 16376
+    },
+    {
+      year: 2011,
+      weekday_sum_ons: 16848
+    },
+    {
+      year: 2012,
+      weekday_sum_ons: 16947
+    },
+    {
+      year: 2013,
+      weekday_sum_ons: 16387
+    },
+    {
+      year: 2014,
+      weekday_sum_ons: 16774
+    },
+    {
+      year: 2015,
+      weekday_sum_ons: 16551
+    },
+    {
+      year: 2016,
+      weekday_sum_ons: 15355
+    },
+    {
+      year: 2017,
+      weekday_sum_ons: 14913
+    }
+  ],
+  20: [
+    {
+      year: 2009,
+      weekday_sum_ons: 10476
+    },
+    {
+      year: 2010,
+      weekday_sum_ons: 10115
+    },
+    {
+      year: 2011,
+      weekday_sum_ons: 10058
+    },
+    {
+      year: 2012,
+      weekday_sum_ons: 10834
+    },
+    {
+      year: 2013,
+      weekday_sum_ons: 10683
+    },
+    {
+      year: 2014,
+      weekday_sum_ons: 11270
+    },
+    {
+      year: 2015,
+      weekday_sum_ons: 11209
+    },
+    {
+      year: 2016,
+      weekday_sum_ons: 10543
+    },
+    {
+      year: 2017,
+      weekday_sum_ons: 10988
+    }
+  ]
+};
+
 function DemoCardVisualization({ isLoading, data }) {
   return (
     !isLoading &&
@@ -48,6 +167,35 @@ function DemoCardVisualization({ isLoading, data }) {
         xNumberFormatter={civicFormat.year}
         subtitle="Average daily ridership for all TriMet bus and rail"
       />
+    )
+  );
+}
+
+function DemoCardVisualizationWithSelector({ isLoading, data }) {
+  const grpLabel = "Bus Line";
+  const radioLabels = ["14", "72", "20"];
+  const [busRoute, setBusRoute] = useState("14");
+  return (
+    !isLoading &&
+    data && (
+      <>
+        <RadioButtonGroup
+          grpLabel={grpLabel}
+          labels={radioLabels}
+          onChange={({ target }) => setBusRoute(target.value)}
+          value={busRoute}
+        />
+        <LineChart
+          data={data[busRoute]}
+          dataKey="year"
+          dataValue="weekday_sum_ons"
+          title="Portland Transit Ridership - Weekdays"
+          xLabel="Year"
+          yLabel="Ridership"
+          xNumberFormatter={civicFormat.year}
+          subtitle="Average daily ridership for all TriMet bus and rail"
+        />
+      </>
     )
   );
 }
@@ -321,6 +469,13 @@ const demoCardMeta = (/* data */) => ({
   ]
 });
 
+const demoCardMetaSideBySide = data => {
+  return {
+    ...demoCardMeta(data),
+    visualization: DemoCardVisualizationWithSelector
+  };
+};
+
 export default () =>
   storiesOf("Component Lib|Story Cards/CIVIC Card", module)
     .add("Layout: Default (full)", () => (
@@ -340,5 +495,13 @@ export default () =>
         data={demoData}
         isLoading={false}
         Layout={CivicCardLayoutVisualizationOnly}
+      />
+    ))
+    .add("Layout: SideBySide", () => (
+      <CivicCard
+        cardMeta={demoCardMetaSideBySide}
+        data={sideBySideDemoData}
+        isLoading={false}
+        Layout={CivicCardLayoutSideBySide}
       />
     ));
