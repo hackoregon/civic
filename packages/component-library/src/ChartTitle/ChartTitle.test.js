6f4b5d74fa8a4912e4cf52cc4d13b02d70fb7ee6
Jaron Heard <jaronheard@gmail.com>
2019-08-18 19:10:20 -0700

    fix chart title test

--- packages/component-library/src/ChartTitle/ChartTitle.test.js
+++ packages/component-library/src/ChartTitle/ChartTitle.test.js
@@ -9,14 +9,14 @@ describe("ChartTitle", () => {
       <ChartTitle title="Fun title" subtitle="Fun subtitle" />
     );
 
-    expect(wrapper.find("h3").text()).to.eql("Fun title");
-    expect(wrapper.find("span").text()).to.eql("Fun subtitle");
+    expect(wrapper.find("h2").text()).to.eql("Fun title");
+    expect(wrapper.find("h3").text()).to.eql("Fun subtitle");
   });
 
   it("should neither render a title nor a subtitle when neither are provided", () => {
     const wrapper = shallow(<ChartTitle />);
 
+    expect(wrapper.find("h2").length).to.eql(0);
     expect(wrapper.find("h3").length).to.eql(0);
-    expect(wrapper.find("span").length).to.eql(0);
   });
 });
