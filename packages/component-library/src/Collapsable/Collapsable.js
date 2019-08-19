34b77132d67ee18bc4af537d3abf1632f7970981
Jaron Heard <jaronheard@gmail.com>
2019-08-19 02:11:33 -0700

    fix linting errors

--- packages/component-library/src/Collapsable/Collapsable.js
+++ packages/component-library/src/Collapsable/Collapsable.js
@@ -1,7 +1,5 @@
-/* TODO: Fix linting errors */
-/* eslint-disable */
-
 import React, { Children } from "react";
+import PropTypes from "prop-types";
 import { css } from "emotion";
 
 const toggleStyle = css`
@@ -27,14 +25,17 @@ class Collapsable extends React.Component {
   }
 
   onToggle() {
-    this.setState({ expanded: !this.state.expanded });
+    const { expanded } = this.state;
+    this.setState({ expanded: !expanded });
   }
 
   renderToggle() {
-    const cta = this.state.expanded ? "Less" : "More";
-    const arrow = this.state.expanded ? "up" : "down";
+    const { expanded } = this.state;
+    const cta = expanded ? "Less" : "More";
+    const arrow = expanded ? "up" : "down";
 
     return (
+      // eslint-disable-next-line
       <a className={toggleStyle} onClick={this.onToggle}>
         {cta}
         <span style={{ display: "block" }} className={`fa fa-arrow-${arrow}`} />
@@ -43,15 +44,17 @@ class Collapsable extends React.Component {
   }
 
   render() {
+    const { expanded } = this.state;
     const children = [];
     let showToggle;
     let toggle;
 
+    // eslint-disable-next-line react/destructuring-assignment
     Children.forEach(this.props.children, child => {
       if (child.props.hidden) {
         showToggle = true;
 
-        if (this.state.expanded) {
+        if (expanded) {
           children.push(child);
         }
       } else {
@@ -75,5 +78,9 @@ class Collapsable extends React.Component {
 const Section = ({ children }) => children;
 
 Collapsable.Section = Section;
+
+Collapsable.propTypes = {
+  children: PropTypes.node
+};
 
 export default Collapsable;
