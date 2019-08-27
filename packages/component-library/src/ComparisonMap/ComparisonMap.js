04f44a569824f60f6036bf9559ff126d07db964b
David Mendoza <david@themendozaline.org>
2019-08-27 06:37:07 +0000

    Add propTypes

--- packages/component-library/src/ComparisonMap/ComparisonMap.js
+++ packages/component-library/src/ComparisonMap/ComparisonMap.js
@@ -1,10 +1,9 @@
 /* eslint-disable */
 /** @jsx jsx */
 import React from "react";
-// import {} from "prop-types";
-import { css, jsx } from "@emotion/core";
-import { isEqual } from "lodash";
 import Slider from "../Slider/Slider";
+import { css, jsx } from "@emotion/core";
+import { node, number, shape } from "prop-types";
 
 const sliderWrapper = css`
   margin: 2em auto;
@@ -25,57 +24,41 @@ const ComparisonMap = props => {
   );
 
   const [viewport, setVP] = React.useState(initialViewport);
-  /* global console */
-  const onVPChange = newestViewport => {
-    console.log("NEW-VP:", newestViewport);
-    setVP(prevVP => {
-      console.log("PREV-VP:", prevVP);
-      console.log("NOT EQUAL:", !isEqual(prevVP, newestViewport));
-      console.log("\n");
-      if (!isEqual(prevVP, newestViewport)) {
-        return newestViewport;
-      }
-    });
-
-    // if (nav) {
-    //   return setVP(prevVP => ({...prevVP, zoom: prevVP.zoom + 1}) );
-    // } else {
-    //   return setVP(newestViewport);
-    // }
-  };
 
   const LeftMap = React.cloneElement(leftMap, {
-    vpChange: onVPChange,
-    sharedViewport: viewport
+    onSharedViewportChange: setVP,
+    sharedViewport: viewport,
+    navigation: false,
+    mapGLOptions: { keyboard: false },
+    updateViewport: false
   });
 
   const RightMap = React.cloneElement(rightMap, {
-    vpChange: onVPChange,
-    sharedViewport: viewport
+    onSharedViewportChange: setVP,
+    sharedViewport: viewport,
+    navigation: false,
+    mapGLOptions: { keyboard: false },
+    updateViewport: false
   });
 
   const container = css`
-    border: 2.5px solid magenta;
     display: flex;
     flex-direction: column;
   `;
 
   const mapsWrapper = css`
     width: 100%;
-    border: 2px solid green;
     position: relative;
     height: ${height}px;
   `;
 
   const leftMapWrapper = css`
-    border: 1px solid blue;
     position: absolute;
     width: 100%;
     height: ${height}px;
   `;
 
   const rightMapWrapper = css`
-    border: 1px solid red;
     position: absolute;
     width: 100%;
     height: ${height}px;
@@ -103,8 +86,17 @@ const ComparisonMap = props => {
   );
 };
 
-ComparisonMap.propTypes = {};
+ComparisonMap.propTypes = {
+  leftMap: node,
+  rightMap: node,
+  height: number,
+  sliderStartPosition: number,
+  initialViewport: shape({})
+};
 
-ComparisonMap.defaultProps = {};
+ComparisonMap.defaultProps = {
+  height: 550,
+  sliderStartPosition: 50
+};
 
 export default ComparisonMap;
