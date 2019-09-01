e4c698e1c3133c954cdb6338288826251ae1846c
curtis mcallister <curtis.s.mcallister@gmail.com>
2019-09-01 14:11:04 -0700

    stub out api call to get disturbance stop data

--- packages/2019-transportation/src/components/DisturbanceStops/DisturbanceStopsVisualization.js
+++ packages/2019-transportation/src/components/DisturbanceStops/DisturbanceStopsVisualization.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import { resourceShape } from "reduxful/react-addons";
 import { isLoaded } from "reduxful";
@@ -22,12 +23,43 @@ const baseMapProps = {
   // mapGLOptions:mapGLOptions,
 };
 
+// http://service.civicpdx.org/transportation2019/v1/toad/disturbanceStops/?limit=100&offset=400&months=9&time_range=6.25%2C9.5&years=2017%2C2018&lines=6%2C10%2C14&service_key=W&bounds=-122.665849%2C45.510867%2C-122.653650%2C45.514367
+const limit = 100;
+const offset = 0;
+const months = "9";
+const timeRange = "6.25,9.5";
+const years = "2017,2018";
+const lines = "14";
+const bounds = "-122.665849,45.510867,-122.653650,45.514367";
+const testUrl = `http://service.civicpdx.org/transportation2019/v1/toad/disturbanceStops/?limit=${limit}&offset=${offset}&months=${months}&time_range=${timeRange}&years=${years}&lines=${lines}&service_key=W&bounds=${bounds}`;
+
 const DisturbanceStopsVisualization = ({ data }) => {
+  const [disturbanceStops2017, setDisturbanceStops2017] = useState([]);
+  const [disturbanceStops2018, setDisturbanceStops2018] = useState([]);
+
+  useEffect(() => {
+    axios.get(testUrl).then(response => {
+      console.log({ response });
+      setDisturbanceStops2017(
+        response.data.results.features.filter(
+          feature => feature.properties.year === 2017
+        )
+      );
+      setDisturbanceStops2018(
+        response.data.results.features.filter(
+          feature => feature.properties.year === 2018
+        )
+      );
+    });
+  }, []);
+
   const isLoading = !isLoaded(data.disturbanceStops);
 
   const DisturbanceStopsMap2017 = <BaseMap {...baseMapProps} />;
-
   const DisturbanceStopsMap2018 = <BaseMap {...baseMapProps} />;
+
+  console.log({ disturbanceStops2017 });
+  console.log({ disturbanceStops2018 });
 
   return (
     <>
