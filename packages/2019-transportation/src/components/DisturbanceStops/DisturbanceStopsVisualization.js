08db36bc2572ca5add07ac62ce3ae9a8be684e73
curtis mcallister <curtis.s.mcallister@gmail.com>
2019-09-01 13:57:52 -0700

    add initial location and zoom props

--- packages/2019-transportation/src/components/DisturbanceStops/DisturbanceStopsVisualization.js
+++ packages/2019-transportation/src/components/DisturbanceStops/DisturbanceStopsVisualization.js
@@ -5,12 +5,29 @@ import { isLoaded } from "reduxful";
 
 import { BaseMap, ComparisonMap } from "@hackoregon/component-library";
 
+const baseMapProps = {
+  updateViewport: false,
+  initialZoom: 14.5,
+  initialLatitude: 45.512,
+  initialLongitude: -122.659
+  // mapboxDataId:"transit-stops-data",
+  // mapboxLayerType:"heatmap",
+  // mapboxData:someKindofData.results,
+  // mapboxDataId:"transit-stops-data",
+  // mapboxLayerType:"heatmap",
+  // mapboxLayerOptions:heatmapLayer,
+  // mapboxLayerId:"transit-stops-map",
+  // civicMapStyle:"dark",
+  // navigation:false,
+  // mapGLOptions:mapGLOptions,
+};
+
 const DisturbanceStopsVisualization = ({ data }) => {
   const isLoading = !isLoaded(data.disturbanceStops);
 
-  const DisturbanceStopsMap2017 = <BaseMap />;
+  const DisturbanceStopsMap2017 = <BaseMap {...baseMapProps} />;
 
-  const DisturbanceStopsMap2018 = <BaseMap />;
+  const DisturbanceStopsMap2018 = <BaseMap {...baseMapProps} />;
 
   return (
     <>
