2455fac070d942d4e8ecf8cc8a4b78e61f749c75
Dianna Faulk <diannafaulk@gmail.com>
2019-09-02 16:43:36 -0700

    Create selectors, actions, reducer for coordinates

--- packages/2018-disaster-resilience/src/state/index.js
+++ packages/2018-disaster-resilience/src/state/index.js
@@ -3,11 +3,13 @@ import { routerReducer } from "react-router-redux";
 import { reducer as reduxFormReducer } from "redux-form";
 import youAndYourNeighbors from "./you-and-your-neighbors";
 import proactivePlanningData from "./proactive-planning/api";
+import youAndYourNeighborsLocalData from "./you-and-your-neighbors-gorilla/local-api";
 import youAndYourNeighborsGorillaData from "./you-and-your-neighbors-gorilla/api";
 
 export default function createReducer(asyncReducers) {
   return combineReducers({
     routing: routerReducer,
+    youAndYourNeighborsLocalData,
     ...youAndYourNeighborsGorillaData.reducers,
     ...proactivePlanningData.reducers,
     youAndYourNeighbors,
