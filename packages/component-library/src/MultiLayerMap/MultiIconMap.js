e5c55c188172daebcc6a3ae40fcb1d96a3b2598b
David Mendoza <david@themendozaline.org>
2019-09-02 01:41:19 +0000

    Fix icon mapping error

--- packages/component-library/src/MultiLayerMap/MultiIconMap.js
+++ packages/component-library/src/MultiLayerMap/MultiIconMap.js
@@ -28,9 +28,8 @@ const MultiIconMap = props => {
     opacity = 0.7,
     getPosition = f => f.geometry.coordinates,
     iconAtlas,
-    iconMapping,
+    iconMapping = {},
     getSizeScale = getIconSizeScale,
-    getIcon = f => f.properties.type,
     iconSize = 10,
     autoHighlight = true,
     highlightColor = [0, 0, 128, 128],
@@ -57,7 +56,7 @@ const MultiIconMap = props => {
       const value = feature.properties[fieldNameColor];
       return value ? colorScale(value) : [0, 0, 0, 128];
     }
-    return colorScale();
+    return colorScale("icon");
   };
 
   const getSize = createSizeScale(iconSize, scaleType, fieldName);
@@ -66,18 +65,30 @@ const MultiIconMap = props => {
 
   const sizeScale = getSizeScale(zoom);
 
+  const iconMap = Object.keys(iconMapping).length !== 0
+    ? iconMapping
+    : {
+        [dataRange[0]]: {
+          "x": 0,
+          "y": 0,
+          "width": 250,
+          "height": 250,
+          "mask": true
+        }
+      };
+
   return (
     <IconLayer
       key={shortid.generate()}
       id={id}
-      data={data}
+      data={data.filter(d => d.geometry)}
       pickable={pickable}
       opacity={opacity}
       getPosition={getPosition}
       iconAtlas={iconAtlas}
-      iconMapping={iconMapping}
+      iconMapping={iconMap}
       sizeScale={sizeScale}
-      getIcon={getIcon}
+      getIcon={f => fieldName && fieldName.color ? f.properties.type : "icon"}
       getSize={getSize}
       getColor={getColor}
       autoHighlight={autoHighlight}
