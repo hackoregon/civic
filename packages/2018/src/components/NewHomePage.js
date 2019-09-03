d2a4a0e241c03f4bcdd651dcdb0c8ba14d507983
Dianna Faulk <diannafaulk@gmail.com>
2019-09-02 23:22:50 -0700

    Add home page section layout and change to rem

--- packages/2018/src/components/NewHomePage.js
+++ packages/2018/src/components/NewHomePage.js
@@ -2,16 +2,22 @@
 import { css, jsx, Global } from "@emotion/core";
 import emotionReset from "emotion-reset";
 import { Fragment } from "react";
-import { Button } from "@hackoregon/component-library";
+import { Button, BrandColors } from "@hackoregon/component-library";
 import HomePageStyle from "./NewHomePage.styles";
 // Placeholders
 import placeholderIntroImg from "../assets/new-home-page-1.png";
+import placeholderWorkImg from "../assets/new-home-page-2.png";
+
+const sectionWorkLayout = css`
+  padding-bottom: 150px;
+`;
 
 const HomePage = () => {
   return (
     <Fragment>
       <Global
         styles={css`
+          font-size: 22px;
           ${emotionReset}
           ${HomePageStyle}
         `}
@@ -32,7 +38,42 @@ const HomePage = () => {
             system built for the needs of institutions managing important data
             systems with public value.
           </p>
-          <Button margin="26px 13px 0 0">EXPLORE CIVIC!</Button>
+          <Button margin="26px 0 0 0">EXPLORE CIVIC</Button>
+        </div>
+        <h2>JOIN THE MOVEMENT</h2>
+        <div className="home-section" css={sectionWorkLayout}>
+          <h3>Work With Us</h3>
+          <h4>
+            CITIES, GOVERNMENTS &<br />
+            ORGANIZATIONS
+          </h4>
+          <p>
+            We connect resources and a nationwide network of collaborators with
+            complex information challenges in the public interest to build
+            projects on CIVIC’s open technology frameworks.
+            <br />
+            <br />
+            Our vision is for public data to be available as a vital resource
+            for collaboration and group problem solving -- accessible
+            programatically, in common formats, with excellent documentation,
+            using secure and reliable technology.
+            <br />
+            <br />
+            The technology is only part of the challenge. Custodians of this
+            data in government, nonprofit, and academia face barriers of limited
+            funding, access to talent, and unique compliance.
+            <br />
+            <br />
+            We’re building the teams and systems to make it happen.
+          </p>
+          <Button margin="96px 0 53px 0" bkgndColor={BrandColors.subdued.rgba}>
+            WORK WITH US
+          </Button>
+          <img
+            className="placeholder-work-image"
+            src={placeholderWorkImg}
+            alt="placeholder"
+          />
         </div>
       </div>
     </Fragment>
--- packages/2018/src/components/NewHomePage.styles.js
+++ packages/2018/src/components/NewHomePage.styles.js
@@ -1,50 +1,77 @@
 import { BrandColors } from "@hackoregon/component-library";
 
-const { primary } = BrandColors;
+const { primary, subdued } = BrandColors;
 
 const introWidth = "800px";
-const defaultFontSize = 22;
+const browserDefaultSize = 16; // Don't modify
+const defaultFontSize = 22; // all em based off of this default value
 
 const textStandard = {
   color: primary.hex,
   fontFamily: "'Rubik', sans-serif",
   fontWeight: "normal",
-  lineHeight: `${36 / defaultFontSize}em`
+  fontSize: `${defaultFontSize / browserDefaultSize}rem`,
+  lineHeight: `${36 / defaultFontSize}rem`
 };
 
 export default {
+  div: {
+    boxSizing: "border-box"
+  },
+
   // Text
   h1: {
     ...textStandard,
-    fontSize: `${60 / defaultFontSize}em`,
+    fontSize: `${60 / defaultFontSize}rem`,
     fontWeight: "500",
     lineHeight: "118%",
-    marginBottom: `${17 / defaultFontSize}em`
+    marginBottom: `${17 / defaultFontSize}rem`
   },
 
   h2: {
     ...textStandard,
-    fontSize: "3em"
+    fontSize: `${48 / defaultFontSize}rem`,
+    fontWeight: "300",
+    lineHeight: "118%"
+  },
+
+  h3: {
+    ...textStandard,
+    fontSize: `${80 / defaultFontSize}rem`,
+    fontWeight: "500",
+    lineHeight: "118%",
+    position: "absolute",
+    top: `-${37 / defaultFontSize}rem`,
+    left: "0"
+  },
+
+  h4: {
+    ...textStandard,
+    fontSize: `${30 / defaultFontSize}rem`,
+    fontWeight: "500",
+    lineHeight: "118%",
+    width: "472px",
+    marginBottom: "28px"
   },
 
   ".page-subtitle": {
     ...textStandard,
-    fontSize: `${40 / defaultFontSize}em`,
+    fontSize: `${40 / defaultFontSize}rem`,
     fontWeight: "300",
     lineHeight: "120%",
     width: "600px",
-    marginBottom: `${60 / defaultFontSize}em`
+    marginBottom: `${60 / defaultFontSize}rem`
   },
 
   ".intro-text": {
     ...textStandard,
-    fontSize: `${36 / defaultFontSize}em`,
-    lineHeigh: `${60 / defaultFontSize}em`
+    fontSize: `${36 / defaultFontSize}rem`,
+    lineHeight: `${60 / defaultFontSize}rem`
   },
 
   // Wrappers
   ".content-wrapper": {
-    fontSize: `${defaultFontSize}px`, // all em based off of this default value
+    ...textStandard,
     width: "100%",
     maxWidth: "1000px",
     margin: "60px auto"
@@ -52,12 +79,25 @@ export default {
 
   ".intro-wrapper": {
     width: introWidth,
-    margin: "0 auto"
+    margin: "0 auto 84px"
+  },
+
+  ".home-section": {
+    width: "100%",
+    backgroundColor: subdued.rgba,
+    margin: "53px auto 205px",
+    padding: "88px 120px 0",
+    position: "relative"
   },
 
   // Placeholder styles
   ".placeholder-intro-image": {
     width: introWidth,
     marginBottom: "28px"
+  },
+
+  ".placeholder-work-image": {
+    width: "calc(100% - 2 * 120px)",
+    position: "absolute"
   }
 };
