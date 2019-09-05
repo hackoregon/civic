02e358e6c5015ebfa96e605bc65dcae0b432c77d
BJHunnicutt <jeannie.hunnicutt@gmail.com>
2019-09-04 17:15:29 -0700

    Add standin data for the HOLC redlining map card

---
+++ packages/2019-housing/src/components/HolcRedlining/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { resourceShape } from "reduxful/react-addons";
+import { CivicCard } from "@hackoregon/component-library";
+
+import holcRedliningMeta from "./holcRedliningMeta";
+import api from "../../state/holc-redlining/api";
+
+const HolcRedlining = ({ init, data, Layout }) => {
+  useEffect(
+    () => {
+      init();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    [init]
+  );
+
+  return <CivicCard cardMeta={holcRedliningMeta} data={data} Layout={Layout} />;
+};
+
+HolcRedlining.displayName = "HolcRedlining";
+
+HolcRedlining.propTypes = {
+  init: PropTypes.func,
+  data: PropTypes.shape({ redliningMap: resourceShape }),
+  Layout: PropTypes.func
+};
+
+export default connect(
+  state => ({
+    data: {
+      redliningMap: api.selectors.getRedliningMapData(
+        state.package2019Housing || state
+      )
+    }
+    // state.packageProjectName || state needed to make work in the project package and 2018 package
+  }),
+  dispatch => ({
+    init() {
+      dispatch(api.actionCreators.getRedliningMapData());
+    }
+  })
+)(HolcRedlining);
