629d80b54d5427c8e482940aed5b70975f6c36c3
Janice Bradford <janice.bradford@outlook.com>
2019-09-05 16:56:01 -0700

    Material UI Theme for Radio Button Group (#910)
    * WIP on adding Material UI Theme.
    
    * Added dependency for material-ui/styles
    
    * add dependencies for Material UI theme
    
    * Create muiTheme in separate file to be imported into all Material UI components.
    
    * Create styling for the Radio Button Group component and addit to the Material UI theme.
    
    * Renamed the Material UI Theme for consistency.  Removed unneeded dependency on material-ui/styles at root level.

---
+++ packages/component-library/src/_Themes/MaterialUI/MaterialUITheme.js
@@ -0,0 +1,33 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import BrandColors from "../Brand/BrandColors";
+
+// Brand Colors
+const civicPrimary = BrandColors.primary.hex;
+const civicSecondary = BrandColors.secondary.hex;
+
+// Typography
+const sansSerif = "Roboto Condensed, Helvetica Neue, Helvetica, sans-serif";
+const size = "1em";
+
+const MaterialTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: civicPrimary
+    },
+    secondary: {
+      main: civicSecondary
+    },
+    text: {
+      primary: civicPrimary
+    }
+  },
+  shape: {
+    borderRadius: 2
+  },
+  typography: {
+    fontFamily: sansSerif,
+    fontSize: size
+  }
+});
+
+export default MaterialTheme;
