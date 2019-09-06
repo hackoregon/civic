490ddefbc85a0eea959c073b67d233fcfcccc244
Dianna Faulk <diannafaulk@gmail.com>
2019-09-05 19:57:59 -0700

    Update colors and add kit background

--- packages/project-disaster-trail/src/components/Game/KitScreen/index.js
+++ packages/project-disaster-trail/src/components/Game/KitScreen/index.js
@@ -1,14 +1,50 @@
-import React, { memo } from "react";
+/** @jsx jsx */
+import { css, jsx, keyframes } from "@emotion/core";
+import { memo, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { getKitCreationItems, addItemToPlayerKit } from "../../../state/kit";
 import { addPoints } from "../../../state/user";
+import { palette } from "../../../constants/style";
 import DurationBar from "../../atoms/DurationBar";
 import Ticker from "../../atoms/Ticker";
 import { MapStyle, GUIStyle } from "../index";
 import OrbManager from "../OrbManager";
 import Kit from "./Kit";
+
+const slide = keyframes`
+  0% {
+    transform: translateX(-25%);
+  }
+  100% {
+    transform: translateX(25%);
+  }
+`;
+
+const bg = css`
+  animation: ${slide} 6s ease-in-out infinite alternate;
+  background-image: linear-gradient(
+    -60deg,
+    ${palette.lightLime} 50%,
+    ${palette.lemon} 50%
+  );
+  bottom: 0;
+  left: -50%;
+  opacity: 0.5;
+  position: fixed;
+  right: -50%;
+  top: 0;
+`;
+
+const bg2 = css`
+  animation-direction: alternate-reverse;
+  animation-duration: 8s;
+`;
+
+const bg3 = css`
+  animation-duration: 10s;
+`;
 
 const KitScreen = ({
   possibleItems,
@@ -26,8 +62,11 @@ const KitScreen = ({
   const checkIfItemIsGood = kitItem => kitItem.good;
 
   return (
-    <>
+    <Fragment>
       <MapStyle>
+        <div css={bg} />
+        <div css={[bg, bg2]} />
+        <div css={[bg, bg3]} />
         <Kit />
       </MapStyle>
       <DurationBar step="Choose supplies" />
@@ -40,7 +79,7 @@ const KitScreen = ({
           activeScreen="kit"
         />
       </GUIStyle>
-    </>
+    </Fragment>
   );
 };
 
