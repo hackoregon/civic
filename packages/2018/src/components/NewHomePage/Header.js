f614e6afc9c9c313140d4f4deb0109246114f96f
Dianna Faulk <diannafaulk@gmail.com>
2019-09-08 03:42:30 -0700

    Create dropdown and styles

--- packages/2018/src/components/NewHomePage/Header.js
+++ packages/2018/src/components/NewHomePage/Header.js
@@ -1,10 +1,32 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
+import { useState, useRef } from "react";
 import { Logo, BrandColors } from "@hackoregon/component-library";
-// import Select from "react-select";
-import { defaultFontSize } from "./index.styles";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
 
-const { primary } = BrandColors;
+import { defaultFontSize } from "./index.styles";
+import navCaret from "../../assets/nav-caret.svg";
+import menuVector from "../../assets/menu-vector.svg";
+
+const { primary, action } = BrandColors;
+
+const dropdownStyles = {
+  marginTop: "30px",
+  backgroundColor: "#F3F2F3"
+};
+
+const optionText = {
+  fontFamily: "Rubik, sans-serif",
+  fontWeight: 500,
+  fontSize: `${16 / defaultFontSize}em`,
+  lineHeight: `${37 / defaultFontSize}em`,
+  color: primary.hex
+};
 
 const headerWrapper = css`
   height: 115px;
@@ -40,7 +62,6 @@ const linkContainer = css`
   justify-self: end;
   grid-template-columns: repeat(4, max-content);
   grid-gap: ${48 / defaultFontSize}em;
-
   justify-items: end;
 `;
 
@@ -53,7 +74,59 @@ const linkStyle = css`
   text-decoration: none;
 `;
 
+const menuButton = css`
+  ${linkStyle};
+  display: inline-grid;
+  text-align: end;
+  align-items: end;
+  background: none;
+  border: none;
+  margin: 0;
+  padding: 0;
+  color: inherit;
+  cursor: pointer;
+
+  :focus {
+    outline: none;
+  }
+`;
+
+const buttonText = css`
+  line-height: unset;
+
+  :hover {
+    color: ${action.hex};
+  }
+`;
+
+const caratStyle = css`
+  height: 8px;
+  margin-left: 5px;
+  padding-bottom: 2px;
+`;
+
+const vectorStyle = css`
+  position: absolute;
+  top: 12px;
+  right: 20px;
+`;
+
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  function handleToggle() {
+    setOpen(prevOpen => !prevOpen);
+  }
+
+  function handleClose(event) {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  }
+
   return (
     <header css={headerWrapper}>
       <div css={contentWrapper}>
@@ -65,7 +138,64 @@ const Header = () => {
               <a css={linkStyle}>EXPLORE CIVIC</a>
             </li>
             <li>
-              <a css={linkStyle}>JOIN THE MOVEMENT</a>
+              <button
+                css={menuButton}
+                type="button"
+                ref={anchorRef}
+                aria-controls="menu-list-grow"
+                aria-haspopup="true"
+                onClick={handleToggle}
+              >
+                <p css={buttonText}>
+                  JOIN THE MOVEMENT
+                  <span>
+                    <img css={caratStyle} src={navCaret} alt="" />
+                  </span>
+                </p>
+              </button>
+              <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                transition
+                disablePortal
+              >
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{
+                      transformOrigin:
+                        placement === "bottom" ? "center top" : "center bottom",
+                      ...dropdownStyles
+                    }}
+                  >
+                    <Paper
+                      id="menu-list-grow"
+                      square
+                      style={dropdownStyles.menu}
+                    >
+                      <img src={menuVector} css={vectorStyle} alt="" />
+                      <ClickAwayListener onClickAway={handleClose}>
+                        <MenuList>
+                          <MenuItem
+                            onClick={handleClose}
+                            style={optionText}
+                            dense
+                          >
+                            Work With Us
+                          </MenuItem>
+                          <MenuItem
+                            onClick={handleClose}
+                            style={optionText}
+                            dense
+                          >
+                            Become a Contributor
+                          </MenuItem>
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                )}
+              </Popper>
             </li>
             <li>
               <a css={linkStyle}>ABOUT</a>
