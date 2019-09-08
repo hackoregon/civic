e78555e0700c94f97e5bc2f9c1e25761bdc63df0
Jaron Heard <jaronheard@gmail.com>
2019-09-08 01:54:33 -0700

    Add Education Sample Card (#930)
    * Add different layout options to embed route
    
    * Add Education Sample Card
    
    * clean up theme

--- packages/2019-education/src/index.js
+++ packages/2019-education/src/index.js
@@ -1,8 +1,13 @@
 import App from "./components/App";
 import Routes from "./routes";
 import Reducers from "./state";
+import IsThereEvidenceTheProgramWorked from "./components/IsThereEvidenceTheProgramWorked";
 
 const CardRegistry = [
+  {
+    slug: "is-there-evidence-the-program-worked",
+    component: IsThereEvidenceTheProgramWorked
+  }
   // leave space for card injection
 ];
 
