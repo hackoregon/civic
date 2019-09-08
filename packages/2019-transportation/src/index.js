1e32a3ccf9411ac705dc873c9180ca40db0b010f
curtis mcallister <curtis.s.mcallister@gmail.com>
2019-09-08 14:27:13 -0700

    remove stub card

--- packages/2019-transportation/src/index.js
+++ packages/2019-transportation/src/index.js
@@ -3,17 +3,12 @@ import Routes from "./routes";
 import Reducers from "./state";
 
 // TODO: Update me before commiting a new card
-import TransportationCard from "./components/TransportationCard";
 import DisturbanceStops from "./components/DisturbanceStops";
 
 const CardRegistry = [
   {
     slug: "disturbance-stops",
     component: DisturbanceStops
-  },
-  {
-    slug: "transportation-card",
-    component: TransportationCard
   }
 ];
 
