d8213816995818443453419a62e565cefccfa294
Cosmo Kent <cosmolightfoot@gmail.com>
2019-09-09 16:25:52 -0700

    added chip from material ui, adjusted story accordingly

--- packages/component-library/src/Chip/Chip.js
+++ packages/component-library/src/Chip/Chip.js
@@ -1,34 +1,34 @@
-/** @jsx jsx */
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { jsx, css } from "@emotion/core";
+import React from "react";
 import PropTypes from "prop-types";
-import { VisualizationColors } from "../_Themes";
+import MaterialChip from "@material-ui/core/Chip";
+import { makeStyles } from "@material-ui/styles";
+import Label from "@material-ui/icons/Label";
+// import { VisualizationColors } from "../_Themes";
 
-function Chip({ tag, index }) {
+const useStyles = makeStyles({
+  tag: props => ({
+    color: props.color
+  })
+});
+
+function Chip({ tag, color, clickable }) {
+  const classes = useStyles({ color });
   return (
-    <span
-      css={css`
-        display: inline-block;
-        padding: 0 2em;
-        height: 2em;
-        line-height: 2em;
-        border-radius: 1em;
-        background-color: ${VisualizationColors.victoryColors[
-          index % VisualizationColors.victoryColors.length
-        ]};
-        margin: 0.5em 0.5em;
-        font-family: "Roboto Condensed", "Helvetica Neue", Helvetica, sans-serif;
-        font-weight: bold;
-        color: white;
-        cursor: pointer;
-      `}
-    >{`#${tag}`}</span>
+    <MaterialChip
+      label={`#${tag}`}
+      icon={<Label className={classes.tag} />}
+      size="small"
+      clickable={clickable}
+      className={classes.chip}
+    />
   );
 }
 
 Chip.propTypes = {
   tag: PropTypes.string,
-  index: PropTypes.number
+  color: PropTypes.string,
+  clickable: PropTypes.bool
 };
 
 export default Chip;
