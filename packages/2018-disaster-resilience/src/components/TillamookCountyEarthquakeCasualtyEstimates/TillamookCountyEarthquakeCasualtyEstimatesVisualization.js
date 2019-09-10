4594577d162f14ddd16290e24a9ab24ffd7f4ae6
Jaron Heard <jaronheard@gmail.com>
2019-09-10 16:23:25 -0700

    wip card

--- packages/2018-disaster-resilience/src/components/TillamookCountyEarthquakeCasualtyEstimates/TillamookCountyEarthquakeCasualtyEstimatesVisualization.js
+++ packages/2018-disaster-resilience/src/components/TillamookCountyEarthquakeCasualtyEstimates/TillamookCountyEarthquakeCasualtyEstimatesVisualization.js
@@ -1,21 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { get } from "lodash";
 import { resourceShape } from "reduxful/react-addons";
 import { isLoaded } from "reduxful";
 
-import { LineChart } from "@hackoregon/component-library";
+import { ScatterPlotMap } from "@hackoregon/component-library";
 
 const TillamookCountyEarthquakeCasualtyEstimatesVisualization = ({ data }) => {
   const isLoading = !isLoaded(data.earthquakeCasualties);
+  const earthquakeCasualtiesData = get(
+    data.earthquakeCasualties.value.results.features
+  );
 
   return (
     <>
       {!isLoading && data && (
-        <LineChart
-          data={data.earthquakeCasualties.value}
-          dataKey="year"
-          dataValue="weekday_sum_ons"
-          title="Template API Plot"
+        <ScatterPlotMap
+          data={earthquakeCasualtiesData}
+          getPosition={f =>
+            f.geometry ? f.geometry.coordinates : [-124.664355, 45.615779]
+          }
+          opacity={0.1}
+          getFillColor={[25, 183, 170, 255]}
+          radiusScale={1}
         />
       )}
     </>
--- packages/2018-disaster-resilience/src/state/tillamook-county-earthquake-casualty-estimates/api.js
+++ packages/2018-disaster-resilience/src/state/tillamook-county-earthquake-casualty-estimates/api.js
@@ -3,11 +3,11 @@ import requestAdapter from "../request-adapter";
 
 const apiConfig = { requestAdapter };
 
-const HOST = "https://service.civicpdx.org/transportation-systems";
+// const HOST = "https://service.civicpdx.org/transportation-systems";
 
 const apiDesc = {
   getEarthquakeCasualtiesData: {
-    url: `${HOST}/passenger-census/system/annual/averages/?format=json`,
+    url: `https://gist.githubusercontent.com/karenng-civicsoftware/b546be2d404a09be4e0572cb491f5d77/raw/3b77acf988000571b085d9559801e9cbc095af2d/aebm_results_storycard1_and_2.json`,
     // you can apply any needed data transformations to value here
     // if complex, separate tranformation function to another file
     dataTransform: data => data
