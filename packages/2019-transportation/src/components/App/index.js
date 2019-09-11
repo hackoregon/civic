eac5889dc96f29a4f7a6328f93729bbe4b0264b4
worc <curtis.s.mcallister@gmail.com>
2019-09-11 16:24:49 -0700

    dataset to data (#1012)
    * dataset to data
    
    * remove dummy text

--- packages/2019-transportation/src/components/App/index.js
+++ packages/2019-transportation/src/components/App/index.js
@@ -5,7 +5,6 @@ import { jsx, css, Global } from "@emotion/core";
 import {
   BrandTheme,
   PageLayout,
-  PullQuote,
   CivicCardLayoutClassic
 } from "@hackoregon/component-library";
 import SystemWideSummary from "../SystemWideSummary";
@@ -15,26 +14,9 @@ import SouthwestMadison from "../SouthwestMadison";
 import DisturbanceStops from "../DisturbanceStops";
 import Hypnotoad from "../../assets/hypnotoad";
 
-const sectionMarginSmall = css`
-  display: block;
-  margin: 12px auto;
-`;
 const sectionMarginMedium = css`
   display: block;
   margin: 64px auto;
-`;
-const sectionMaxWidthSmall = css`
-  max-width: 700px;
-`;
-const sectionMaxWidthMedium = css`
-  max-width: 900px;
-`;
-const sectionBodyHeading = css`
-  margin: 80px auto 24px;
-`;
-const paragraphStyle = css`
-  line-height: 2;
-  font-size: 18px;
 `;
 
 const App = () => (
@@ -42,7 +24,7 @@ const App = () => (
     <Global styles={BrandTheme} />
     <PageLayout
       teamTitle="Transportation"
-      heroTitle="Transit Operations Analytics Dataset (TOAD)"
+      heroTitle="Transit Operations Analytics Data (TOAD)"
       heroSubtitle="Visualizing transit operations data"
       overlay
     >
@@ -65,45 +47,6 @@ const App = () => (
 
       <section css={sectionMarginMedium}>
         <NorthwestEverett Layout={CivicCardLayoutClassic} />
-      </section>
-
-      <section css={[sectionBodyHeading, sectionMaxWidthSmall]}>
-        <h2>Ullamcorper dignissim cras tincidunt?</h2>
-      </section>
-
-      <section css={[sectionMarginSmall, sectionMaxWidthSmall]}>
-        <p css={paragraphStyle}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum
-          curabitur vitae nunc sed. Nisl condimentum id venenatis a condimentum
-          vitae sapien pellentesque. Sapien eget mi proin sed libero enim sed
-          faucibus turpis. Fermentum leo vel orci porta.
-        </p>
-      </section>
-      <section css={[sectionMarginSmall, sectionMaxWidthSmall]}>
-        <p css={paragraphStyle}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum
-          curabitur vitae nunc sed. Nisl condimentum id venenatis a condimentum
-          vitae sapien pellentesque. Sapien eget mi proin sed libero enim sed
-          faucibus turpis. Fermentum leo vel orci porta.
-        </p>
-      </section>
-      <section css={[sectionMarginMedium, sectionMaxWidthMedium]}>
-        <PullQuote
-          quoteText="The lorem ipsum text is typically a scrambled section of De
-        finibus bonorum et malorum, with words altered, added, and removed to make
-        it nonsensical, improper Latin."
-        />
-      </section>
-      <section css={[sectionMarginSmall, sectionMaxWidthSmall]}>
-        <p css={paragraphStyle}>
-          Dui accumsan sit amet nulla facilisi. Sed adipiscing diam donec
-          adipiscing. Amet volutpat consequat mauris nunc congue nisi vitae
-          suscipit. In fermentum et sollicitudin ac orci phasellus egestas
-          tellus rutrum. Sed turpis tincidunt id aliquet risus feugiat. Aliquet
-          enim tortor at auctor.
-        </p>
       </section>
     </PageLayout>
   </Fragment>
