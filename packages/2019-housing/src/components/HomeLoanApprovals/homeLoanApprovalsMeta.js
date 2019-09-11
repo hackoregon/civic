0ca1c41302c6cc204bff23435e17c5348e759c3e
Jaron Heard <jaronheard@gmail.com>
2019-09-11 07:18:40 -0700

    fix authors

--- packages/2019-housing/src/components/HomeLoanApprovals/homeLoanApprovalsMeta.js
+++ packages/2019-housing/src/components/HomeLoanApprovals/homeLoanApprovalsMeta.js
@@ -38,10 +38,7 @@ const HomeLoanApprovalsMeta = (/* data */) => ({
     }
   ],
   // authors likely an array of keys in the future
-  authors: [
-    "https://civicsoftwarefoundation.org/static/human-grid-test-4c90bfc3f316f5d4e104320cb98c43c8.png",
-    "https://civicsoftwarefoundation.org/static/human-grid-test2-ea1849501456af341647068243fc72bb.png"
-  ]
+  authors: []
 });
 
 export default HomeLoanApprovalsMeta;
