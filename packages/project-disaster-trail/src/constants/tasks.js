a5081a434bed80b6d7ca331dcc2e2db98f4d09a8
Dianna Faulk <diannafaulk@gmail.com>
2019-09-11 12:00:28 -0700

    Use new svgs

--- packages/project-disaster-trail/src/constants/tasks.js
+++ packages/project-disaster-trail/src/constants/tasks.js
@@ -1,7 +1,7 @@
 import {
   blanket,
   dustMask,
-  fireExtinguisher,
+  // fireExtinguisher,
   firstAidKit,
   flashlight,
   food,
@@ -14,7 +14,7 @@ import {
 // Task icons
 import cold from "../../assets/task_icons/cold.svg";
 import dust from "../../assets/task_icons/dust.svg";
-import fire from "../../assets/task_icons/fire.svg";
+// import fire from "../../assets/task_icons/fire.svg";
 import hole from "../../assets/task_icons/hole.svg";
 import hunger from "../../assets/task_icons/hunger.svg";
 import injury from "../../assets/task_icons/injury.svg";
@@ -26,11 +26,11 @@ import weather from "../../assets/task_icons/weather.svg";
 // Task scenes
 import sceneCold from "../../assets/task_scenes/scene-cold.svg";
 import sceneDust from "../../assets/task_scenes/scene-dust.svg";
-import sceneFire from "../../assets/task_scenes/scene-fire.svg";
+// import sceneFire from "../../assets/task_scenes/scene-fire.svg";
 import sceneHole from "../../assets/task_scenes/scene-hole.svg";
 import sceneHunger from "../../assets/task_scenes/scene-hunger.svg";
 import sceneInjury from "../../assets/task_scenes/scene-injury.svg";
-import sceneLostPet from "../../assets/task_scenes/scene-lost-pet.svg";
+import sceneLostPet from "../../assets/task_scenes/scene-lost-pet-2.svg";
 import sceneRubble from "../../assets/task_scenes/scene-rubble.svg";
 import sceneThirst from "../../assets/task_scenes/scene-thirst.svg";
 import sceneWeather from "../../assets/task_scenes/scene-weather.svg";
@@ -40,7 +40,7 @@ export const DUST = "dust";
 export const RUBBLE = "rubble";
 // Save others ids
 export const COLD = "cold";
-export const FIRE = "fire";
+// export const FIRE = "fire";
 export const HUNGER = "hunger";
 export const HOLE = "hole";
 export const INJURY = "injury";
@@ -70,7 +70,7 @@ export const tasks = {
     id: DUST,
     time: 15,
     requiredItem: dustMask,
-    numberItemsToSolve: 1,
+    numberItemsToSolve: 3,
     points: 3,
     text: "It's so dusty!",
     imageSVG: dust,
@@ -84,7 +84,7 @@ export const tasks = {
     id: COLD,
     time: 15,
     requiredItem: blanket,
-    numberItemsToSolve: 1,
+    numberItemsToSolve: 3,
     points: 3,
     text: "It's c-c-cold...",
     imageSVG: cold,
@@ -93,19 +93,19 @@ export const tasks = {
     sceneAlt: "a shivering person",
     locations: [[-122.610302, 45.504791], [-122.675011, 45.503044]]
   },
-  [FIRE]: {
-    id: FIRE,
-    time: 15,
-    requiredItem: fireExtinguisher,
-    numberItemsToSolve: 5,
-    points: 5,
-    text: "Uh oh! This fire could spread!",
-    imageSVG: fire,
-    imageAlt: "fire",
-    sceneSVG: sceneFire,
-    sceneAlt: "a fire that's spreading",
-    locations: [[-122.615447, 45.514957], [-122.685672, 45.525356]]
-  },
+  // [FIRE]: {
+  //   id: FIRE,
+  //   time: 15,
+  //   requiredItem: fireExtinguisher,
+  //   numberItemsToSolve: 5,
+  //   points: 5,
+  //   text: "Uh oh! This fire could spread!",
+  //   imageSVG: fire,
+  //   imageAlt: "fire",
+  //   sceneSVG: sceneFire,
+  //   sceneAlt: "a fire that's spreading",
+  //   locations: [[-122.615447, 45.514957], [-122.685672, 45.525356]]
+  // },
   [HUNGER]: {
     id: HUNGER,
     time: 15,
@@ -127,7 +127,7 @@ export const tasks = {
     id: HOLE,
     time: 15,
     requiredItem: rope,
-    numberItemsToSolve: 1,
+    numberItemsToSolve: 3,
     points: 3,
     text: "Excuse me, sir. I seem to be stuck",
     imageSVG: hole,
@@ -197,6 +197,6 @@ export const tasks = {
 export const tasksForEnvironment = {
   [URBAN]: {
     saveYourself: [RUBBLE, DUST],
-    saveOthers: [FIRE, INJURY, HOLE, HUNGER]
+    saveOthers: [INJURY, HOLE, HUNGER, COLD, WEATHER, LOST_PET, THIRST]
   }
 };
