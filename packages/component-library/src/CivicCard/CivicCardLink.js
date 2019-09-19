72b63179de176c7f468f0c983df3639ecb02b112
fstorr <fstorr@protonmail.com>
2019-09-18 19:55:54 -0700

    Projectcard accessibility links (#1042)
    * hide svg watermark from assistive technology
    
    Also remove xmlns which isn’t needed for SVGs in a page of HTML.
    
    * Update links in projectContentContainer
    
    - Moved `<Link>` inside `<h2>` so that link text doesn’t include the card’s heading and description (bad for assistive technology SEO).
    - Replaced CardAction `<button>` with a `<span>` so we don’t have two adjacent elements going to the same URL (bad for keyboard users and assistive technology users)
    - Added CSS to retain existing design and behavior.
    - **note**: the `<Link to={link}>` wasn’t creating an `href` attribute so I put in a `tabindex=“0”` attribute solely for testing. This should be removed before the code goes live as it will be superfluous.
    
    * replace focus with focus-visible and -moz-focusring
    
    Not ideal based on browser support. Need to dig in an see how to use the focus-visible polyfill with the emotion style library.
    
    * improved heading links for project cards
    
    * change tagList to ul elements
    
    * fix colors for links inside headings
    
    * add comment explaining accessibility-related CSS

--- packages/component-library/src/CivicCard/CivicCardLink.js
+++ packages/component-library/src/CivicCard/CivicCardLink.js
@@ -1,11 +1,12 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
+import { Fragment } from "react";
 import { Link } from "react-router";
 import { get } from "lodash";
 
 import PropTypes from "prop-types";
 
-const CardLink = ({ children, slug }) => {
+const CardLink = ({ children, slug, ...other }) => {
   const isEmbedded =
     `${get(window, "location.origin", "")}/cards/${slug}/embed/` ===
       get(window, "location.href", "") ||
@@ -16,20 +17,21 @@ const CardLink = ({ children, slug }) => {
     )}/cards/${slug}/embed/visualization` === get(window, "location.href", "");
 
   return (
-    <div>
+    <Fragment>
       {isEmbedded ? (
         <Link
           target="_blank"
           to={`${get(window, "location.origin")}/cards/${slug}`}
+          {...other}
         >
           <span>{children}</span>
         </Link>
       ) : (
-        <Link to={`${get(window, "location.origin")}/cards/${slug}`}>
+        <Link to={`${get(window, "location.origin")}/cards/${slug}`} {...other}>
           <span>{children}</span>
         </Link>
       )}
-    </div>
+    </Fragment>
   );
 };
 
