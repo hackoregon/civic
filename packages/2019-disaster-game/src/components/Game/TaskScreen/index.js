86ef06e9cf3791cfcb22cb31b26e25a83fe0acc3
Dianna Faulk <diannafaulk@gmail.com>
2019-10-17 02:52:34 -0700

    Fix solve task bug

--- packages/2019-disaster-game/src/components/Game/TaskScreen/index.js
+++ packages/2019-disaster-game/src/components/Game/TaskScreen/index.js
@@ -37,7 +37,7 @@ const screenLayout = css`
 `;
 
 const chapterDuration = 120;
-const votingDuration = 20;
+const votingDuration = 15;
 const mapTransitionDuration = 3;
 const taskVotesDefault = {
   mostVotesId: null,
@@ -158,6 +158,15 @@ const TaskScreen = ({
     };
   }, [activeTaskIndex, taskPhase]); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const setEndSolvingChapterAnimationTimeouts = () => {
+    const newTimeout = setTimeout(() => {
+      setCorrectItemsChosen(0);
+      setAnimatingTaskTransition(false);
+      phaseTimer.stopEarly();
+    }, 1000);
+    setSolvingTransitionTimeout(newTimeout);
+  };
+
   const onItemSelection = orbModel => {
     if (orbModel.type === activeTask.requiredItem) {
       const itemsNowChosen = correctItemsChosen + 1;
@@ -166,12 +175,7 @@ const TaskScreen = ({
         setAnimatingTaskTransition(true);
         completeActiveTask(activeTask);
         if (solvingTransitionTimeout) clearTimeout(solvingTransitionTimeout);
-        const newTimeout = setTimeout(() => {
-          phaseTimer.stopEarly();
-          setCorrectItemsChosen(0);
-          setAnimatingTaskTransition(false);
-        }, 1000);
-        setSolvingTransitionTimeout(newTimeout);
+        setEndSolvingChapterAnimationTimeouts();
       }
       return true;
     }
