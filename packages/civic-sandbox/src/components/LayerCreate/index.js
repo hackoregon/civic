f90e24e2e5ae2f8d3396cc6bf4a56c1e8c13e395
Stephanie Keske <skeske@gmail.com>
2019-11-16 12:34:13 -0800

    starting simpler

---
+++ packages/civic-sandbox/src/components/LayerCreate/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from "react";
+
+function LayerCreateComponent() {
+  return <div> This is not a form. </div>;
+}
+
+export default LayerCreateComponent;
