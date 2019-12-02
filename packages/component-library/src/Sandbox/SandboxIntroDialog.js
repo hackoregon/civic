a6cf8f66a8fb1d37d15179cedba4a81668e9643b
Jaron Heard <jaronheard@gmail.com>
2019-12-01 16:30:01 -0800

    Update IntroDialog

--- packages/component-library/src/Sandbox/SandboxIntroDialog.js
+++ packages/component-library/src/Sandbox/SandboxIntroDialog.js
@@ -1,19 +1,32 @@
 import PropTypes from "prop-types";
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx, css } from "@emotion/core";
 
 import Dialog from "../Dialog/Dialog";
 import ButtonNew from "../ButtonNew/ButtonNew";
 
+const contentWrapper = css`
+  margin: 20px;
+`;
+
+const buttonWrapper = css`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
 const SandboxIntroDialog = ({ open, onClose }) => (
   <Dialog open={open} onClose={onClose}>
-    <h1>CIVIC Sandbox</h1>
-    <p>
-      The CIVIC Sandbox is a curated data exploration tool to encourage
-      exploration, enable comparison, and impact decision making.
-    </p>
-    <ButtonNew label="Add Your Data" />
-    <ButtonNew label="Explore Data" onClick={onClose} />
+    <div css={contentWrapper}>
+      <h1>CIVIC Sandbox</h1>
+      <h2>
+        The CIVIC Sandbox is a curated data exploration tool to encourage
+        exploration, enable comparison, and impact decision making.
+      </h2>
+      <div css={buttonWrapper}>
+        <ButtonNew label="Add Your Data" />
+        <ButtonNew label="Explore Data" onClick={onClose} />
+      </div>
+    </div>
   </Dialog>
 );
 
