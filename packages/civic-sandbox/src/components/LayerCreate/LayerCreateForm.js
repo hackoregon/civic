1a73c90bdf3848d851828dd3b288da5643381e31
Stephanie Keske <stephanie.keske@hackoregon.org>
2019-12-05 17:15:08 -0800

    fleshing out form

--- packages/civic-sandbox/src/components/LayerCreate/LayerCreateForm.js
+++ packages/civic-sandbox/src/components/LayerCreate/LayerCreateForm.js
@@ -1,38 +1,97 @@
-import React, { Fragment } from "react";
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { Fragment } from "react";
 import PropTypes from "prop-types";
 import * as Yup from "yup";
 
-import { Form, TextField } from "@hackoregon/component-library";
+import { Form, TextField, ButtonNew } from "@hackoregon/component-library";
+
+const wrapper = css`
+  padding-bottom: 80px;
+`;
 
 const fields = {
   email: {
     label: "Email",
-    section: "testSection",
+    section: "baseData",
     component: TextField,
     validation: Yup.string("Enter your email")
       .email("Enter a valid email")
       .required("Email is required")
   },
-  email2: {
-    label: "Email",
-    section: "testSection",
-    component: TextField,
-    validation: Yup.string("Enter your email")
-      .email("Enter a valid email")
-      .required("Email is required")
+  layerName: {
+    label: "Layer Name",
+    section: "baseData",
+    component: TextField
+  },
+  description: {
+    label: "Description",
+    section: "baseData",
+    component: TextField
+  },
+  creator: {
+    label: "Creator",
+    section: "baseData",
+    component: TextField
+  },
+  affiliation: {
+    label: "Affiliation",
+    section: "baseData",
+    component: TextField
+  },
+  rating: {
+    label: "Rating",
+    section: "baseData",
+    component: TextField
+  },
+  association: {
+    label: "Association",
+    section: "baseData",
+    component: TextField
+  },
+  tags: {
+    label: "Tags",
+    section: "baseData",
+    component: TextField
+  },
+  dataEndpoint: {
+    label: "GeoJSON Data Endpoint",
+    section: "dataDetails",
+    component: TextField
+  },
+  dateGranularity: {
+    label: "Data Endpoint",
+    section: "dataDetails",
+    component: TextField
+  },
+  defaultDate: {
+    label: "Data Endpoint",
+    section: "dataDetails",
+    component: TextField
+  },
+  mapType: {
+    label: "Map Type",
+    section: "mapType",
+    component: TextField
   }
 };
 
 const LayerCreateForm = ({ initialValues, onSubmit, children }) => (
   <Fragment>
-    <Form
-      fields={fields}
-      sections={["testSection"]}
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-    >
-      {children}
-    </Form>
+    <div css={wrapper}>
+      <Form
+        fields={fields}
+        sections={["baseData", "dataDetails", "mapType"]}
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+      >
+        {children}
+      </Form>
+    </div>
+    <ButtonNew label="Next" />
   </Fragment>
 );
 
--- packages/civic-sandbox/src/components/LayerCreate/index.js
+++ packages/civic-sandbox/src/components/LayerCreate/index.js
@@ -5,6 +5,7 @@
 import { jsx, css } from "@emotion/core";
 import React, { Fragment } from "react";
 import LayerCreateForm from "./LayerCreateForm";
+import "@hackoregon/component-library/assets/global.styles.css";
 
 const formTitle = css`
   font-size: 35px;
@@ -12,25 +13,72 @@ const formTitle = css`
   margin: 10px 0px;
 `;
 
+const sectionHeader = css`
+  font-size: 20px;
+  letter-spacing: -0.5px;
+  margin: 80px 0px 10px 0px;
+  font-weight: bold;
+`;
+
+const container = css`
+  position: absolute;
+  top: 50px;
+  left: 25px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  width: 575px;
+  background: rgba(243, 242, 243, 0.9);
+  color: rgb(85, 85, 85);
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  box-shadow: 5px 5px 15px -3px rgba(0, 0, 0, 0.2);
+  @media (max-width: 900px) {
+    width: 90%;
+    bottom: 30px;
+    left: 8%;
+  }
+`;
+
 class LayerCreateComponent extends React.Component {
   render() {
     return (
-      <LayerCreateForm
-        onSubmit={values => {
-          console.log(values);
-        }}
-        initialValues={{
-          email: ""
-        }}
-      >
-        {({ formSections }) => (
-          <Fragment>
-            <p css={formTitle}>Test Form</p>
-            <p>This is a test form with a text field.</p>
-            {formSections.testSection}
-          </Fragment>
-        )}
-      </LayerCreateForm>
+      <div css={container}>
+        <LayerCreateForm
+          onSubmit={values => {
+            console.log(values);
+          }}
+          initialValues={{
+            email: ""
+          }}
+        >
+          {({ formSections }) => (
+            <Fragment>
+              <p css={formTitle}>Create a New CIVIC Sandbox Layer</p>
+              <p>
+                Let CIVIC Sandbox tell the story of your data. This form will
+                help you upload your dataset and choose the best way to display
+                it. Currently we support GeoJSON datasets.
+              </p>
+              <p>
+                If you have a question, get in touch at *insert email here*.{" "}
+              </p>
+              <p css={sectionHeader}>
+                First, let&#39;s get the basics out of the way:
+              </p>
+              {formSections.baseData}
+              <p css={sectionHeader}>
+                Okay, tell us more about the data itself:
+              </p>
+              {formSections.dataDetails}
+              <p css={sectionHeader}>
+                Next, let&#39;s choose the best type of map for your data:
+              </p>
+              {formSections.mapType}
+            </Fragment>
+          )}
+        </LayerCreateForm>
+      </div>
     );
   }
 }
