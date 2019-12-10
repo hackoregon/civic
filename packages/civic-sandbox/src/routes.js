9f0f7c6e6ca00368328498d62fbc6fd412391930
Jaron Heard <jaronheard@gmail.com>
2019-12-09 22:52:03 -0500

    update child route

--- packages/civic-sandbox/src/routes.js
+++ packages/civic-sandbox/src/routes.js
@@ -1,17 +1,9 @@
-import App from "./components/App";
 import LayerCreate from "./components/LayerCreate";
 
 const routes = () => [
   {
-    path: "sandbox",
-    name: "sandbox",
-    indexRoute: { component: App },
-    childRoutes: [
-      {
-        path: "layercreate",
-        component: LayerCreate
-      }
-    ]
+    path: "layercreate",
+    component: LayerCreate
   }
 ];
 
