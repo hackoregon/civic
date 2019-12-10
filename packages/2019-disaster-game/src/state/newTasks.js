44567c46dd24327449e63b7e68fccf8917f1497e
Dianna Faulk <diannafaulk@gmail.com>
2019-12-09 18:21:39 -0800

    Begin creating new tasks reducer

---
+++ packages/2019-disaster-game/src/state/newTasks.js
@@ -0,0 +1,167 @@
+import { createReducer, createSelector } from "redux-starter-kit";
+import { shuffle } from "lodash";
+
+import {
+  tasks,
+  tasksForEnvironment,
+  URBAN,
+  hardcodedTaskLocations
+} from "../constants/tasks";
+import Timer from "../utils/timer";
+
+// CONSTANTS
+const defaultEnv = URBAN;
+
+export const taskPhaseKeys = {
+  SOLVING_SAVE_YOURSELF: "SOLVING_SAVE_YOURSELF",
+  SOLVING_SAVE_OTHERS: "SOLVING_SAVE_OTHERS",
+  MODAL_SAVE_OTHERS_INTRO: "MODAL_SAVE_OTHERS_INTRO",
+  CHOOSE_TASK: "CHOOSE_TASK",
+  MODAL_CHOSEN_TASK: "MODAL_CHOSEN_TASK",
+  MODAL_SOLVED_TASK: "MODAL_SOLVED_TASK",
+  MODAL_UNSOLVED_TASK: "MODAL_UNSOLVED_TASK",
+  MODAL_NO_ITEM: "MODAL_NO_ITEM"
+};
+
+const {
+  SOLVING_SAVE_YOURSELF,
+  SOLVING_SAVE_OTHERS,
+  MODAL_SAVE_OTHERS_INTRO,
+  CHOOSE_TASK,
+  MODAL_CHOSEN_TASK,
+  MODAL_SOLVED_TASK,
+  MODAL_UNSOLVED_TASK,
+  MODAL_NO_ITEM
+} = taskPhaseKeys;
+
+const taskPhases = {
+  [SOLVING_SAVE_YOURSELF]: { time: 30 },
+  [SOLVING_SAVE_OTHERS]: { time: 20 },
+  [MODAL_SAVE_OTHERS_INTRO]: { time: 10 },
+  [CHOOSE_TASK]: { time: 20 },
+  [MODAL_CHOSEN_TASK]: { time: 10 },
+  [MODAL_SOLVED_TASK]: { time: 10 },
+  [MODAL_UNSOLVED_TASK]: { time: 10 },
+  [MODAL_NO_ITEM]: { time: 10 }
+};
+
+const actionTypes = {
+  GO_TO_NEXT_TASK_PHASE_NEW: "GO_TO_NEXT_TASK_PHASE_NEW"
+};
+
+const phaseTimer = new Timer();
+
+// INITIAL STATE
+const initialState = {
+  tasks,
+  taskLocations: hardcodedTaskLocations,
+  tasksForEnvironment,
+  activeEnvironment: defaultEnv,
+  taskOrder: shuffle(tasksForEnvironment[defaultEnv].saveYourself),
+  activeTaskIndex: -1,
+  activeTaskPhase: SOLVING_SAVE_YOURSELF,
+  taskPhases
+};
+
+// ACTIONS
+export const goToNextTaskPhase = () => dispatch => {
+  phaseTimer.reset();
+  phaseTimer.addCompleteCallback(() => {
+    goToNextTaskPhase()(dispatch);
+  });
+  dispatch({ type: actionTypes.GO_TO_NEXT_TASK_PHASE_NEW, phaseTimer });
+};
+
+export const completeTask = () => dispatch => {
+  phaseTimer.reset();
+  phaseTimer.addCompleteCallback(() => {
+    goToNextTaskPhase()(dispatch);
+  });
+  dispatch({ type: actionTypes.COMPLETE_TASK_NEW });
+  dispatch({ type: actionTypes.GO_TO_NEXT_TASK_PHASE_NEW, phaseTimer });
+};
+
+// REDUCER HELPERS
+/* eslint-disable no-param-reassign */
+
+// Mark task as completed, increase activeTaskIndex, change to next phase if applicable, and start timer
+const completeSaveYourselfTask = state => {
+  if (state.taskOrder[state.activeTaskIndex])
+    state.taskOrder[state.activeTaskIndex].completed = true;
+  if (state.activeTaskIndex === 1) {
+    state.activeTaskPhase = MODAL_SAVE_OTHERS_INTRO;
+    phaseTimer.setDuration(taskPhases[MODAL_SAVE_OTHERS_INTRO].time);
+  } else {
+    phaseTimer.setDuration(taskPhases[SOLVING_SAVE_YOURSELF].time);
+  }
+  state.activeTaskIndex += 1;
+};
+
+const getRandomNumberFromRange = range => {
+  const min = Math.ceil(range[0]);
+  const max = Math.floor(range[1]);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// REDUCERS
+export const tasksReducer = createReducer(initialState, {
+  [actionTypes.GO_TO_NEXT_TASK_PHASE_NEW]: (state, action) => {
+    // add check at start if game should end
+    if (state.activeTaskPhase === SOLVING_SAVE_YOURSELF) {
+      completeSaveYourselfTask(state, action);
+    } else if (state.activeTaskPhase === MODAL_SAVE_OTHERS_INTRO) {
+      state.activeTaskPhase = CHOOSE_TASK;
+      phaseTimer.setDuration(taskPhases[CHOOSE_TASK].time);
+    } else if (state.activeTaskPhase === CHOOSE_TASK) {
+      state.activeTaskPhase = MODAL_CHOSEN_TASK;
+      phaseTimer.setDuration(taskPhases[MODAL_CHOSEN_TASK].time);
+    } else if (state.activeTaskPhase === MODAL_CHOSEN_TASK) {
+      state.activeTaskPhase = SOLVING_SAVE_OTHERS;
+      phaseTimer.setDuration(taskPhases[SOLVING_SAVE_OTHERS].time);
+    } else if (state.activeTaskPhase === SOLVING_SAVE_OTHERS) {
+      if (action.solved === true) {
+        state.activeTaskPhase = MODAL_SOLVED_TASK;
+        phaseTimer.setDuration(taskPhases[MODAL_SOLVED_TASK].time);
+      } else if (action.solved === false) {
+        state.activeTaskPhase = MODAL_UNSOLVED_TASK;
+        phaseTimer.setDuration(taskPhases[MODAL_UNSOLVED_TASK].time);
+      } else {
+        state.activeTaskPhase = MODAL_NO_ITEM;
+        phaseTimer.setDuration(taskPhases[MODAL_NO_ITEM].time);
+      }
+    } else if (
+      state.activeTaskPhase === MODAL_SOLVED_TASK ||
+      state.activeTaskPhase === MODAL_UNSOLVED_TASK ||
+      state.activeTaskPhase === MODAL_NO_ITEM
+    ) {
+      state.activeTaskPhase = CHOOSE_TASK;
+      phaseTimer.setDuration(taskPhases[CHOOSE_TASK].time);
+    }
+    phaseTimer.start();
+  },
+  [actionTypes.COMPLETE_TASK_NEW]: state => {
+    const completedTask = state.taskOrder[state.activeTaskIndex];
+    completedTask.completed = true;
+    completedTask.completedResults = {
+      people: getRandomNumberFromRange(completedTask.peopleSavedRange),
+      pets: completedTask.petsSaved
+    };
+  }
+});
+/* eslint-enable no-param-reassign */
+
+export default tasksReducer;
+
+// SELECTORS
+
+export const getActiveTask = createSelector(
+  ["newTasks.taskOrder", "newTasks.activeTaskIndex"],
+  (taskOrder, activeTaskIndex) => {
+    return taskOrder[activeTaskIndex];
+  }
+);
+
+export const getTaskPhase = createSelector(
+  ["newTasks.activeTaskPhase"],
+  activeTaskPhase => activeTaskPhase
+);
