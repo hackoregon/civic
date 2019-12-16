7dab1ddf1024fb3edef773f14913267da797a9f8
Dianna Faulk <diannafaulk@gmail.com>
2019-12-16 06:06:37 -0800

    Abstract badges to constants

---
+++ packages/2019-disaster-game/src/constants/badges.js
@@ -0,0 +1,67 @@
+// import teamworkBadge from "../../assets/badges/teamwork.svg";
+// import superTeamBadge from "../../assets/badges/super-team.svg";
+import preparerBadge from "../../assets/badges/preparer.svg";
+// import masterPreparerBadge from "../../assets/badges/master-preparer.svg";
+import taskSurvivorBadge from "../../assets/badges/task-survivor.svg";
+import taskNeighborhoodHeroBadge from "../../assets/badges/task-neighborhood-hero.svg";
+import taskCitySuperheroBadge from "../../assets/badges/task-city-superhero.svg";
+import earthquakeHeroBadge from "../../assets/badges/earthquake-hero.svg";
+
+export default {
+  badgesAcquired: 0,
+  // teamwork: {
+  //   teamworkBadge: {
+  //     title: "Teamwork",
+  //     id: "teamworkBadge",
+  //     badgeSVG: teamworkBadge,
+  //     acquired: false
+  //   },
+  //   superTeamBadge: {
+  //     title: "Super Team",
+  //     id: "superTeamBadge",
+  //     badgeSVG: superTeamBadge,
+  //     acquired: false
+  //   }
+  // },
+  prepared: {
+    preparerBadge: {
+      title: "Preparer",
+      id: "preparerBadge",
+      badgeSVG: preparerBadge,
+      acquired: false
+    }
+    // Maybe do if has 3 of each special item
+    // masterPreparerBadge: {
+    //   title: "Master Preparer",
+    //   id: "masterPreparerBadge",
+    //   badgeSVG: masterPreparerBadge,
+    //   acquired: false
+    // }
+  },
+  hero: {
+    taskSurvivorBadge: {
+      title: "Survivor",
+      id: "taskSurvivorBadge",
+      badgeSVG: taskSurvivorBadge,
+      acquired: false
+    },
+    taskNeighborhoodHeroBadge: {
+      title: "Neighborhood Hero",
+      id: "taskNeighborhoodHeroBadge",
+      badgeSVG: taskNeighborhoodHeroBadge,
+      acquired: false
+    },
+    taskCitySuperheroBadge: {
+      title: "City Superhero",
+      id: "taskCitySuperheroBadge",
+      badgeSVG: taskCitySuperheroBadge,
+      acquired: false
+    },
+    earthquakeHeroBadge: {
+      title: "Earthquake Hero",
+      id: "earthquakeHeroBadge",
+      badgeSVG: earthquakeHeroBadge,
+      acquired: false
+    }
+  }
+};
--- packages/2019-disaster-game/src/state/user.js
+++ packages/2019-disaster-game/src/state/user.js
@@ -1,12 +1,5 @@
 import { createReducer, createSelector } from "redux-starter-kit";
-// import teamworkBadge from "../../assets/badges/teamwork.svg";
-// import superTeamBadge from "../../assets/badges/super-team.svg";
-import preparerBadge from "../../assets/badges/preparer.svg";
-// import masterPreparerBadge from "../../assets/badges/master-preparer.svg";
-import taskSurvivorBadge from "../../assets/badges/task-survivor.svg";
-import taskNeighborhoodHeroBadge from "../../assets/badges/task-neighborhood-hero.svg";
-import taskCitySuperheroBadge from "../../assets/badges/task-city-superhero.svg";
-import earthquakeHeroBadge from "../../assets/badges/earthquake-hero.svg";
+import badges from "../constants/badges";
 
 // INITIAL STATE
 // items will be a list of objects, where each object is an id and a quantity
@@ -15,64 +8,7 @@ const initialState = {
   points: 0,
   petsSaved: 0,
   peopleSaved: 0,
-  badges: {
-    badgesAcquired: 0,
-    // teamwork: {
-    //   teamworkBadge: {
-    //     title: "Teamwork",
-    //     id: "teamworkBadge",
-    //     badgeSVG: teamworkBadge,
-    //     acquired: false
-    //   },
-    //   superTeamBadge: {
-    //     title: "Super Team",
-    //     id: "superTeamBadge",
-    //     badgeSVG: superTeamBadge,
-    //     acquired: false
-    //   }
-    // },
-    prepared: {
-      preparerBadge: {
-        title: "Preparer",
-        id: "preparerBadge",
-        badgeSVG: preparerBadge,
-        acquired: false
-      }
-      // Maybe do if has 3 of each special item
-      // masterPreparerBadge: {
-      //   title: "Master Preparer",
-      //   id: "masterPreparerBadge",
-      //   badgeSVG: masterPreparerBadge,
-      //   acquired: false
-      // }
-    },
-    hero: {
-      taskSurvivorBadge: {
-        title: "Survivor",
-        id: "taskSurvivorBadge",
-        badgeSVG: taskSurvivorBadge,
-        acquired: false
-      },
-      taskNeighborhoodHeroBadge: {
-        title: "Neighborhood Hero",
-        id: "taskNeighborhoodHeroBadge",
-        badgeSVG: taskNeighborhoodHeroBadge,
-        acquired: false
-      },
-      taskCitySuperheroBadge: {
-        title: "City Superhero",
-        id: "taskCitySuperheroBadge",
-        badgeSVG: taskCitySuperheroBadge,
-        acquired: false
-      },
-      earthquakeHeroBadge: {
-        title: "Earthquake Hero",
-        id: "earthquakeHeroBadge",
-        badgeSVG: earthquakeHeroBadge,
-        acquired: false
-      }
-    }
-  }
+  badges
 };
 
 // CONSTANTS
