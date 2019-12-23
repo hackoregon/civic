3498e5391a8ab53754523489fb19815fb4436051
Dianna Faulk <diannafaulk@gmail.com>
2019-12-23 02:38:07 -0800

    Change badges drawer for summary screen only use

--- packages/2019-disaster-game/src/components/atoms/TitleBar/Badge.js
+++ packages/2019-disaster-game/src/components/atoms/TitleBar/Badge.js
@@ -3,28 +3,51 @@ import { css, jsx } from "@emotion/core";
 import { PropTypes } from "prop-types";
 
 import EmptyBadgeSVG from "../../../../assets/badges/empty-badge.svg";
+import DarkEmptyBadgeSVG from "../../../../assets/badges/empty-badge-dark.svg";
+import { palette } from "../../../constants/style";
 
 const badgeContainer = css`
   display: grid;
   justify-content: center;
+  justify-items: center;
+  align-content: start;
 `;
 
 const badgeStyle = css`
   height: 75px;
 `;
 
-const Badge = ({ badge }) => {
+const summaryBadgeStyle = css`
+  height: 110px;
+`;
+
+const titleStyle = css`
+  margin: 0;
+  font-family: "Akkurat", sans-serif;
+  font-weight: bold;
+  font-size: 2em;
+  line-height: 1.3em;
+  text-align: center;
+  color: ${palette.darkGrey};
+`;
+
+const Badge = ({ badge, isSummary }) => {
   return (
     <div css={badgeContainer}>
       {badge.shown ? (
         <img
           src={badge.badgeSVG}
           alt={`${badge.title} badge`}
-          css={badgeStyle}
+          css={isSummary ? summaryBadgeStyle : badgeStyle}
         />
       ) : (
-        <img src={EmptyBadgeSVG} alt="Badge slot" css={badgeStyle} />
+        <img
+          src={isSummary ? DarkEmptyBadgeSVG : EmptyBadgeSVG}
+          alt="Badge slot"
+          css={isSummary ? summaryBadgeStyle : badgeStyle}
+        />
       )}
+      {isSummary && <p css={titleStyle}>{badge.title}</p>}
     </div>
   );
 };
@@ -36,7 +59,8 @@ Badge.propTypes = {
     id: PropTypes.string,
     shown: PropTypes.bool,
     activeTaskIndexWhenEarned: PropTypes.oneOfType([null, PropTypes.number])
-  })
+  }),
+  isSummary: PropTypes.bool
 };
 
 export default Badge;
--- packages/2019-disaster-game/src/components/atoms/TitleBar/BadgesDrawer.js
+++ packages/2019-disaster-game/src/components/atoms/TitleBar/BadgesDrawer.js
@@ -2,111 +2,49 @@
 import { css, jsx } from "@emotion/core";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { palette } from "../../../constants/style";
 import { getBadges } from "../../../state/tasks";
-import Badge from "../Badge";
+import Badge from "./Badge";
 
-const drawerContainer = css`
-  width: 950px;
-  background-color: ${palette.lemon};
-  grid-template-columns: auto 700px;
-  padding-left: 200px;
-  margin-left: -130px;
-  z-index: unset;
-  transition: all 1s;
-  box-shadow: 0px 4px 2px 0px rgba(0, 0, 0, 0.15);
-`;
-
-const closedStyle = css`
-  transform: translateX(-100%);
-`;
-
-const openStyle = css`
-  transform: translateX(0%);
-`;
-
-const summaryStyle = css`
+const container = css`
+  width: 885px;
   top: 90px;
   right: 950px;
   position: absolute;
-
   z-index: 10;
   transition: all 1s;
   transform: none;
-
   display: grid;
   grid-template-columns: auto 630px;
   padding: 0 80px;
-  border-radius: 80px;
+  border-radius: 20px;
   align-content: center;
   justify-content: space-between;
   justify-self: end;
-  height: 130px;
-`;
-
-const headerStyle = css`
-  font-family: "Luckiest Guy", sans-serif;
-  font-size: 3.75rem;
-  line-height: initial;
-  color: ${palette.blue};
-  margin: 0;
+  transform: translateX(-5%);
 `;
 
 const badgesContainer = css`
   display: grid;
   grid-template-columns: repeat(5, auto);
   align-content: center;
+  grid-column-gap: 40px;
+  justify-items: center;
 `;
 
-const BadgesDrawer = ({
-  journeyBarContainerStyle,
-  isOpen,
-  isSummary,
-  initialSummaryStyle,
-  badges,
-  openBadgeDrawer
-}) => {
+const BadgesDrawer = ({ initialSummaryStyle, badges }) => {
   return (
     <div
       css={css`
-        ${journeyBarContainerStyle};
-        ${drawerContainer};
-        ${isOpen ? openStyle : closedStyle};
-        ${isSummary ? summaryStyle : ""};
+        ${container};
         ${initialSummaryStyle || ""};
       `}
     >
-      <p css={headerStyle}>
-        Badges
-        <br />
-        Earned
-      </p>
       <div css={badgesContainer}>
-        <Badge
-          badgeInfo={badges.preparerBadge}
-          openBadgeDrawer={openBadgeDrawer}
-          isSummary={isSummary}
-        />
-        <Badge
-          badgeInfo={badges.taskSurvivorBadge}
-          openBadgeDrawer={openBadgeDrawer}
-          isSummary={isSummary}
-        />
-        <Badge
-          badgeInfo={badges.taskNeighborhoodHeroBadge}
-          openBadgeDrawer={openBadgeDrawer}
-          isSummary={isSummary}
-        />
-        <Badge
-          badgeInfo={badges.taskCitySuperheroBadge}
-          openBadgeDrawer={openBadgeDrawer}
-          isSummary={isSummary}
-        />
-        <Badge
-          badgeInfo={badges.earthquakeHeroBadge}
-          openBadgeDrawer={openBadgeDrawer}
-          isSummary={isSummary}
-        />
+        <Badge isSummary badge={badges.preparerBadge} />
+        <Badge isSummary badge={badges.taskSurvivorBadge} />
+        <Badge isSummary badge={badges.taskNeighborhoodHeroBadge} />
+        <Badge isSummary badge={badges.taskCitySuperheroBadge} />
+        <Badge isSummary badge={badges.earthquakeHeroBadge} />
       </div>
     </div>
   );
@@ -114,11 +52,8 @@ const BadgesDrawer = ({
 
 BadgesDrawer.propTypes = {
   journeyBarContainerStyle: PropTypes.shape({}),
-  isSummary: PropTypes.bool,
   initialSummaryStyle: PropTypes.shape({}),
-  badges: PropTypes.shape({}),
-  isOpen: PropTypes.bool,
-  openBadgeDrawer: PropTypes.func
+  badges: PropTypes.shape({})
 };
 
 const mapStateToProps = state => ({
