e143f30c930d07edd0ff6cb06bb22cb761cc671d
Cosmo Kent <cosmolightfoot@gmail.com>
2019-12-27 18:11:15 -0800

    updated tags list in 2018 package to reflect tags present on cards in explore page

--- packages/2018/src/tags.js
+++ packages/2018/src/tags.js
@@ -1,5 +1,25 @@
 export default {
-  topics: ["Transportation", "Disaster Resilience", "Housing", "Race"],
-  locations: ["Portland", "Oregon", "Nationwide", "Your City"],
+  topics: [
+    "Transportation",
+    "Disaster Resilience",
+    "Housing",
+    "Race",
+    "Earthquake",
+    "Homeownership",
+    "Wealth",
+    "Redlining",
+    "HOLC",
+    "Permits",
+    "ADUs",
+    "Black",
+    "Gentrification"
+  ],
+  locations: [
+    "Portland",
+    "Oregon",
+    "Nationwide",
+    "Your City",
+    "Tillamook County"
+  ],
   visualizations: ["Chart", "Map", "Infographic", "Quiz"]
 };
