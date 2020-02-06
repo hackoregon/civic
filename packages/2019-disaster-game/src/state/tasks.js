2e00126ee1805e385b474c666ea4dbaa5d8672b0
Dianna Faulk <diannafaulk@gmail.com>
2020-02-06 14:58:29 -0800

    Shorten modal times

--- packages/2019-disaster-game/src/state/tasks.js
+++ packages/2019-disaster-game/src/state/tasks.js
@@ -44,13 +44,13 @@ const taskPhasesChapterCanEndAfter = [
 const taskPhases = {
   [SOLVING_SAVE_YOURSELF]: { time: 31 },
   [SOLVING_SAVE_OTHERS]: { time: 21 },
-  [MODAL_SAVE_OTHERS_INTRO]: { time: 8 },
+  [MODAL_SAVE_OTHERS_INTRO]: { time: 6 },
   [CHOOSE_TASK]: { time: 21 },
-  [MODAL_CHOSEN_TASK]: { time: 8 },
-  [MODAL_SOLVED_TASK]: { time: 8 },
-  [MODAL_UNSOLVED_TASK]: { time: 8 },
-  [MODAL_NO_ITEM]: { time: 8 },
-  [MODAL_BADGE_EARNED]: { time: 8 }
+  [MODAL_CHOSEN_TASK]: { time: 6 },
+  [MODAL_SOLVED_TASK]: { time: 6 },
+  [MODAL_UNSOLVED_TASK]: { time: 6 },
+  [MODAL_NO_ITEM]: { time: 6 },
+  [MODAL_BADGE_EARNED]: { time: 6 }
 };
 
 const actionTypes = {
