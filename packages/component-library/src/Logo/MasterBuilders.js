701aa331e26091478cbbb415fe3ed6f5c454f780
Jaron Heard <jaronheard@gmail.com>
2020-02-13 18:11:08 -0800

    add logos for hack oregon and master builders

---
+++ packages/component-library/src/Logo/MasterBuilders.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import isClient from "../utils/isClient";
+import masterBuilders from "../../assets/civic-master-builders.svg";
+
+const styles = {
+  height: "60px",
+  width: "auto"
+};
+
+const MasterBuilders = ({ alt, ...other }) =>
+  isClient && <img style={styles} src={masterBuilders} alt={alt} {...other} />;
+
+MasterBuilders.displayName = "Logo";
+MasterBuilders.propTypes = {
+  alt: PropTypes.string
+};
+
+export default MasterBuilders;
