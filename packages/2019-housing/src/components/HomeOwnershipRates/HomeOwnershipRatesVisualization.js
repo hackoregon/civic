a3a31979bb21cdbe3852f4ab3bf048fd85b6eadf
Jaron Heard <jaronheard@gmail.com>
2020-03-02 09:47:32 -0800

    replace subtitle

--- packages/2019-housing/src/components/HomeOwnershipRates/HomeOwnershipRatesVisualization.js
+++ packages/2019-housing/src/components/HomeOwnershipRates/HomeOwnershipRatesVisualization.js
@@ -28,8 +28,8 @@ const HomeOwnershipRatesVisualization = ({ data }) => {
           x: [1990, 2017],
           y: [0, 0.83]
         }}
-        subtitle="subtitle"
-        title="Home Ownership By Race In Multnomah County 1990 to 2017"
+        subtitle="Multnomah County, 1990 to 2017, census data"
+        title="Home Ownership By Race"
         xLabel="Census Year"
         yLabel="Home Ownership Rate"
         xNumberFormatter={x => civicFormat.year(x)}
