9a0f731071bff127540273b461015f66f6471d5c
Jaron Heard <jaronheard@gmail.com>
2020-03-05 02:40:43 -0800

    add isClient export

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -17,6 +17,7 @@ export { default as ungroupBy } from "./utils/ungroupBy";
 export { default as DemoJSONLoader } from "./DemoJSONLoader/DemoJSONLoader";
 export { default as ErrorBoundary } from "./ErrorBoundary/ErrorBoundary";
 export { default as protectData } from "./utils/protectData";
+export { default as isClient } from "./utils/isClient";
 
 export {
   wallOfText,
