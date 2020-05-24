9e848111cc5a3e2eb2b3900f1e50880e3a44f320
Jaron Heard <jaronheard@gmail.com>
2020-05-24 14:28:27 -0700

    move ui-platform to toMove

--- packages/ui-cards/stories/CivicCardSample/SampleCardVisualization.js
+++ packages/ui-cards/stories/CivicCardSample/SampleCardVisualization.js
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { civicFormat } from "@hackoregon/utils";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { LineChart } from "@hackoregon/ui-charts";
+
+// this file should only be used for stories
 
 function SampleCardVisualization({ isLoading, data }) {
   return (
