b4a17d3ccfd8990d6d06d5ed1fd0a68738017cd5
Jaron Heard <jaronheard@gmail.com>
2020-09-05 01:00:24 -0400

    setup hot reloading

--- packages/webpack-common/index.js
+++ packages/webpack-common/index.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const {
   createConfig,
@@ -52,7 +53,17 @@ module.exports = {
         publicPath: "/",
         filename: "[name].bundle.js"
       }),
-      babel({ rootMode: "upward" }),
+      match(
+        ["*.js", "!*node_modules*"],
+        [
+          babel({
+            rootMode: "upward",
+            plugins: [!isProd && require.resolve("react-refresh/babel")].filter(
+              Boolean
+            )
+          })
+        ]
+      ),
       match(
         ["*.css"],
         [
@@ -81,7 +92,12 @@ module.exports = {
         addPlugins([
           new webpack.HotModuleReplacementPlugin(),
           new webpack.NamedModulesPlugin(),
-          new BundleAnalyzerPlugin({ openAnalyzer: false })
+          new BundleAnalyzerPlugin({ openAnalyzer: false }),
+          new ReactRefreshWebpackPlugin({
+            overlay: {
+              sockIntegration: "whm"
+            }
+          })
         ])
       ]),
       env("production", [])
