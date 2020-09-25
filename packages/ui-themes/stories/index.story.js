778309ea3c82ed4dfaa17ed36f430252321f10c1
Jaron Heard <jaronheard@gmail.com>
2020-09-24 22:40:01 -0400

    remove blank ui-theme story file

--- packages/ui-themes/stories/index.story.js
+++
@@ -1,8 +0,0 @@
-// If there was theme story it would live here
-
-// import React from "react";
-// import { storiesOf } from "@storybook/react";
-
-// storiesOf("Welcome/Theme", module)
-//   .addParameters({ options: { showPanel: false } })
-//   .add("Theme", () => <Theme />)
