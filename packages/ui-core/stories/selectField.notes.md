3f334e285818a18132c653d36517b33254514b4e
Jaron Heard <jaronheard@gmail.com>
2020-09-25 02:36:49 -0400

    remove unused stories directories

--- packages/ui-core/stories/selectField.notes.md
+++
@@ -1,5 +0,0 @@
-# SelectField Component
-
-## Standard
-
-The Standard story shows uses the standard usage of the SelectField for the CIVIC platform. SelectField includes standard styling.
--- packages/utils/stories/index.story.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { storiesOf } from "@storybook/react";
-
-// DO NOT REMOVE OR MODIFY THIS COMMENT - hygen component generator import injection
-
-storiesOf("Welcome/Utils", module).addParameters({
-  options: { showPanel: false }
-});
-// DO NOT REMOVE OR MODIFY THIS COMMENT - hygen component generator story injection
