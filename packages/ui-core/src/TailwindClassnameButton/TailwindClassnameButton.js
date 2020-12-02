0dc49fb438541fdedf0a3cbf2f0be4c587ade5d4
Vincent Gonzalez <elvincent@gmail.com>
2020-12-01 23:40:43 -0700

    configure variable defaults

--- packages/ui-core/src/TailwindClassnameButton/TailwindClassnameButton.js
+++ packages/ui-core/src/TailwindClassnameButton/TailwindClassnameButton.js
@@ -13,7 +13,7 @@ export const TailwindClassnameButton = () => (
   <div className="grid items-center justify-center h-screen">
     <button
       type="button"
-      className="relative flex justify-center w-64 min-w-full px-4 py-2 text-sm font-bold leading-4 text-white border border-transparent rounded-md bg-red-600 hover:bg-red-500 active:bg-red-700 transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+      className="relative flex justify-center px-4 py-2 w-64 min-w-full text-white text-sm font-bold leading-4 hover:bg-red-500 bg-red-600 active:bg-red-700 border border-transparent focus:border-transparent rounded-md focus:outline-none transition duration-150 ease-in-out focus:ring-purple focus:ring-2"
     >
       className + Tailwind
     </button>
@@ -29,7 +29,7 @@ export const TailwindClassnameButton = () => (
 
 // const styles = {
 //   container: "grid items-center justify-center h-screen",
-//   button: "relative flex justify-center w-64 min-w-full px-4 py-2 text-sm font-bold leading-4 text-white border border-transparent rounded-md bg-red-600 hover:bg-red-500 active:bg-red-700 transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+//   button: "relative flex justify-center w-64 min-w-full px-4 py-2 text-sm font-bold leading-4 text-white border border-transparent rounded-md bg-red-600 hover:bg-red-500 active:bg-red-700 transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
 // }
 
 // export const TailwindClassnameButton = () => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,41 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: "#201024",
+      white: "#ffffff",
+      red: {
+        50: "#fef2f2",
+        100: "#fee2e2",
+        200: "#fecaca",
+        300: "#fca5a5",
+        400: "#f87171",
+        500: "#ef4444",
+        600: "#dc2626",
+        700: "#b91c1c",
+        800: "#991b1b",
+        900: "#7f1d1d"
+      },
+      primary: "#201024",
+      secondary: "#DC4556",
+      tertiarty: "#201024",
+      medium: "#AAA4AB",
+      subdued: "#F3F2F3",
+      action: {
+        DEFAULT: "#1E62BD",
+        plum: "#726371"
+      },
+      pink: "#DC4556",
+      blue: "#1E62BD",
+      green: "#19B7AA",
+      purple: "#721D7C",
+      yellow: "#FFB226"
+    },
+    fontFamily: {
+      sans: ["Roboto Condensed", "sans-serif"],
+      serif: ["Merriweather", "serif"]
+    },
     extend: {}
   },
   variants: {
