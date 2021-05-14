19f1004ca0394268f902e7a37d23aa723073dcd6
Jaron Heard <jaronheard@gmail.com>
2021-05-14 11:22:05 -0700

    add icons to accordion labels

--- packages/2019-housing/src/components/HousingDisplacement/Context.js
+++ packages/2019-housing/src/components/HousingDisplacement/Context.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx, css } from "@emotion/core";
 import { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Accordion from "@material-ui/core/Accordion";
@@ -38,7 +38,31 @@ function ContextAccordion() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <h2>About this data</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+              />
+            </svg>
+            <h2>About this data</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <About />
@@ -50,7 +74,31 @@ function ContextAccordion() {
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <h2>Data Lineage</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+              />
+            </svg>
+            <h2>Lineage</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <Lineage />
@@ -62,7 +110,31 @@ function ContextAccordion() {
           aria-controls="panel3a-content"
           id="panel3a-header"
         >
-          <h2>Methodology</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
+              />
+            </svg>
+            <h2>Methodology</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <Methodology />
@@ -74,7 +146,31 @@ function ContextAccordion() {
           aria-controls="panel4a-content"
           id="panel4a-header"
         >
-          <h2>Governance</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+              />
+            </svg>
+            <h2>Governance</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <Governance />
@@ -86,7 +182,31 @@ function ContextAccordion() {
           aria-controls="panel5a-content"
           id="panel5a-header"
         >
-          <h2>Data Access</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+              />
+            </svg>
+            <h2>Data Access</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <Access />
@@ -98,7 +218,31 @@ function ContextAccordion() {
           aria-controls="panel6a-content"
           id="panel6a-header"
         >
-          <h2>Privacy & Security</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+              />
+            </svg>
+            <h2>Privacy & Security</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <PrivacySecurity />
@@ -110,7 +254,31 @@ function ContextAccordion() {
           aria-controls="panel7a-content"
           id="panel7a-header"
         >
-          <h2>Data Constituent Engagement</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
+              />
+            </svg>
+            <h2>Data Constituent Engagement</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <Constituents />
@@ -122,7 +290,31 @@ function ContextAccordion() {
           aria-controls="panel8a-content"
           id="panel8a-header"
         >
-          <h2>Attribution</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+            <h2>Attribution</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <Attribution />
@@ -134,7 +326,31 @@ function ContextAccordion() {
           aria-controls="panel9a-content"
           id="panel9a-header"
         >
-          <h2>Links & Resources</h2>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+            `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              css={css`
+                height: 1.5rem;
+                margin-right: 0.5rem;
+              `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+            <h2>Links & Resources</h2>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <LinksResources />
