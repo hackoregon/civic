3be3a824f0209ac274356229b2b6f14af86fb537
Jaron Heard <jaronheard@gmail.com>
2021-05-14 17:57:29 -0700

    update checkbox alignment

--- packages/2019-housing/src/components/HousingDisplacement/Checkbox.js
+++ packages/2019-housing/src/components/HousingDisplacement/Checkbox.js
@@ -1,15 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx, css } from "@emotion/core";
 import CheckBoxIcon from "@material-ui/icons/CheckBox";
 import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 import { bool } from "prop-types";
 
 const Checkbox = ({ checked }) => {
   return checked ? (
-    <CheckBoxIcon fontSize="small" />
+    <CheckBoxIcon
+      fontSize="small"
+      css={css`
+        vertical-align: text-top;
+      `}
+    />
   ) : (
-    <CheckBoxOutlineBlankIcon fontSize="small" />
+    <CheckBoxOutlineBlankIcon
+      fontSize="small"
+      css={css`
+        vertical-align: text-top;
+      `}
+    />
   );
 };
 
